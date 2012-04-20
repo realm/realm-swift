db8aa43488c8adf67b6d11e1237b5e02f2918040
Thomas Andersen <thomas.1.andersen@gmail.com>
2012-04-20 19:28:40 +0200

    Remove invulgosoft

--- TightDbExample/TightDbExample/ViewController.h
+++ TightDbExample/TightDbExample/ViewController.h
@@ -1,10 +1,6 @@
 //
 //  ViewController.h
 //  TightDbExample
-//
-//  Created by Thomas Andersen on 20/04/12.
-//  Copyright (c) 2012 InvulgoSoft. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
