8cfe873aed692eb495795a1736859a966bc7e99b
Thomas Andersen <thomas.1.andersen@gmail.com>
2012-05-24 17:25:13 +0200

    Beautifyed output

--- TightDbExample/TightDbExample/Utils.m
+++ TightDbExample/TightDbExample/Utils.m
@@ -11,6 +11,7 @@
 @implementation Utils
 {
     UIScrollView *_view;
+    NSArray *_groups;
     float y;
 }
 
@@ -22,6 +23,14 @@
     if (self) {
         _view = view;
         y = 0;
+        NSArray *colors = [NSArray arrayWithObjects:[UIColor whiteColor], [UIColor yellowColor], [UIColor cyanColor], [UIColor greenColor], nil];
+        _groups = [NSArray arrayWithObjects:[[UIView alloc] init], [[UIView alloc] init], [[UIView alloc] init], [[UIView alloc] init], nil];
+        int idx = 0;
+        for(UIView *v in _groups) {
+            [_view addSubview:v];
+            [v setBackgroundColor:[colors objectAtIndex:idx]];
+            idx++;
+        }
     }
     return self;
 }
@@ -40,14 +49,41 @@
 
 -(void)Eval:(BOOL)good msg:(NSString *)msg
 {
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, y, _view.bounds.size.width, LineHeight)];
-    label.text = [NSString stringWithFormat:@"%@ - %@", good?@"OK":@"Fail", msg];
+    [self OutGroup:GROUP_LOG msg:[NSString stringWithFormat:@"%@ - %@", good?@"OK":@"Fail", msg] good:good];
+}
+
+-(void)resizeParent
+{
+    float height = 0;
+    for(UIView *view in _groups) {
+        CGRect frame = view.frame;
+        frame.origin.y = height;
+        view.frame = frame;
+        height += view.frame.size.height;
+//        NSLog(@"View: %@", NSStringFromCGRect(frame));
+    }
+    _view.contentSize = CGSizeMake(_view.contentSize.width, height);
+}
+-(void)OutGroup:(int)group msg:(NSString *)msg
+{
+    [self OutGroup:group msg:msg good:YES];
+}
+-(void)OutGroup:(int)group msg:(NSString *)msg good:(BOOL)good
+{
+    UIView *view = [_groups objectAtIndex:group];
+    CGRect frame = view.frame;
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, frame.size.height, view.bounds.size.width, LineHeight)];
+    [label setBackgroundColor:[UIColor clearColor]];
+    label.text = msg;
     if (!good)
         label.backgroundColor = [UIColor redColor];
-    label.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    [_view addSubview:label];
-    y += LineHeight;
-    _view.contentSize = CGSizeMake(_view.bounds.size.width, y);
+    label.autoresizingMask = UIViewAutoresizingFlexibleWidth;    
+    view.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [view addSubview:label];
+    frame.size.width = _view.bounds.size.width;
+    frame.size.height += label.frame.size.height;
+    view.frame = frame;
+    [self resizeParent];
 }
 
 
