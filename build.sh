71e6ec9e59ea2e437e28401a9f61eb478877dbb8
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-08-22 02:35:55 +0200

    Makfile update

--- build.sh
+++ build.sh
@@ -8,7 +8,34 @@ MODE="$1"
 
 case "$MODE" in
 
-    "copy")
+    "clean")
+        echo "Not yet implemented" 1>&2
+        exit 1
+        ;;
+
+    "build")
+        echo "Not yet implemented" 1>&2
+        exit 1
+        ;;
+
+    "test")
+        echo "Not yet implemented" 1>&2
+        exit 1
+        ;;
+
+    "install")
+        PREFIX="$1"
+        echo "Not yet implemented" 1>&2
+        exit 1
+        ;;
+
+    "test-installed")
+        PREFIX="$1"
+        echo "Not yet implemented" 1>&2
+        exit 1
+        ;;
+
+    "dist-copy")
         # Copy to distribution package
         TARGET_DIR="$1"
         if ! [ "$TARGET_DIR" -a -d "$TARGET_DIR" ]; then
@@ -21,26 +48,10 @@ case "$MODE" in
         (cd "$TARGET_DIR" && tar xzf "$TEMP_DIR/archive.tar.gz") || exit 1
         ;;
 
-    "build")
-        exit 1
-        ;;
-
-    "test")
-        exit 1
-        ;;
-
-    "install")
-        PREFIX="$1"
-        exit 1
-        ;;
-
-    "test-installed")
-        PREFIX="$1"
-        exit 1
-        ;;
-
     *)
         echo "Unspecified or bad mode '$MODE'" 1>&2
+        echo "Available modes are: clean build test install test-installed" 1>&2
+        echo "As well as: dist-copy" 1>&2
         exit 1
         ;;
 
