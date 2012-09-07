53c5623a41a810053472d2eaa34c90ee1d5d7178
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-09-07 12:54:51 +0200

    Order of items in enum ColumnType updated to mach recent changes in the core library

--- Deliv/ColumnType.h
+++ Deliv/ColumnType.h
@@ -1,35 +1,41 @@
 #ifndef __TDB_COLUMNTYPE__
 #define __TDB_COLUMNTYPE__
 
+// Make sure numbers match with <tightdb/column_type.hpp>
+
 #ifdef __cplusplus
 #include <cstdlib>
 enum ColumnType {
-	// Single ref  // Make sure numbers match with tightdb columntype.
-	COLUMN_TYPE_INT = 0,
-	COLUMN_TYPE_BOOL = 1,
-	COLUMN_TYPE_STRING = 2,
-	COLUMN_TYPE_DATE = 3,
-	COLUMN_TYPE_BINARY = 4,
-	COLUMN_TYPE_TABLE = 5,
-	COLUMN_TYPE_MIXED = 6,
-    
+	COLUMN_TYPE_INT         =  0,
+	COLUMN_TYPE_BOOL        =  1,
+	COLUMN_TYPE_STRING      =  2,
+	COLUMN_TYPE_STRING_ENUM =  3, // double refs
+	COLUMN_TYPE_BINARY      =  4,
+	COLUMN_TYPE_TABLE       =  5,
+	COLUMN_TYPE_MIXED       =  6,
+	COLUMN_TYPE_DATE        =  7,
+        COLUMN_TYPE_RESERVED1   =  8, // DateTime
+        COLUMN_TYPE_RESERVED2   =  9, // Float
+        COLUMN_TYPE_RESERVED3   = 10, // Double
+        COLUMN_TYPE_RESERVED4   = 11  // Decimal
+
 	// Double refs
-	COLUMN_TYPE_STRING_ENUM = 7
 };
 #else
 #include <stdlib.h>
 typedef enum  {
-	// Single ref
-	COLUMN_TYPE_INT = 0,
-	COLUMN_TYPE_BOOL = 1,
-	COLUMN_TYPE_STRING = 2,
-	COLUMN_TYPE_DATE = 3,
-	COLUMN_TYPE_BINARY = 4,
-	COLUMN_TYPE_TABLE = 5,
-	COLUMN_TYPE_MIXED = 6,
-    
-	// Double refs
-	COLUMN_TYPE_STRING_ENUM = 7
+	COLUMN_TYPE_INT         =  0,
+	COLUMN_TYPE_BOOL        =  1,
+	COLUMN_TYPE_STRING      =  2,
+	COLUMN_TYPE_STRING_ENUM =  3, // double refs
+	COLUMN_TYPE_BINARY      =  4,
+	COLUMN_TYPE_TABLE       =  5,
+	COLUMN_TYPE_MIXED       =  6,
+	COLUMN_TYPE_DATE        =  7,
+        COLUMN_TYPE_RESERVED1   =  8, // DateTime
+        COLUMN_TYPE_RESERVED2   =  9, // Float
+        COLUMN_TYPE_RESERVED3   = 10, // Double
+        COLUMN_TYPE_RESERVED4   = 11  // Decimal
 } ColumnType;
 #endif
 
