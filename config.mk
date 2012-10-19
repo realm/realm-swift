673101eb920ea3cb118543cd34add6adc74a5ee6
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-10-19 17:58:23 +0200

    Multiacrh disabled: Not supported by Objective-C ARC

--- config.mk
+++ config.mk
@@ -1,12 +1,13 @@
-# Construct fat binaries on Darwin when using Clang
-ifneq ($(TIGHTDB_ENABLE_FAT_BINARIES),)
-ifneq ($(call CC_CXX_AND_LD_ARE,clang),)
-ifeq ($(shell uname),Darwin)
-CFLAGS_DEFAULT  += -arch i386 -arch x86_64
-LDFLAGS_DEFAULT += -arch i386 -arch x86_64
-endif
-endif
-endif
+# FIXME: Our language binding requires that Objective-C ARC is enabled, which in turn only is available withing a 64-bit environment, so for now we cannot build a "fat" version.
+## Construct fat binaries on Darwin when using Clang
+#ifneq ($(TIGHTDB_ENABLE_FAT_BINARIES),)
+#ifneq ($(call CC_CXX_AND_LD_ARE,clang),)
+#ifeq ($(shell uname),Darwin)
+#CFLAGS_DEFAULT  += -arch i386 -arch x86_64
+#LDFLAGS_DEFAULT += -arch i386 -arch x86_64
+#endif
+#endif
+#endif
 
 CFLAGS_DEFAULT += -Wextra -ansi -pedantic -Wno-long-long
 # FIXME: '-fno-elide-constructors' currently causes TightDB to fail
