c49181203078306dbf8b38e84ef3eb9540c77788
Kristian Spangsege <kristian.spangsege@gmail.com>
2012-10-28 19:03:13 +0100

    More fixes for: Now also building for iPhone when building on OS X

--- config.mk
+++ config.mk
@@ -1,12 +1,11 @@
-# FIXME: Our language binding requires that Objective-C ARC is enabled, which, in turn, is only available on a 64-bit architecture, so for now we cannot build a "fat" version.
-## Construct fat binaries on Darwin when using Clang
-#ifneq ($(TIGHTDB_ENABLE_FAT_BINARIES),)
-#ifneq ($(call CC_CXX_AND_LD_ARE,clang),)
-#ifeq ($(shell uname),Darwin)
-#CFLAGS_ARCH  += -arch i386 -arch x86_64
-#endif
-#endif
-#endif
+# Construct fat binaries on Darwin when using Clang
+ifneq ($(TIGHTDB_ENABLE_FAT_BINARIES),)
+ifneq ($(call CC_CXX_AND_LD_ARE,clang),)
+ifeq ($(shell uname),Darwin)
+CFLAGS_ARCH += -arch i386 -arch x86_64
+endif
+endif
+endif
 
 # FIXME: '-fno-elide-constructors' currently causes TightDB to fail
 #CFLAGS_DEBUG   += -fno-elide-constructors
