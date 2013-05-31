85ef8253f8b6e10581c8e4242f5d5553ae415268
Jesper Jepsen <jepsen.jesper@gmail.com>
2013-05-31 19:06:49 +0200

    Added "intro" examples for Table, Query, TableView (typed) and Group

---
+++ doc/ref/examples/ex_objc_group_intro.m
@@ -0,0 +1,51 @@
+// @@Example: ex_objc_group_intro @@
+
+#import <tightdb/objc/group.h>
+#import <tightdb/objc/table.h>
+#import <tightdb/objc/tightdb.h>
+
+TIGHTDB_TABLE_2(PeopleTable,
+                Name, String,
+                Age, Int)
+
+int main()
+{
+    @autoreleasepool {
+        
+        // Creates a group and uses it to create a new table.
+
+        TightdbGroup *group = [TightdbGroup group];
+        PeopleTable *table = [group getTable:@"people" withClass:[PeopleTable class]];
+        
+        // Adds values to the table.
+        
+        [table addName:@"Mary" Age:14];
+        [table addName:@"Joe" Age:17];
+        
+        // Write the group (and the contained table) to a specified file.
+        
+        [group write:@"filename.tightdb"];
+        
+        // Adds another row to the table. Note the update is NOT persisted
+        // automatically.
+        
+        [table addName:@"Sam" Age:17];
+        
+        // To save this update to disk write the group to a different file.
+        // WARNING: do not overwrite the old file this will corrupt the table.
+        // This issue will be resolved in TightDB build XX.
+        
+        [group write:@"filename2.tightdb"];
+        
+        // Retrieves a byte array from the group and uses it to create an
+        // NSData object.
+        
+        size_t size;
+        const char *buffer = [group writeToMem:&size];
+        [NSData dataWithBytes:buffer length:size];
+        
+        // call free() here?
+        
+    }
+}
+// @@EndExample@@
--- doc/ref/examples/ex_objc_typed_table_intro.m
+++ doc/ref/examples/ex_objc_typed_table_intro.m
@@ -1,9 +1,14 @@
 // @@Example: ex_objc_typed_table_intro @@
 
-#import <tightdb/objc/group.h>
 #import <tightdb/objc/table.h>
 #import <tightdb/objc/tightdb.h>
 
+// Defines a new table with two columns Name and Age. IMPORTANT: the column
+// names MUST begin with a capital. This table definition triggers a macro
+// defining the classes PeopleTable, PeopleTable_Query, PeopleTable_Curser
+// and PeopleTable_View. The column types must be TightDB types (refer to
+// the constructor TIGHTDB_TABLE_* in the documentation.
+ 
 TIGHTDB_TABLE_2(PeopleTable,
                 Name, String,
                 Age, Int)
@@ -11,22 +16,54 @@ TIGHTDB_TABLE_2(PeopleTable,
 int main()
 {
     @autoreleasepool {
-       
+        
+        // Creates a new table of the type defined above.
+
         PeopleTable *table = [[PeopleTable alloc] init];
         
-        [table addname:@"Mary" Age:14];
-        //[table addName:@"Joe" Age:17];
-        //[table addName:@"Jack" Age:22];
-        //[table addName:@"Paul" Age:33];
-        //[table addName:@"Simon" Age:17];
-        //[table addName:@"Carol" Age:22];
+        // Adds rows to the table. Notice that the signtaure of the method for
+        // adding rows includes the custom defined column nammes.
+        
+        [table addName:@"Mary" Age:14];
+        [table addName:@"Joe" Age:17];
+        [table addName:@"Jack" Age:22];
+        [table addName:@"Paul" Age:33];
+        [table addName:@"Simon" Age:16];
+        [table addName:@"Carol" Age:66];
+        
+        // Creates a query expression to filter on age. Note that the
+        // quiry is defined but not executed here.
         
         PeopleTable_Query *query = [[table where].Age between:13 to:19];
-  
-        NSLog(@"People of age between 13 and 19:");
+        
+        // Accesses query result directly on the quiry object. The quiry is
+        // executed once.
+        
         for (PeopleTable_Cursor *curser in query)
-            NSLog(@"Name: %@", [curser name]);
+            NSLog(@"Name: %@", [curser Name]);
+        
+        // For time consuming queries (in particular) the following is
+        // inefficient becuase the quiry is executed again.
+
+        for (PeopleTable_Cursor *curser in query)
+            NSLog(@"Name: %lld", [curser Age]);
+        
+        // To avoid repeating the same quiry, the result may be stored in
+        // a table view for multiple access. The following code executes the
+        // query once and saves the result in a table view.
+        
+        PeopleTable_View *tableView = [query findAll];
+        
+        // Iterates over all rows in the result (view) 2 times based on the single
+        // query executed above.
+        
+        for (PeopleTable_Cursor *curser in tableView)
+            NSLog(@"Name: %@", [curser Name]);
+            
+        for (PeopleTable_Cursor *curser in tableView)
+            NSLog(@"Name: %lld", [curser Age]);
 
     }
 }
+
 // @@EndExample@@
