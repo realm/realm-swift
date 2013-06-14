bbcb5e08a6934dfad1c11228a7bea9aa9a1dca59
Jesper Jepsen <jepsen.jesper@gmail.com>
2013-06-14 15:30:00 +0200

    Added more examples.

---
+++ doc/ref/examples/ex_objc_table_dynamic_intro.m
@@ -0,0 +1,22 @@
+// @@Example: ex_objc_table_dynamic_intro @@
+
+#import <tightdb/objc/table.h>
+#import <tightdb/objc/tightdb.h>
+
+// Defines a new table with two columns Name and Age. 
+ 
+
+
+int main()
+{
+    @autoreleasepool {
+        
+        // Creates a new table of the type defined above.
+        
+        
+        
+        
+    }
+}
+
+// @@EndExample@@
