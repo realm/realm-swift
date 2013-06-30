3e9f17d936cac4db10a64dca9b12217bb6ea661a
Thomas Andersen <thomas.1.andersen@gmail.com>
2013-06-30 14:56:00 +0200

    Error handling added for query. Note that in order to maintain query stackability, last error is kept and given in the actual request on the query.
    In this implementation NSNumber is used to be able to return nil on error.

--- src/tightdb/objc/test/tutorial.m
+++ src/tightdb/objc/test/tutorial.m
@@ -123,11 +123,11 @@ TIGHTDB_TABLE_IMPL_2(PeopleTable2,
                                   .Age between:20 to:30];
 
     // Get number of matching entries
-    NSLog(@"Query count: %zu", [q count]);
-    STAssertEquals([q count], (size_t)2,@"Expected 2 rows in query", nil);
+    NSLog(@"Query count: %@", [q count]);
+    STAssertEquals([[q count] unsignedLongValue] , (size_t)2,@"Expected 2 rows in query", nil);
 
     // Get the average age - currently only a low-level interface!
-    double avg = [q.Age avg];
+    double avg = [[q.Age avg] doubleValue];
     NSLog(@"Average: %f    [20.5]", avg);
     STAssertEquals(avg, 20.5,@"Expected 20.5 average", nil);
 
