8d9fc10cd00211dd152ab44bff7daa802e1d04dc
Jesper Jepsen <jepsen.jesper@gmail.com>
2013-07-01 16:40:19 +0200

    Adding conditions in Query Dynamic (with tests). In progress.

--- src/tightdb/objc/test/query.m
+++ src/tightdb/objc/test/query.m
@@ -193,15 +193,45 @@ TIGHTDB_TABLE_9(TestQueryAllTypes,
         [table setMixed:MIXED_COL ndx:0 value:mixInt1];
         [table setMixed:MIXED_COL ndx:1 value:mixString];
 
-        // Test correct values are returned.
-
-        STAssertEquals([[table where] sumInt:INT_COL], (int64_t)860, @"IntCol max");
-
-        // Test that currect query retult size returned.
+        // Conditions (note that count is invoked to get the number of matches)
 
         STAssertEquals([[[table where] betweenInt:859 to:861 colNdx:INT_COL] count], (size_t)1, @"betweenInt");
-        STAssertEquals([[[table where] betweenFloat:5.5 to:5.7 colNdx:FLOAT_COL] count], (size_t)1, @"betweenInt");
-        STAssertEquals([[[table where] betweenDouble:5.5 to:5.7 colNdx:DOUBLE_COL] count], (size_t)1, @"betweenInt");
+        STAssertEquals([[[table where] betweenFloat:5.5 to:5.7 colNdx:FLOAT_COL] count], (size_t)1, @"betweenFloat");
+        STAssertEquals([[[table where] betweenDouble:5.5 to:5.7 colNdx:DOUBLE_COL] count], (size_t)1, @"betweenDouble");
+
+        STAssertEquals([[[table where] equalBool:YES colNdx:BOOL_COL] count], (size_t)1, @"equalBool");
+        STAssertEquals([[[table where] equalInt:860 colNdx:INT_COL] count], (size_t)1, @"equalInt");
+        STAssertEquals([[[table where] equalFloat:5.6 colNdx:FLOAT_COL] count], (size_t)1, @"equalFloat");
+        STAssertEquals([[[table where] equalDouble:5.6 colNdx:DOUBLE_COL] count], (size_t)1, @"equalDouble");
+        STAssertEquals([[[table where] equalString:@"foo" colNdx:STRING_COL] count], (size_t)1, @"equalString");        
+        STAssertEquals([[[table where] equalString:@"Foo" colNdx:STRING_COL caseSensitive:NO] count], (size_t)1, @"equalStringCaseNo");
+        STAssertEquals([[[table where] equalString:@"Foo" colNdx:STRING_COL caseSensitive:YES] count], (size_t)0, @"equalStringCaseYes");
+        STAssertEquals([[[table where] equalDate:timeNow colNdx:DATE_COL] count], (size_t)1, @"equalDate");
+        STAssertEquals([[[table where] equalBinary:bin1 colNdx:BINARY_COL] count], (size_t)1, @"equalBinary");
+
+        STAssertEquals([[[table where] notEqualInt:860 colNdx:INT_COL] count], (size_t)1, @"notEqualInt");
+        STAssertEquals([[[table where] notEqualFloat:5.6 colNdx:FLOAT_COL] count], (size_t)1, @"notEqualFloat");
+        STAssertEquals([[[table where] notEqualDouble:5.6 colNdx:DOUBLE_COL] count], (size_t)1, @"notEqualDouble");
+        STAssertEquals([[[table where] notEqualString:@"foo" colNdx:STRING_COL] count], (size_t)1, @"notEqualString");        
+        STAssertEquals([[[table where] notEqualString:@"Foo" colNdx:STRING_COL caseSensitive:NO] count], (size_t)1, @"notEqualStringCaseNo");
+        STAssertEquals([[[table where] notEqualString:@"Foo" colNdx:STRING_COL caseSensitive:YES] count], (size_t)2, @"notEqualStringCaseYes");
+        STAssertEquals([[[table where] notEqualDate:timeNow colNdx:DATE_COL] count], (size_t)1, @"notEqualDate");
+        STAssertEquals([[[table where] notEqualBinary:bin1 colNdx:BINARY_COL] count], (size_t)1, @"notEqualBinary");
+
+        STAssertEquals([[[table where] greaterInt:859 colNdx:INT_COL] count], (size_t)1, @"greaterInt");
+        STAssertEquals([[[table where] greaterFloat:5.5 colNdx:FLOAT_COL] count], (size_t)1, @"greaterFloat");
+        STAssertEquals([[[table where] greaterDouble:5.5 colNdx:DOUBLE_COL] count], (size_t)1, @"greaterDouble");
+        STAssertEquals([[[table where] greaterDate:0 colNdx:DATE_COL] count], (size_t)1, @"greaterDate");
+
+        STAssertEquals([[[table where] greaterEqualInt:860 colNdx:INT_COL] count], (size_t)1, @"notEqualInt");
+        STAssertEquals([[[table where] greaterEqualFloat:5.6 colNdx:FLOAT_COL] count], (size_t)1, @"notEqualFloat");
+        STAssertEquals([[[table where] greaterEqualDouble:5.6 colNdx:DOUBLE_COL] count], (size_t)1, @"notEqualDouble");
+        STAssertEquals([[[table where] greaterEqualDate:timeNow colNdx:DATE_COL] count], (size_t)1, @"notEqualDate");
+
+   
+
+
+
 }
 
 @end
