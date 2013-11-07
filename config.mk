4b9901f4ffa650a86bea2c059f924366b78f2598
Kristian Spangsege <kristian.spangsege@gmail.com>
2013-11-07 18:01:56 +0100

    Specify --mmacosx-version-min=10.7 when building on Darwin

--- config.mk
+++ config.mk
@@ -4,10 +4,14 @@ ENABLE_INSTALL_DEBUG_LIBS  = 1
 # Construct fat binaries on Darwin when using Clang
 ifneq ($(TIGHTDB_ENABLE_FAT_BINARIES),)
 ifneq ($(call CC_CXX_AND_LD_ARE,clang),)
-ifeq ($(shell uname),Darwin)
+ifeq ($(OS),Darwin)
 CFLAGS_ARCH += -arch i386 -arch x86_64
 endif
 endif
+endif
+
+ifeq ($(OS),Darwin)
+CFLAGS_ARCH += -mmacosx-version-min=10.7
 endif
 
 # FIXME: '-fno-elide-constructors' currently causes TightDB to fail
