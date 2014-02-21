3513fc0d6cb09fcf56212fafcf2f35518ddfa900
mekjaer <mk@tightdb.com>
2014-02-21 14:21:19 +0100

    small changes

--- tutorial/iPhoneMiniTutorial/iPhoneMiniTutorial/main.m
+++ tutorial/iPhoneMiniTutorial/iPhoneMiniTutorial/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  iPhoneMiniTutorial
-//
-//  Created by Morten Kjaer on 21/02/14.
-//  Copyright (c) 2014 tightdb. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "TAppDelegate.h"
