f4c9d6dcb67693143945bac4efdfc4b3f84d1c24
mekjaer <mk@tightdb.com>
2014-02-24 15:40:15 +0100

    added import of shared group into tightdb.h

--- src/tightdb/objc/tightdb.h
+++ src/tightdb/objc/tightdb.h
@@ -32,6 +32,7 @@
 #import <tightdb/objc/query.h>
 #import <tightdb/objc/cursor.h>
 #import <tightdb/objc/helper_macros.h>
+#import <tightdb/objc/group_shared.h>
 
 
 #define TIGHTDB_TABLE_DEF_1(TableName, CName1, CType1) \
