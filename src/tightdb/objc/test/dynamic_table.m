83fd2a35bacd0e07cba74a0b8a8fda0b35fe7804
oleks <os@tightdb.com>
2014-03-06 14:07:55 +0100

    A couple more type-checking tests.

--- src/tightdb/objc/test/dynamic_table.m
+++ src/tightdb/objc/test/dynamic_table.m
@@ -162,6 +162,26 @@
     STAssertFalse(([t appendRow:@[]]), @"Too few items for a row.");
 }
 
+-(void)testAppendRowsWrongType
+{
+    TightdbTable *t = [[TightdbTable alloc] init];
+    [t addColumnWithType:tightdb_Int andName:@"first"];
+    STAssertFalse(([t appendRow:@[@true]]), @"Wrong type for column.");
+    STAssertFalse(([t appendRow:@[@""]]), @"Wrong type for column.");
+    STAssertFalse(([t appendRow:@[@3.5]]), @"Wrong type for column.");
+    STAssertFalse(([t appendRow:@[@3.5F]]), @"Wrong type for column.");
+    STAssertFalse(([t appendRow:@[@[]]]), @"Wrong type for column.");
+}
+
+-(void)testAppendRowsBoolColumn
+{
+    TightdbTable *t = [[TightdbTable alloc] init];
+    [t addColumnWithType:tightdb_Bool andName:@"first"];
+    STAssertTrue(([t appendRow:@[@YES]]), @"Cannot append bool column.");
+    STAssertTrue(([t appendRow:@[@NO]]), @"Cannot append bool column.");
+    STAssertEquals(2, [t count], @"2 rows expected");
+}
+
 -(void)testAppendRowsIntSubtableColumns
 {
     TightdbTable* t = [[TightdbTable alloc] init];
