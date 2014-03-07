804f24a4784180007481a666efc211dfc08a5b25
Kenneth Geisshirt <kg@tightdb.com>
2014-03-07 15:29:52 +0100

    mini-tutorial is using appendRow

--- tutorial/iPhoneMiniTutorial/iPhoneMiniTutorial/TViewController.m
+++ tutorial/iPhoneMiniTutorial/iPhoneMiniTutorial/TViewController.m
@@ -2,7 +2,6 @@
 #import <Tightdb/Tightdb.h>
 
 @interface TViewController ()
-
 @end
 
 @implementation TViewController
@@ -11,45 +10,38 @@
 {
     [super viewDidLoad];
     
+    // Create a data file
     TightdbSharedGroup *sharedGroup = [TightdbSharedGroup sharedGroupWithFile:[self writeablePathForFile: @"MyDatabase.db"] withError:nil];
-    
+
+    // Perform a write transaction
     [sharedGroup writeWithBlock:^(TightdbGroup *group) {
         
-        // Create a free-standing table
+        // Access table from group
         TightdbTable *table = [group getTable:@"myTable" error:nil];
         
         // Add columns to the table
-        int const NAME = [table addColumnWithType:tightdb_String andName:@"Name"];
-        int const AGE = [table addColumnWithType:tightdb_Int andName:@"Age"];
-        int const HIRED = [table addColumnWithType:tightdb_Bool andName:@"Hired"];
+        int const NAME  = [table addColumnWithType:tightdb_String andName:@"Name"];
+        int const AGE   = [table addColumnWithType:tightdb_Int    andName:@"Age"];
+        int const HIRED = [table addColumnWithType:tightdb_Bool   andName:@"Hired"];
         
-        // Add new row to the table and set values
-        TightdbCursor *cursor0 = [table addEmptyRow];
-        [cursor0 setString:@"Jill" inColumn:NAME];
-        [cursor0 setInt: 21 inColumn:AGE];
-        [cursor0 setBool:YES inColumn:HIRED];
-        
-        // Add one more row and set values
-        TightdbCursor *cursor1 = [table addEmptyRow];
-        [cursor1 setString:@"Mary" inColumn:NAME];
-        [cursor1 setInt: 40 inColumn:AGE];
-        [cursor1 setBool:NO inColumn:HIRED];
-        
+        // Add two rows to the table
+        [table appendRow:@[@"Jill", @21, @YES]];
+        [table appendRow:@[@"Mary", @40, @NO]];
+      
         // Change value in row
-        [cursor1 setBool:YES inColumn:HIRED];
+        TightdbCursor *cursor = [table cursorAtIndex:0];
+        [cursor setBool:NO inColumn:HIRED];
         
         // Remove row from table
-        [table removeRowAtIndex:cursor1.index];
-        cursor1 = nil;
-        
+        [table removeRowAtIndex:1];
+
         // Print out info on iPhone screen
         self.tableColumnCountOutlet.text = [NSString stringWithFormat:@"# of columns: %zu", [table getColumnCount]];
         self.tableSizeOutlet.text = [NSString stringWithFormat:@"# of rows: %zu", [table count]];
 
         return YES;
     } withError:nil];
-    
-    }
+}
 
 - (NSString*)writeablePathForFile:(NSString*)fileName
 {
