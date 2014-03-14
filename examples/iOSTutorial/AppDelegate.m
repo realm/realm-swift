a6361f7ef17dc85009d7c257090262221b50a2a5
Alexander Stigsen <astigsen@tightdb.com>
2014-03-14 14:59:54 -0700

    Updated tutorial with latest syntax improvements

--- examples/iOSTutorial/AppDelegate.m
+++ examples/iOSTutorial/AppDelegate.m
@@ -27,46 +27,43 @@ void tableFunc() {
     cursor.Hired = YES;
 
     // Add more rows
-    [people appendRow:@[@"Mary", @76, @NO]];
-    [people appendRow:@[@"Lars", @22, @YES]];
-    [people appendRow:@[@"Phil", @43, @NO]];
-    [people appendRow:@[@"Anni", @54, @YES]];
+    [people appendRow:@{@"Name": @"Mary", @"Age": @76, @"Hired": @NO}];
+    [people appendRow:@{@"Name": @"Lars", @"Age": @22, @"Hired": @YES}];
+    [people appendRow:@{@"Name": @"Phil", @"Age": @43, @"Hired": @NO}];
+    [people appendRow:@{@"Name": @"Anni", @"Age": @54, @"Hired": @YES}];
+
     // @@EndExample@@
 
     // @@Example: insert_at_index @@
-    cursor = [people insertEmptyRowAtIndex:2];
-    cursor.Name = @"Frank";
-    cursor.Age = 34;
-    cursor.Hired = YES;
+    [people insertRow:@{@"Name": @"Frank", @"Age": @34, @"Hired": @YES} atRowIndex:2];
 
     // @@EndExample@@
 
     // @@Example: number_of_rows @@
-    size_t cnt1 = people.rowCount;                       // cnt = 6
-    BOOL empty = people.rowCount;                      // empty = NO
+    size_t cnt1 = people.rowCount;                     // cnt = 6
+    BOOL empty  = people.rowCount;                     // empty = NO
     // @@EndExample@@
 
     // @@Example: accessing_rows @@
     // Getting values directly
     NSString* name = people[5].Name;                   // =&gt; 'Anni'
+
     // Using a cursor
-    PeopleTable_Cursor *myRow = [people cursorAtIndex:5];
+    PeopleTable_Cursor *myRow = people[5];
     int64_t age = myRow.Age;                           // =&gt; 54
     BOOL hired  = myRow.Hired;                         // =&gt; true
 
     // Setting values
-    [[people cursorAtIndex:5] setAge:43];              // Getting younger
-    // or with dot-syntax:
+    people[5].Age = 43;                                // Getting younger
     myRow.Age += 1;                                    // Happy birthday!
     // @@EndExample@@
 
     // @@Example: last_row @@
-    NSString *last = [people cursorAtLastIndex].Name;         // =&gt; "Anni"
+    NSString *last = [people cursorAtLastIndex].Name;  // =&gt; "Anni"
     // @@EndExample@@
 
     // @@Example: updating_entire_row @@
-    // Coming soon!
-    // [people setRowAtIndex:4 to:@[@"Eric", @50, @YES]];
+    people[4] = @{@"Name": @"Eric", @"Age": @50, @"Hired": @YES};
     // @@EndExample@@
 
     // @@Example: deleting_row @@
@@ -76,15 +73,15 @@ void tableFunc() {
 
     // @@Example: iteration @@
     for (size_t i = 0; i < people.rowCount; ++i) {
-        PeopleTable_Cursor *row = [people cursorAtIndex:i];
+        PeopleTable_Cursor *row = people[i];
         NSLog(@"%@ is %lld years old", row.Name, row.Age);
     }
     // @@EndExample@@
 
     // @@Example: simple_seach @@
-    size_t row;
-    row = [people.Name find:@"Philip"];                 // (size_t)-1. Not found
-    row = [people.Name find:@"Mary"];                     // row = 1
+    size_t row_id;
+    row_id = [people.Name find:@"Philip"];                 // (size_t)-1. Not found
+    row_id = [people.Name find:@"Mary"];                   // row = 1
     // @@EndExample@@
 
     // @@Example: advanced_search @@
@@ -101,9 +98,7 @@ void tableFunc() {
     // Execute the query and return a table (view)
     PeopleTable_View *res = [q findAll];
     for (size_t i = 0; i < res.rowCount; ++i) {
-        NSLog(@"%zu: %@ is %lld years old", i,
-              [people cursorAtIndex:i].Name,
-              [people cursorAtIndex:i].Age);
+        NSLog(@"%zu: %@ is %lld years old", i, people[i].Name, people[i].Age);
     }
 
     // Alternatively with fast emunaration
@@ -118,40 +113,31 @@ void sharedGroupFunc() {
 
     // @@Example: transaction @@
     TDBSharedGroup *sharedGroup = [TDBSharedGroup sharedGroupWithFile:@"people.tightdb"
-                                                          withError:nil];
+                                                            withError:nil];
 
     // Start a write transaction
     [sharedGroup writeWithBlock:^(TDBGroup *group) {
-
         // Get a specific table from the group
-        PeopleTable *table = [group getOrCreateTableWithName: @"employees"
-                                    asTableClass: [PeopleTable class]
-                                    error:     nil];
+        PeopleTable *table = [group getOrCreateTableWithName:@"employees"
+                                                asTableClass:[PeopleTable class]
+                                                       error:nil];
 
-        // Rollback if the table is not empty
-        if (table.rowCount > 0) {
-            NSLog(@"Not empty!");
-            return NO; // Rollback
-        }
-
-        // Otherwise add a row
+        // Add a row
         [table addName:@"Bill" Age:53 Hired:YES];
         NSLog(@"Row added!");
-        return YES; // Commit
-
+        return YES; // Commit (NO would rollback)
     } withError:nil];
 
     // Start a read transaction
     [sharedGroup readWithBlock:^(TDBGroup *group) {
-
         // Get the table
-        PeopleTable *table = [group getOrCreateTableWithName:  @"employees"
-                                    asTableClass: [PeopleTable class]
-                                    error:     nil];
+        PeopleTable *table = [group getOrCreateTableWithName:@"employees"
+                                                asTableClass:[PeopleTable class]
+                                                       error:nil];
 
         // Interate over all rows in table
-        for (PeopleTable_Cursor *curser in table) {
-            NSLog(@"Name: %@", [curser Name]);
+        for (PeopleTable_Cursor *row in table) {
+            NSLog(@"Name: %@", row.Name);
         }
     }];
     // @@EndExample@@
