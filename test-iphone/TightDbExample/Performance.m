af754ef72b63ce0df15e1a0add498bfd39798719
mekjaer <mk@tightdb.com>
2014-03-19 11:06:33 +0100

    fixed example to use Row instead of Cursor in typed interface

--- test-iphone/TightDbExample/Performance.m
+++ test-iphone/TightDbExample/Performance.m
@@ -259,8 +259,8 @@ TIGHTDB_TABLE_4(PerfTable,
 
     PerfTable_View *res = [q findAll];
     int agesum = 0;
-    for (PerfTable_Cursor *cur in res) {
-        agesum += cur.Age;
+    for (PerfTable_Row *row in res) {
+        agesum += row.Age;
         counter++;
     }
     NSTimeInterval stop = [NSDate timeIntervalSinceReferenceDate];
@@ -277,8 +277,8 @@ TIGHTDB_TABLE_4(PerfTable,
     PerfTable *diskTable = [fromDisk getOrCreateTableWithName:@"employees" asTableClass:[PerfTable class]];
 
     int agesum = 0;
-    for (PerfTable_Cursor *cur in diskTable) {
-        agesum += cur.Age;
+    for (PerfTable_Row *row in diskTable) {
+        agesum += row.Age;
     }
     NSTimeInterval stop = [NSDate timeIntervalSinceReferenceDate];
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -315,8 +315,8 @@ TIGHTDB_TABLE_4(PerfTable,
 
         PerfTable_View *res = [q findAll];
         int agesum = 0;
-        for (PerfTable_Cursor *cur in res) {
-            agesum += cur.Age;
+        for (PerfTable_Row *row in res) {
+            agesum += row.Age;
         }
     }];
 
