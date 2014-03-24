4886a2efd66272bc986f2f83c56f0fb637df71a1
mekjaer <mk@tightdb.com>
2014-03-24 17:28:46 +0100

    changes txt + headers updated

--- src/tightdb/objc/TDBTable.mm
+++ src/tightdb/objc/TDBTable.mm
@@ -1,7 +1,22 @@
-//
-//  table.mm
-//  TightDB
-//
+/*************************************************************************
+ *
+ * TIGHTDB CONFIDENTIAL
+ * __________________
+ *
+ *  [2011] - [2014] TightDB Inc
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of TightDB Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to TightDB Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from TightDB Incorporated.
+ *
+ **************************************************************************/
 
 #import <Foundation/Foundation.h>
 
@@ -206,7 +221,7 @@ using namespace std;
     return [TDBDescriptor descWithDesc:desc.get() readOnly:read_only error:error];
 }
 
--(NSUInteger)rowCount //Synthesize property
+-(NSUInteger)rowCount // Implementing property accessor
 {
     return m_table->size();
 }
