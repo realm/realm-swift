5ba87b6e72363b554f0c29a73aef1d3d8d9026aa
oleks <os@tightdb.com>
2014-03-28 15:52:46 +0100

    Tests now run on device

---
+++ test-core/iOSTestCoreApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSTemp
+//
+//  Created by Oleks on 28/03/14.
+//  Copyright (c) 2014 Tightdb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
