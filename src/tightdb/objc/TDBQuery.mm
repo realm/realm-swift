853b8d530d5bfdad6859dc3301df4709430dd832
mekjaer <mk@tightdb.com>
2014-04-08 17:17:20 +0200

    remove rows now check for readonly

--- src/tightdb/objc/TDBQuery.mm
+++ src/tightdb/objc/TDBQuery.mm
@@ -166,6 +166,11 @@ using namespace std;
 
 -(NSUInteger)removeRows
 {
+    if ([m_table isReadOnly]) {
+        @throw [NSException exceptionWithName:@"tightdb:table_is_read_only"
+                                       reason:@"You tried to modify an immutable table"
+                                     userInfo:[NSMutableDictionary dictionary]];
+    }
     return m_query->remove();
 }
 
