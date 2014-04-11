16d0cf5cbb9c33ccf18ed125758a91e64c0fc9f5
Kenneth Geisshirt <kg@tightdb.com>
2014-04-11 16:34:07 +0200

    Moving categories around

--- doc/ref/examples/ex_objc_sharedgroup_intro.m
+++ doc/ref/examples/ex_objc_sharedgroup_intro.m
@@ -24,49 +24,47 @@
 
 void ex_objc_context_intro()
 {
-    /* Remove any previous file */
+    // Remove any previous file
     NSFileManager *fm = [NSFileManager defaultManager];
     [fm removeItemAtPath:@"contextTest.tightdb" error:nil];
     [fm removeItemAtPath:@"contextTest.tightdb.lock" error:nil];
-    
-    /* Create datafile with a new table. */
-    TDBContext *context = [TDBContext contextPersistedAtPath:@"contextTest.tightdb"
-                                                             error:nil];
 
-    /* Perform a write transaction (with commit to file). */
+    // Create datafile with a new table
+    TDBContext *context = [TDBContext contextPersistedAtPath:@"contextTest.tightdb"
+                                                       error:nil];
+
+    // Perform a write transaction (with commit to file)
     NSError *error = nil;
     BOOL success;
     success = [context writeUsingBlock:^(TDBTransaction *transaction) {
         People *table = [transaction createTableWithName:@"employees"
                                             asTableClass:[People class]];
         [table addRow:@{@"Name":@"Bill", @"Age":@53, @"Hired":@YES}];
-        
-        return YES; /* Commit */
+
+        return YES; // Commit
     } error:&error];
     if (!success)
         NSLog(@"write-transaction failed: %@", [error description]);
 
-    
-    /* Perform a write transaction (with rollback). */
+    // Perform a write transaction (with rollback)
     success = [context writeUsingBlock:^(TDBTransaction *transaction) {
         People *table = [transaction createTableWithName:@"employees"
                                             asTableClass:[People class]];
         if ([table rowCount] == 0) {
             NSLog(@"Roll back!");
-            return NO; /* Rollback */
+            return NO;
         }
         [table addName:@"Bill" Age:53 Hired:YES];
         NSLog(@"Commit!");
-        return YES; /* Commit */
+        return YES;
     } error:&error];
     if (!success)
         NSLog(@"Transaction Rolled back : %@", [error description]);
 
-    
-    /* Perfrom a read transaction */
-    [context readUsingBlock:^(TDBTransaction *group) {
-        People *table = [group tableWithName:@"employees"
-                                asTableClass:[People class]];
+    // Perform a read transaction
+    [context readUsingBlock:^(TDBTransaction *transaction) {
+        People *table = [transaction tableWithName:@"employees"
+                                      asTableClass:[People class]];
         for (PeopleRow *row in table) {
             NSLog(@"Name: %@", row.Name);
         }
