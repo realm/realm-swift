4e6c74ba84e55c669941032bd831bec6af2cce5a
Fiel Guhit <fg@tightdb.com>
2014-04-16 10:48:51 -0700

    Renamed TDBContectDidChangeNotification

--- src/tightdb/objc/TDBConstants.m
+++ src/tightdb/objc/TDBConstants.m
@@ -21,4 +21,4 @@
 #import "TDBConstants.h"
 
 
-NSString *const TDBContextDidChangeNotification = @"TDBContextDidChangeNotification";
+NSString *const RLMContextDidChangeNotification = @"RLMContextDidChangeNotification";
