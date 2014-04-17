8672837f22fedb605b805993061dccf433da77ca
JP Simard <jp@jpsim.com>
2014-04-16 18:05:05 -0700

    Proof-of-concept support for Appledoc

--- src/tightdb/objc/PrivateHelperMacros.h
+++ src/tightdb/objc/PrivateHelperMacros.h
@@ -1,22 +1,22 @@
-/*************************************************************************
- *
- * TIGHTDB CONFIDENTIAL
- * __________________
- *
- *  [2011] - [2014] TightDB Inc
- *  All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of TightDB Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to TightDB Incorporated
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from TightDB Incorporated.
- *
- **************************************************************************/
+////////////////////////////////////////////////////////////////////////////
+//
+// TIGHTDB CONFIDENTIAL
+// __________________
+//
+//  [2011] - [2014] TightDB Inc
+//  All Rights Reserved.
+//
+// NOTICE:  All information contained herein is, and remains
+// the property of TightDB Incorporated and its suppliers,
+// if any.  The intellectual and technical concepts contained
+// herein are proprietary to TightDB Incorporated
+// and its suppliers and may be covered by U.S. and Foreign Patents,
+// patents in process, and are protected by trade secret or copyright law.
+// Dissemination of this information or reproduction of this material
+// is strictly forbidden unless prior written permission is obtained
+// from TightDB Incorporated.
+//
+////////////////////////////////////////////////////////////////////////////
 
 #define TIGHTDB_VA_NARGS_IMPL(_1, _2, _3, _4, _5, N, ...) N
 #define TIGHTDB_VA_NARGS(...) TIGHTDB_VA_NARGS_IMPL(__VA_ARGS__, 5, 4, 3, 2, 1)
--- src/tightdb/objc/TDBType.h
+++ src/tightdb/objc/TDBType.h
@@ -1,43 +1,42 @@
-/*************************************************************************
- *
- * TIGHTDB CONFIDENTIAL
- * __________________
- *
- *  [2011] - [2014] TightDB Inc
- *  All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of TightDB Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to TightDB Incorporated
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from TightDB Incorporated.
- *
- **************************************************************************/
+////////////////////////////////////////////////////////////////////////////
+//
+// TIGHTDB CONFIDENTIAL
+// __________________
+//
+//  [2011] - [2014] TightDB Inc
+//  All Rights Reserved.
+//
+// NOTICE:  All information contained herein is, and remains
+// the property of TightDB Incorporated and its suppliers,
+// if any.  The intellectual and technical concepts contained
+// herein are proprietary to TightDB Incorporated
+// and its suppliers and may be covered by U.S. and Foreign Patents,
+// patents in process, and are protected by trade secret or copyright law.
+// Dissemination of this information or reproduction of this material
+// is strictly forbidden unless prior written permission is obtained
+// from TightDB Incorporated.
+//
+////////////////////////////////////////////////////////////////////////////
+
 #ifndef TIGHTDB_OBJC_TYPE_H
 #define TIGHTDB_OBJC_TYPE_H
 
 /* Make sure numbers match those in <tightdb/data_type.hpp> */
-typedef enum {
-    TDBBoolType   =  1,
-    TDBIntType    =  0,
-    TDBFloatType  =  9,
-    TDBDoubleType = 10,
-    TDBStringType =  2,
-    TDBBinaryType =  4,
-    TDBDateType   =  7,
-    TDBTableType  =  5,
-    TDBMixedType  =  6,
-} TDBType;
+NS_ENUM(NSInteger, TDBType) {
+    TDBTypeBool   = 1,
+    TDBTypeInt    = 0,
+    TDBTypeFloat  = 9,
+    TDBTypeDouble = 10,
+    TDBTypeString = 2,
+    TDBTypeBinary = 4,
+    TDBTypeDate   = 7,
+    TDBTypeTable  = 5,
+    TDBTypeMixed  = 6
+};
 
-
-typedef enum {
-    TDBAscending   =  0,
-    TDBDescending   =  1,
-
-} TDBSortOrder;
+NS_ENUM(NSInteger, TDBSortOrder) {
+    TDBSortOrderAscending  = 0,
+    TDBSortOrderDescending = 1
+};
 
 #endif /* TIGHTDB_OBJC_TYPE_H */
