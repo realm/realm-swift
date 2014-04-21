16be4e06e2afa927ecbc5a547531c2d5b32145aa
JP Simard <jp@jpsim.com>
2014-04-21 11:47:41 -0700

    Added smart context documentation and example. Also fixed an issue with ex_objc_context_intro example

--- doc/ref/examples/examples.h
+++ doc/ref/examples/examples.h
@@ -2,6 +2,7 @@ void ex_objc_group_intro();
 void ex_objc_query_dynamic_intro();
 void ex_objc_query_typed_intro();
 void ex_objc_context_intro();
+void ex_objc_smart_context_intro();
 void ex_objc_table_dynamic_intro();
 void ex_objc_table_typed_intro();
 void ex_objc_table_typed_intro_with_many_comments();
