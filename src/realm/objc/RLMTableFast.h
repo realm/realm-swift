229fedf271ff54034ff4f4e267afa67297465b4b
Fiel Guhit <--global>
2014-04-22 14:13:09 -0700

    Removed optimize from RLMTable interface and moved it to RLMTableFast. Need to import RLMTableFast to test cases that use optimize method

---
+++ src/realm/objc/RLMTableFast.h
@@ -0,0 +1,15 @@
+//
+//  RLMTable+Fast.h
+//  RealmObjcDyn
+//
+//  Created by Fiel Guhit on 4/22/14.
+//  Copyright (c) 2014 Thomas Andersen. All rights reserved.
+//
+
+#import "RLMTable.h"
+
+@interface RLMTable (Fast)
+
+-(BOOL)optimize;
+
+@end
