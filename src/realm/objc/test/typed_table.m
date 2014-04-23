89058299bf7fa047dffeb0d0905dab8fc5223634
Ari Lazier <alazier@gmail.com>
2014-04-23 11:29:35 -0700

    add tests for c/c++ bool and alternate int types

--- src/realm/objc/test/typed_table.m
+++ src/realm/objc/test/typed_table.m
@@ -33,13 +33,15 @@ RLM_DEFINE_TABLE_TYPE_FOR_OJBECT_TYPE(AgeTable, Sub)
 
 @interface AllTypes : RLMRow
 @property BOOL           boolCol;
-@property NSInteger      intCol;
+@property int            intCol;
 @property float          floatCol;
 @property double         doubleCol;
 @property NSString      *stringCol;
 @property NSData        *binaryCol;
 @property NSDate        *dateCol;
 @property AgeTable *tableCol;
+@property bool           cBoolCol;
+@property long           longCol;
 @end
 
 @implementation AllTypes
@@ -90,6 +92,8 @@ RLM_DEFINE_TABLE_TYPE_FOR_OJBECT_TYPE(KeyedTable, KeyedObject)
     XCTAssertEqual(RLMTypeBinary, [table columnTypeOfColumnWithIndex:5], @"Sixth column not binary");
     XCTAssertEqual(RLMTypeDate,   [table columnTypeOfColumnWithIndex:6], @"Seventh column not date");
     XCTAssertEqual(RLMTypeTable,  [table columnTypeOfColumnWithIndex:7], @"Eighth column not table");
+    XCTAssertEqual(RLMTypeBool,   [table columnTypeOfColumnWithIndex:8], @"Ninth column not bool");
+    XCTAssertEqual(RLMTypeInt,    [table columnTypeOfColumnWithIndex:9], @"Tenth column not long");
 
     const char bin[4] = { 0, 1, 2, 3 };
     NSData* bin1 = [[NSData alloc] initWithBytes:bin length:sizeof bin / 2];
@@ -112,13 +116,13 @@ RLM_DEFINE_TABLE_TYPE_FOR_OJBECT_TYPE(KeyedTable, KeyedObject)
     c = table.lastRow;
     
     c.BoolCol   = NO   ; c.IntCol  = 54 ; c.FloatCol = 0.7     ; c.DoubleCol = 0.8     ; c.StringCol = @"foo";
-    c.BinaryCol = bin1 ; c.DateCol = timeZero  ; c.TableCol = subtab1     ;
+    c.BinaryCol = bin1 ; c.DateCol = timeZero  ; c.TableCol = subtab1     ; c.cBoolCol = false; c.longCol = 99;
     
     [table addRow:nil];
     c = table.lastRow; 
     
     c.BoolCol   = YES  ; c.IntCol  = 506     ; c.FloatCol = 7.7         ; c.DoubleCol = 8.8       ; c.StringCol = @"banach";
-    c.BinaryCol = bin2 ; c.DateCol = timeNow ; c.TableCol = subtab2     ;
+    c.BinaryCol = bin2 ; c.DateCol = timeNow ; c.TableCol = subtab2     ; c.cBoolCol = true;    c.longCol = -20;
     
     //AllTypes* row1 = [table rowAtIndex:0];
     //AllTypes* row2 = [table rowAtIndex:1];
@@ -141,6 +145,10 @@ RLM_DEFINE_TABLE_TYPE_FOR_OJBECT_TYPE(KeyedTable, KeyedObject)
     XCTAssertTrue(([row2.dateCol isEqual:timeNow]),   @"row2.DateCol");
     XCTAssertTrue([row1.tableCol isEqual:subtab1],    @"row1.TableCol");
     XCTAssertTrue([row2.tableCol isEqual:subtab2],    @"row2.TableCol");
+    XCTAssertEqual(row1.cBoolCol, false,              @"row1.cBoolCol");
+    XCTAssertEqual(row2.cBoolCol, true,               @"row2.cBoolCol");
+    XCTAssertEqual(row1.longCol, 99,                  @"row1.IntCol");
+    XCTAssertEqual(row2.longCol, -20,                 @"row2.IntCol");
 
     /* Not yet supported
     XCTAssertEqual([table.IntCol minimum], (int64_t)54,                 @"IntCol min");
