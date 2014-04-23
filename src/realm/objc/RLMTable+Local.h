1eb241cc41d146c8c89eb59063ff2ab5fb5552ff
Fiel Guhit <fg@tightdb.com>
2014-04-23 12:53:08 -0700

    Added init methods for unit test initialization outside of context

--- src/realm/objc/RLMTable+Local.h
+++ src/realm/objc/RLMTable+Local.h
@@ -1,14 +1,29 @@
-//
-//  RLMTable+Local.h
-//  Realm
-//
-//  Created by Fiel Guhit on 4/23/14.
-//  Copyright (c) 2014 Realm. All rights reserved.
-//
+/*************************************************************************
+ *
+ * TIGHTDB CONFIDENTIAL
+ * __________________
+ *
+ *  [2011] - [2014] TightDB Inc
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of TightDB Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to TightDB Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from TightDB Incorporated.
+ *
+ **************************************************************************/
 
-#ifndef Realm_RLMTable_Local_h
-#define Realm_RLMTable_Local_h
+#import "RLMTable.h"
 
+@interface RLMTable (Local)
 
+// Initializers for standalone tables
+-(instancetype)initLocal;
+-(instancetype)initLocalWithColumns:(NSArray *)columns;
 
-#endif
+@end
