9f07cc9f2bfdb845a8cfb85fc41240cfaa58db61
Ari Lazier <alazier@gmail.com>
2014-04-22 22:27:35 -0700

    fallback to dynamic lookup when surpassing column limit - detailed comments on template function generation

--- src/realm/objc/RLMProperty.mm
+++ src/realm/objc/RLMProperty.mm
@@ -28,6 +28,8 @@
 #include <map>
 
 // templated getters/setters
+// these are used so we can use the same signature for all types
+// allowing us to use a single mechanism for generating column specific acessors
 template<typename T> inline T column_get(RLMRow *row, int col);
 template<typename T> inline void column_set(RLMRow *row, int col, T val);
 
@@ -56,6 +58,7 @@ template<> inline id column_get<id>(RLMRow *row, int col) { return row[col]; }
 template<> inline void column_set<id>(RLMRow *row, int col, id val) { row[col] = val; }
 
 // fixed column accessors
+// bakes the column number into the method signature to avoid looking up by name
 template<typename T, int C>
 T column_get(RLMRow *row, SEL) {
     return column_get<T>(row, C);
@@ -66,31 +69,41 @@ void column_set(RLMRow *row, SEL, T val) {
 }
 
 // column lookup accessors
-template<typename T, int C>
+// these are the slow versions of the above and are used in objects where you have more
+// than NUM_COLUMN_ACCESSORS columns
+template<typename T>
 T column_lookup_get(RLMRow *row, SEL sel) {
     NSUInteger col = [row.table indexOfColumnWithName:NSStringFromSelector(sel)];
     return column_get<T>(row, col);
 }
-template<typename T, int C>
+template<typename T>
 void column_lookup_set(RLMRow *row, SEL sel, T val) {
     NSString *name = NSStringFromSelector(sel);
+    // TODO - this currently assumes setters are named set<propertyname>
+    // we need to validated this and have a table of actual accessor names rather
+    // than making this asumption (in asana)
     name = [name substringWithRange:NSMakeRange(3, name.length-4)];
     NSUInteger col = [row.table indexOfColumnWithName:name];
     column_set<T>(row, col, val);
 }
 
 
-// column accessor enumerator
+// column accessor enumerator objects for storing generated functions
 typedef std::vector<IMP> ColumnFuncs;
 typedef std::pair<ColumnFuncs, ColumnFuncs> GettersSetters;
 
+// column generator for generating fast accessors with baked in column indexes
+// works with template recursion - we instantiate a version of this class for the
+// NUM column, which references a version of this class for each previous column
+// until we get to the 0th column
 template <int NUM, typename TYPE>
 class ColumnFuncsEnumerator {
 public:
     // column index
     enum { column = NUM - 1 };
     
-    // static method for function generation
+    // entry point for function generation
+    // creates the lookup table, and starts populating with the last column
     static GettersSetters enumerate(void) {
         ColumnFuncsEnumerator<NUM, TYPE> enumerator;
         GettersSetters funcs;
@@ -100,6 +113,9 @@ public:
         return funcs;
     }
     
+    // this is called recursively for each column starting with the last
+    // once we get to the 0th column, the specialized version
+    // of this function is called which ends the recursion
 	ColumnFuncsEnumerator<column, TYPE> prev;
 	void registerFuncs(GettersSetters & funcs) {
         funcs.first[column] = (IMP)column_get<TYPE, column>;
@@ -108,7 +124,7 @@ public:
 	}
 };
 
-// partial specialization to end the loop
+// partial specialization to end the recursion
 template <typename T>
 class ColumnFuncsEnumerator<0, T> {
 public:
@@ -116,13 +132,16 @@ public:
     void registerFuncs(GettersSetters &) {}
 };
 
+// static accessor lookup table and method type strings
 static std::map<char, GettersSetters> s_columnAccessors;
+static std::map<char, IMP> s_dynamicGetters, s_dynamicSetters;
 static std::map<char, const char *> s_getterTypeStrings, s_setterTypeStrings;
 
+// macros to generate objc type strings when registering methods
 #define GETTER_TYPES(C) C "@:"
 #define SETTER_TYPES(C) "v@:" C
 
-#define NUM_COLUMN_ACCESSORS 50
+#define NUM_COLUMN_ACCESSORS 25
 
 // determine RLMType from objc code
 void type_for_property_string(const char *code,
@@ -164,7 +183,8 @@ void type_for_property_string(const char *code,
                     // if typename is of form "@\"RLMTable<SubObjectClas>\""
                     const unsigned int subOffset = 11;
                     if (type.length > subOffset) {
-                        NSString *subclassName = [type substringWithRange:NSMakeRange(subOffset, type.length - subOffset - 2)];
+                        NSRange range = NSMakeRange(subOffset, type.length - subOffset - 2);
+                        NSString *subclassName = [type substringWithRange:range];
                         *outSubtableObjectClass = NSClassFromString(subclassName);
                     }
                 }
@@ -190,6 +210,7 @@ id dynamic_get_subtable(RLMRow *row, SEL sel) {
     return table;
 }
 
+
 @implementation RLMProperty
 
 +(void)initialize {
@@ -200,6 +221,13 @@ id dynamic_get_subtable(RLMRow *row, SEL sel) {
     s_columnAccessors['B'] = ColumnFuncsEnumerator<NUM_COLUMN_ACCESSORS, bool>::enumerate();
     s_columnAccessors['@'] = ColumnFuncsEnumerator<NUM_COLUMN_ACCESSORS, id>::enumerate();
     
+    s_dynamicGetters['i'] = (IMP)column_lookup_get<int>; s_dynamicSetters['i'] = (IMP)column_lookup_set<int>;
+    s_dynamicGetters['l'] = (IMP)column_lookup_get<long>; s_dynamicSetters['l'] = (IMP)column_lookup_set<long>;
+    s_dynamicGetters['f'] = (IMP)column_lookup_get<float>; s_dynamicSetters['f'] = (IMP)column_lookup_set<float>;
+    s_dynamicGetters['d'] = (IMP)column_lookup_get<double>; s_dynamicSetters['d'] = (IMP)column_lookup_set<double>;
+    s_dynamicGetters['B'] = (IMP)column_lookup_get<bool>; s_dynamicSetters['B'] = (IMP)column_lookup_set<bool>;
+    s_dynamicGetters['@'] = (IMP)column_lookup_get<id>; s_dynamicSetters['@'] = (IMP)column_lookup_set<id>;
+
     s_getterTypeStrings['i'] = GETTER_TYPES("i"); s_setterTypeStrings['i'] = SETTER_TYPES("i");
     s_getterTypeStrings['l'] = GETTER_TYPES("l"); s_setterTypeStrings['l'] = SETTER_TYPES("l");
     s_getterTypeStrings['f'] = GETTER_TYPES("f"); s_setterTypeStrings['f'] = SETTER_TYPES("f");
@@ -230,11 +258,10 @@ id dynamic_get_subtable(RLMRow *row, SEL sel) {
         else class_addMethod(cls, get, (IMP)dynamic_get_subtable, "@@:");
     }
     else {
-        // TODO - make sure we don't go over column limit
         char t = self.objcType;
         GettersSetters & accessors = s_columnAccessors[t];
-        IMP getter = (IMP)accessors.first[column];
-        IMP setter = (IMP)accessors.second[column];
+        IMP getter = column < NUM_COLUMN_ACCESSORS ? (IMP)accessors.first[column] : (IMP)s_dynamicGetters[t];
+        IMP setter = column < NUM_COLUMN_ACCESSORS ? (IMP)accessors.second[column] : (IMP)s_dynamicSetters[t];
         if (exists) {
             class_replaceMethod(cls, get, getter, s_getterTypeStrings[t]);
             class_replaceMethod(cls, set, setter, s_setterTypeStrings[t]);
