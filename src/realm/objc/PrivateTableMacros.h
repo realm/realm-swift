52c98a7b887e291c2c8353652195497e4d90681e
Fiel Guhit <fg@tightdb.com>
2014-04-23 12:51:59 -0700

    Removed init for typed tables to make them default to RLMTable implementation

--- src/realm/objc/PrivateTableMacros.h
+++ src/realm/objc/PrivateTableMacros.h
@@ -156,17 +156,6 @@ REALM_COLUMN_PROXY_IMPL(CName1, CType1) \
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     return self; \
 } \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    return self; \
-} \
 -(void)add##CName1:(REALM_ARG_TYPE(CType1))CName1 \
 { \
     NSUInteger ndx = self.rowCount; \
@@ -395,18 +384,6 @@ REALM_COLUMN_PROXY_IMPL(CName2, CType2) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     return self; \
@@ -654,19 +631,6 @@ REALM_COLUMN_PROXY_IMPL(CName3, CType3) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -930,20 +894,6 @@ REALM_COLUMN_PROXY_IMPL(CName4, CType4) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -1223,21 +1173,6 @@ REALM_COLUMN_PROXY_IMPL(CName5, CType5) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -1533,22 +1468,6 @@ REALM_COLUMN_PROXY_IMPL(CName6, CType6) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -1860,23 +1779,6 @@ REALM_COLUMN_PROXY_IMPL(CName7, CType7) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -2204,24 +2106,6 @@ REALM_COLUMN_PROXY_IMPL(CName8, CType8) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    REALM_COLUMN_PROXY_INIT(self, 7, CName8, CType8); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -2565,25 +2449,6 @@ REALM_COLUMN_PROXY_IMPL(CName9, CType9) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    REALM_COLUMN_PROXY_INIT(self, 7, CName8, CType8); \
-    REALM_COLUMN_PROXY_INIT(self, 8, CName9, CType9); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -2943,26 +2808,6 @@ REALM_COLUMN_PROXY_IMPL(CName10, CType10) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    REALM_COLUMN_PROXY_INIT(self, 7, CName8, CType8); \
-    REALM_COLUMN_PROXY_INIT(self, 8, CName9, CType9); \
-    REALM_COLUMN_PROXY_INIT(self, 9, CName10, CType10); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -3338,27 +3183,6 @@ REALM_COLUMN_PROXY_IMPL(CName11, CType11) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    REALM_COLUMN_PROXY_INIT(self, 7, CName8, CType8); \
-    REALM_COLUMN_PROXY_INIT(self, 8, CName9, CType9); \
-    REALM_COLUMN_PROXY_INIT(self, 9, CName10, CType10); \
-    REALM_COLUMN_PROXY_INIT(self, 10, CName11, CType11); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -3750,28 +3574,6 @@ REALM_COLUMN_PROXY_IMPL(CName12, CType12) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    REALM_COLUMN_PROXY_INIT(self, 7, CName8, CType8); \
-    REALM_COLUMN_PROXY_INIT(self, 8, CName9, CType9); \
-    REALM_COLUMN_PROXY_INIT(self, 9, CName10, CType10); \
-    REALM_COLUMN_PROXY_INIT(self, 10, CName11, CType11); \
-    REALM_COLUMN_PROXY_INIT(self, 11, CName12, CType12); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -4179,29 +3981,6 @@ REALM_COLUMN_PROXY_IMPL(CName13, CType13) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    REALM_COLUMN_PROXY_INIT(self, 7, CName8, CType8); \
-    REALM_COLUMN_PROXY_INIT(self, 8, CName9, CType9); \
-    REALM_COLUMN_PROXY_INIT(self, 9, CName10, CType10); \
-    REALM_COLUMN_PROXY_INIT(self, 10, CName11, CType11); \
-    REALM_COLUMN_PROXY_INIT(self, 11, CName12, CType12); \
-    REALM_COLUMN_PROXY_INIT(self, 12, CName13, CType13); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
@@ -4641,30 +4420,6 @@ REALM_COLUMN_PROXY_IMPL(CName14, CType14) \
     REALM_COLUMN_PROXY_INIT(self, 13, CName14, CType14); \
     return self; \
 } \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    REALM_COLUMN_PROXY_INIT(self, 7, CName8, CType8); \
-    REALM_COLUMN_PROXY_INIT(self, 8, CName9, CType9); \
-    REALM_COLUMN_PROXY_INIT(self, 9, CName10, CType10); \
-    REALM_COLUMN_PROXY_INIT(self, 10, CName11, CType11); \
-    REALM_COLUMN_PROXY_INIT(self, 11, CName12, CType12); \
-    REALM_COLUMN_PROXY_INIT(self, 12, CName13, CType13); \
-    REALM_COLUMN_PROXY_INIT(self, 13, CName14, CType14); \
-    return self; \
-} \
 -(void)add##CName1:(REALM_ARG_TYPE(CType1))CName1 CName2:(REALM_ARG_TYPE(CType2))CName2 CName3:(REALM_ARG_TYPE(CType3))CName3 CName4:(REALM_ARG_TYPE(CType4))CName4 CName5:(REALM_ARG_TYPE(CType5))CName5 CName6:(REALM_ARG_TYPE(CType6))CName6 CName7:(REALM_ARG_TYPE(CType7))CName7 CName8:(REALM_ARG_TYPE(CType8))CName8 CName9:(REALM_ARG_TYPE(CType9))CName9 CName10:(REALM_ARG_TYPE(CType10))CName10 CName11:(REALM_ARG_TYPE(CType11))CName11 CName12:(REALM_ARG_TYPE(CType12))CName12 CName13:(REALM_ARG_TYPE(CType13))CName13 CName14:(REALM_ARG_TYPE(CType14))CName14 \
 { \
     NSUInteger ndx = self.rowCount; \
@@ -5088,31 +4843,6 @@ REALM_COLUMN_PROXY_IMPL(CName15, CType15) \
     self = [super _initRaw]; \
     if (!self) \
         return nil; \
-    REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
-    REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
-    REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
-    REALM_COLUMN_PROXY_INIT(self, 3, CName4, CType4); \
-    REALM_COLUMN_PROXY_INIT(self, 4, CName5, CType5); \
-    REALM_COLUMN_PROXY_INIT(self, 5, CName6, CType6); \
-    REALM_COLUMN_PROXY_INIT(self, 6, CName7, CType7); \
-    REALM_COLUMN_PROXY_INIT(self, 7, CName8, CType8); \
-    REALM_COLUMN_PROXY_INIT(self, 8, CName9, CType9); \
-    REALM_COLUMN_PROXY_INIT(self, 9, CName10, CType10); \
-    REALM_COLUMN_PROXY_INIT(self, 10, CName11, CType11); \
-    REALM_COLUMN_PROXY_INIT(self, 11, CName12, CType12); \
-    REALM_COLUMN_PROXY_INIT(self, 12, CName13, CType13); \
-    REALM_COLUMN_PROXY_INIT(self, 13, CName14, CType14); \
-    REALM_COLUMN_PROXY_INIT(self, 14, CName15, CType15); \
-    return self; \
-} \
--(id)init \
-{ \
-    self = [super init]; \
-    if (!self) \
-        return nil; \
-    if (![self _addColumns]) \
-        return nil; \
-\
     REALM_COLUMN_PROXY_INIT(self, 0, CName1, CType1); \
     REALM_COLUMN_PROXY_INIT(self, 1, CName2, CType2); \
     REALM_COLUMN_PROXY_INIT(self, 2, CName3, CType3); \
