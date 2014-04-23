2094f1e09273df618334cd6f093b9e179c5ea343
Fiel Guhit <fg@tightdb.com>
2014-04-23 12:50:50 -0700

    New Local category files

---
+++ src/realm/objc/RLMLocal.h
@@ -0,0 +1,14 @@
+//
+//  RLMLocal.h
+//  Realm
+//
+//  Created by Fiel Guhit on 4/23/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#ifndef Realm_RLMLocal_h
+#define Realm_RLMLocal_h
+
+
+
+#endif
---
+++ src/realm/objc/RLMTable+Local.h
@@ -0,0 +1,14 @@
+//
+//  RLMTable+Local.h
+//  Realm
+//
+//  Created by Fiel Guhit on 4/23/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#ifndef Realm_RLMTable_Local_h
+#define Realm_RLMTable_Local_h
+
+
+
+#endif
