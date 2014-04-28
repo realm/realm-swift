88d1b64992624f0c9ff5542f1ee43fb6f12ebab4
Tim Anglade <timanglade@gmail.com>
2014-04-28 16:22:29 -0700

    Prevent private methods from seeping into the appledocs

--- src/realm/objc/RLMContext.h
+++ src/realm/objc/RLMContext.h
@@ -28,26 +28,100 @@ typedef void(^RLMWriteBlockWithRollback)(RLMRealm *realm, BOOL *rollback);
 typedef void(^RLMTableReadBlock)(RLMTable *table);
 typedef void(^RLMTableWriteBlock)(RLMTable *table);
 
-/****************	  RLMContext	****************/
-
+/**
+ 
+ RLMContexts are used to perform read and write transactions on an RLMRealm.
+ 
+ While an RLMRealm can be access directly on the Main UI Thread to read without transaction, an RLMContext
+ must be used on any other threads, and to perform any writes (including writes from the Main thread).
+ This is so that the Realm library can perform any necessary locks (in the case of writes), or bring the RLMRealm
+ up to date with the event loop for transactionless reads on the Main thread.
+ 
+ */
 @interface RLMContext : NSObject
 
 +(NSString *) defaultPath;
 
-// Initializers
+/**
+ *  Checks if the underlying RLMRealm has received any writes since the last time you
+ *  used this RLMContext.
+ *
+ *  @return YES if there have been any changes to the RLMRealm; NO otherwise.
+ */
+-(BOOL)hasChangedSinceLastTransaction;
+
+/**---------------------------------------------------------------------------------------
+ *  @name Creating & Initializing Contexts
+ *  ---------------------------------------------------------------------------------------
+ */
+/**
+ *  Creates an RLMContext for the RLMRealm persisted at the default location
+ *  (`Documents/default.realm`).
+ *
+ *  @return A reference to the RLMContext.
+ */
 +(RLMContext *)contextWithDefaultPersistence;
+/**
+ *  Creates an RLMContext for the RLMRealm persisted at a specific location.
+ *
+ *  @param path  Path to the Realm file you want to access.
+ *  @param error Pass-by-reference for errors.
+ *
+ *  @return A reference to the RLMContext
+ */
 +(RLMContext *)contextPersistedAtPath:(NSString *)path error:(NSError **)error;
 
-// Transactions
--(void)readUsingBlock:(RLMReadBlock)block;
--(void)writeUsingBlock:(RLMWriteBlock)block;
--(void)writeUsingBlockWithRollback:(RLMWriteBlockWithRollback)block;
 
-// Shortcuts for transactions on a single table
+/**---------------------------------------------------------------------------------------
+ *  @name Reading a Realm through a Context
+ *  ---------------------------------------------------------------------------------------
+ */
+/**
+ *  Performs a (non-blocking) read transaction on the RLMRealm referenced by this RLMContext.
+ *
+ *  @param block A block containing the read code you want to perform.
+ */
+-(void)readUsingBlock:(RLMReadBlock)block;
+
+/**
+ *  Performs a (non-blocking) read transaction and also pre-opens the specified table as a variable
+ * 
+ *  This is a helpful shortcut removing the need to perform a Table instantiation every time you
+ *  open a transaction. You can still open additional table within the block if you want to.
+ *
+ *  @param tablename The name of the table you’d like to open.
+ *  @param block     A block containing the read code you want to perform.
+ */
 -(void)readTable:(NSString*)tablename usingBlock:(RLMTableReadBlock)block;
+
+
+/**---------------------------------------------------------------------------------------
+ *  @name Writing to a Realm through a Context
+ *  ---------------------------------------------------------------------------------------
+ */
+
+/**
+ *  Performs a (blocking) write transaction on the RLMRealm referenced by this RLMContext
+ *
+ *  @param block A block containing the write code you want to perform.
+ */
+-(void)writeUsingBlock:(RLMWriteBlock)block;
+/**
+ *  Performs a (blocking) write transaction, with automatic rollback in case of errors
+ *
+ *  @param block A block containing the write code you want to perform.
+ */
+-(void)writeUsingBlockWithRollback:(RLMWriteBlockWithRollback)block;
+/**
+ *  Performs a (blocking) write transaction and also pre-opens the specificied table as a variable
+ *
+ *  This is a helpful shortcut removing the need to perform a Table instantiation every time you
+ *  open a transaction. You can still open additional table within the block if you want to.
+ *
+ *  @param tablename The name of the table you’d like to open.
+ *  @param block     A block containing the write code you want to perform.
+ */
 -(void)writeTable:(NSString*)tablename usingBlock:(RLMTableWriteBlock)block;
 
-// Context state info
--(BOOL)hasChangedSinceLastTransaction;
 
 @end
