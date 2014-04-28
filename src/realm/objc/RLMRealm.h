c75afb0d4709a2da6befb4e572596ea05aea592d
JP Simard <jp@jpsim.com>
2014-04-28 11:35:10 -0700

    Worked on RLMRealm appledoc's

--- src/realm/objc/RLMRealm.h
+++ src/realm/objc/RLMRealm.h
@@ -23,51 +23,178 @@
 
 @class RLMTable;
 
+/**
+ `RLMRealm` is a class that provides fully ACID compliant access to `RLMTable`'s.
+ 
+ `RLMRealm` objects are used for writing or reading tables. These tables are 
+ persisted to disk, and changes to the tables are done through explicit transactions 
+ within write blocks on a context.
+ 
+ Implicit transactions are updated at intervals of the runloop cycle only when 
+ there are new changes available. This implies that an update to a table is either 
+ stored or will fail, and the context is thereby always consistent.
+ 
+ Realm objects can either be created independently, or by an `RLMContext`'s 
+ write/read block.
+ 
+ Using a realm, you can access your data within the main thread of your 
+ application, without the need for an explicit context or transactions. The file 
+ format used by Realm can be used on any platform, and data can be accessed by 
+ any language Realm supports.
+ */
 @interface RLMRealm : NSObject
 
+#pragma mark - Constructors
+
+///------------------------------------------------
+/// @name Constructors
+///------------------------------------------------
+
 /**
- * Use the main run loop and the default notification center.
+ Creates a stand-alone `RLMRealm` at the default file path that lives outside any
+ explicit `RLMContext`. Can only be called on the main thread.
+ 
+ @return The newly initialized realm.
  */
 + (instancetype)realmWithDefaultPersistence;
 
+/**
+ Creates a stand-alone `RLMRealm` at the default file path that lives outside any 
+ explicit `RLMContext`.
+ 
+ Can only be called on the main thread. Runs an optional initialization block on
+ a writable context before returning.
+ 
+ @param initBlock The optional initialization block on a writable context before returning.
+ 
+ @return The newly initialized realm.
+ */
 + (instancetype)realmWithDefaultPersistenceAndInitBlock:(RLMWriteBlock)initBlock;
 
+/**
+ Creates a stand-alone `RLMRealm` at the specified file path that lives outside any
+ explicit `RLMContext`. Can only be called on the main thread.
+ 
+ @param path The file path where the realm is represented on disk.
+ 
+ @return The newly initialized realm.
+ */
 + (instancetype)realmWithPersistenceToFile:(NSString *)path;
 
+/**
+ Creates a stand-alone `RLMRealm` at the specified file path that lives outside any
+ explicit `RLMContext`.
+ 
+ Can only be called on the main thread. Runs an optional initialization block on
+ a writable context before returning.
+ 
+ @param path The file path where the realm is represented on disk.
+ @param initBlock The optional initialization block on a writable context before returning.
+ 
+ @return The newly initialized realm.
+ */
 + (instancetype)realmWithPersistenceToFile:(NSString *)path
                                  initBlock:(RLMWriteBlock)initBlock;
 
+/**
+ Creates a stand-alone `RLMRealm` at the specified file path that lives outside any
+ explicit `RLMContext`.
+ 
+ Read and write transactions are synchronized to cycles of the specified run loop.
+ 
+ @param path The file path where the realm is represented on disk.
+ @param runLoop The run loop on which read transactions will be synchronized. 
+        E.g. `[NSRunLoop mainRunLoop]`
+ @param notificationCenter The notification center realm notifications will be sent to. 
+        E.g. `[NSNotificationCenter defaultCenter]`
+ @param error A pointer to an `NSError`.
+ 
+ @return The newly initialized realm.
+ */
 + (instancetype)realmWithPersistenceToFile:(NSString *)path
                                    runLoop:(NSRunLoop *)runLoop
                         notificationCenter:(NSNotificationCenter *)notificationCenter
                                      error:(NSError **)error;
 
+/**
+ Creates a stand-alone `RLMRealm` at the specified file path that lives outside any
+ explicit `RLMContext`.
+ 
+ Read and write transactions are synchronized to cycles of the specified run loop.
+ Runs an optional initialization block on a writable context before returning.
+ 
+ @param path The file path where the realm is represented on disk.
+ @param initBlock The optional initialization block on a writable context before returning.
+ @param runLoop The run loop on which read transactions will be synchronized.
+ E.g. `[NSRunLoop mainRunLoop]`
+ @param notificationCenter The notification center realm notifications will be sent to.
+ E.g. `[NSNotificationCenter defaultCenter]`
+ @param error A pointer to an `NSError`.
+ 
+ @return The newly initialized realm.
+ */
 + (instancetype)realmWithPersistenceToFile:(NSString *)path
                                  initBlock:(RLMWriteBlock)initBlock
                                    runLoop:(NSRunLoop *)runLoop
                         notificationCenter:(NSNotificationCenter *)notificationCenter
                                      error:(NSError **)error;
 
+#pragma mark - Properties
+
+///------------------------------------------------
+/// @name Properties
+///------------------------------------------------
+
+/**
+ The number of tables in the realm
+ */
 @property (nonatomic, readonly) NSUInteger tableCount;
-@property (nonatomic, readonly) BOOL       isEmpty;
 
 /**
- * This method returns YES if a table with the specified name already exists. NO if it does not exist.
+ Returns `YES` if there are no tables in the realm. `NO` otherwise.
  */
--(BOOL)hasTableWithName:(NSString *)name;
+@property (nonatomic, readonly) BOOL isEmpty;
 
+#pragma mark - Get Tables
 
 /**
- * This method returns a table with the specified name from the realm.
- * Returns nil if no table with the specified name exists.
- * Optionally specify object class to be used when accessing table rows
+ Whether or not a table with the specified name already exists in the realm.
+ 
+ @param name The name string of the table to check for existence in the realm.
+ 
+ @return `YES` if the realm has a table with that name. `NO` otherwise.
  */
--(RLMTable *)tableWithName:(NSString *)name;
--(RLMTable *)tableWithName:(NSString *)name objectClass:(Class)objClass;
+- (BOOL)hasTableWithName:(NSString *)name;
 
 /**
- * This method returns a table with the specified name from the realm.
- * Returns nil if no table with the specified name exists.
+ Returns a table with the specified name from the realm.
+ 
+ @param name The name string of the table to get from the realm.
+ 
+ @return The table with the specified name in the realm if it exists.
+ `nil` otherwise.
+ */
+- (RLMTable *)tableWithName:(NSString *)name;
+
+/**
+ Returns a table with the specified name and object class from the realm.
+ 
+ @param name The name string of the table to get from the realm.
+ @param objClass The class of `RLMRow`-derived objects stored in the table.
+ 
+ @return The table with the specified name and object class in the realm if it exists.
+ `nil` otherwise.
+ */
+- (RLMTable *)tableWithName:(NSString *)name objectClass:(Class)objClass;
+
+/**
+ Returns a table with the specified name and table class from the realm.
+ 
+ @param name The name string of the table to get from the realm.
+ @param obj The class of the typed table.
+ 
+ @return The table with the specified name and table class in the realm if it exists.
+ `nil` otherwise.
  */
 -(id)tableWithName:(NSString *)name asTableClass:(Class)obj;
 
