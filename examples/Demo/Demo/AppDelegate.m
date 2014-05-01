8f996321e564f9a90922252ce92e22be53fed5e4
JP Simard <jp@jpsim.com>
2014-05-01 13:54:53 -0700

    Renamed RLMDemo->Demo

--- examples/Demo/Demo/AppDelegate.m
+++ examples/Demo/Demo/AppDelegate.m
@@ -1,22 +1,21 @@
 //
-//  RLMAppDelegate.m
-//  RLMDemo
+//  AppDelegate.m
+//  Demo
 //
-//  Created by JP Simard on 4/16/14.
 //  Copyright (c) 2014 Realm. All rights reserved.
 //
 
-#import "RLMAppDelegate.h"
-#import "RLMTableViewController.h"
+#import "AppDelegate.h"
+#import "TableViewController.h"
 
-@implementation RLMAppDelegate
+@implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[RLMTableViewController alloc] initWithStyle:UITableViewStylePlain]];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[TableViewController alloc] initWithStyle:UITableViewStylePlain]];
     [self.window makeKeyAndVisible];
     return YES;
 }
---
+++ examples/Demo/Demo/TableViewController.h
@@ -0,0 +1,12 @@
+//
+//  TableViewController.h
+//  Demo
+//
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
