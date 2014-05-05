d136e6f131bab3e6dbd90d6224b9b570352c96d6
JP Simard <jp@jpsim.com>
2014-05-05 11:13:41 -0700

    commented out unused function

--- src/realm/objc/query_util.mm
+++ src/realm/objc/query_util.mm
@@ -57,34 +57,34 @@ NSExpressionType validated_expression_type(NSExpression *expression) {
     return expression.expressionType;
 }
 
-// apply an expression between two columns to a query
-void update_query_with_column_expression(RLMTable * table, tightdb::Query & query,
-                                         NSString * col1, NSString * col2, NSPredicateOperatorType operatorType) {
-    
-    // only support equality for now
-    if (operatorType != NSEqualToPredicateOperatorType) {
-        @throw RLM_predicate_exception(@"Invalid predicate comparison type",
-                                       @"only support equality comparison type");
-    }
-    
-    // validate column names
-    NSUInteger index1 = RLM_validated_column_index(table, col1);
-    NSUInteger index2 = RLM_validated_column_index(table, col2);
-    
-    // make sure they are the same type
-    tightdb::DataType type1 = table->m_table->get_column_type(index1);
-    tightdb::DataType type2 = table->m_table->get_column_type(index2);
-    
-    if (type1 == type2) {
-        @throw RLM_predicate_exception(@"Invalid predicate expression",
-                                       @"Columns must be the same type");
-    }
-    
-    // not suppoting for now - if we changed names for column comparisons so that we could
-    // use templated function for all numeric types this would be much easier
-    @throw RLM_predicate_exception(@"Unsupported predicate",
-                                   @"Not suppoting column comparison for now");
-}
+//// apply an expression between two columns to a query
+//void update_query_with_column_expression(RLMTable * table, tightdb::Query & query,
+//                                         NSString * col1, NSString * col2, NSPredicateOperatorType operatorType) {
+//    
+//    // only support equality for now
+//    if (operatorType != NSEqualToPredicateOperatorType) {
+//        @throw RLM_predicate_exception(@"Invalid predicate comparison type",
+//                                       @"only support equality comparison type");
+//    }
+//    
+//    // validate column names
+//    NSUInteger index1 = RLM_validated_column_index(table, col1);
+//    NSUInteger index2 = RLM_validated_column_index(table, col2);
+//    
+//    // make sure they are the same type
+//    tightdb::DataType type1 = table->m_table->get_column_type(index1);
+//    tightdb::DataType type2 = table->m_table->get_column_type(index2);
+//    
+//    if (type1 == type2) {
+//        @throw RLM_predicate_exception(@"Invalid predicate expression",
+//                                       @"Columns must be the same type");
+//    }
+//    
+//    // not suppoting for now - if we changed names for column comparisons so that we could
+//    // use templated function for all numeric types this would be much easier
+//    @throw RLM_predicate_exception(@"Unsupported predicate",
+//                                   @"Not suppoting column comparison for now");
+//}
 
 // add a clause for numeric constraints based on operator type
 template <typename T>
