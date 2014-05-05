72b241ae2c25832f2aaeb1151e3a29b225ff64ac
JP Simard <jp@jpsim.com>
2014-05-05 12:59:04 -0700

    moved get_subtable test to object interface

--- src/realm/objc/test/get_subtable.m
+++ src/realm/objc/test/get_subtable.m
@@ -11,18 +11,17 @@
 #import <realm/objc/RLMRealm.h>
 #import <realm/objc/RLMTransactionManager.h>
 
-REALM_TABLE_2(GetSubtable,
-                Hired, Bool,
-                Age,   Int)
+@interface RLMTestObj : RLMRow
 
-REALM_TABLE_2(WrongNameTable,
-                HiredFor, Bool,
-                Ageing,   Int)
+@property (nonatomic, assign) BOOL hired;
+@property (nonatomic, assign) int age;
 
+@end
 
-REALM_TABLE_2(WrongTypeTable,
-                Hired, Int,
-                Age,   Bool)
+@implementation RLMTestObj
+@end
+
+RLM_TABLE_TYPE_FOR_OBJECT_TYPE(RLMTestSubtable, RLMTestObj);
 
 @interface MACTestGetSubtable: RLMTestCase
 
@@ -37,27 +36,21 @@ REALM_TABLE_2(WrongTypeTable,
         RLMDescriptor * desc = table.descriptor;
         [desc addColumnWithName:@"Outer" type:RLMTypeBool];
         [desc addColumnWithName:@"Number" type:RLMTypeInt];
-        RLMDescriptor * subdesc = [desc addColumnTable:@"GetSubtable"];
-        [subdesc addColumnWithName:@"Hired" type:RLMTypeBool];
-        [subdesc addColumnWithName:@"Age" type:RLMTypeInt];
+        RLMDescriptor * subdesc = [desc addColumnTable:@"RLMTestSubtable"];
+        [subdesc addColumnWithName:@"hired" type:RLMTypeBool];
+        [subdesc addColumnWithName:@"age" type:RLMTypeInt];
         
-        [table RLM_insertBool:0 ndx:0 value:NO];
-        [table RLM_insertInt:1 ndx:0 value:10];
-        [table RLM_insertSubtable:2 ndx:0];
-        [table RLM_insertDone];
+        {
+            [table addRow:@[@NO, @10, @[]]];
+            RLMTable *subtable = table.lastRow[@"RLMTestSubtable"];
+            [subtable addRow:@[@YES, @42]];
+        }
         
-        RLMTable* subtable = [table RLM_tableInColumnWithIndex:2 atRowIndex:0];
-        [subtable RLM_insertBool:0 ndx:0 value:YES];
-        [subtable RLM_insertInt:1 ndx:0 value:42];
-        [subtable RLM_insertDone];
-        
-        GetSubtable* testTable = [table RLM_tableInColumnWithIndex:2 atRowIndex:0 asTableClass:[GetSubtable class]];
-        GetSubtableRow* cursor = [testTable rowAtIndex:0];
-        NSLog(@"Age in subtable: %lld", cursor.Age);
-        XCTAssertEqual(cursor.Age, (int64_t)42, @"Sub table row should be 42");
-        
-        XCTAssertNil([table RLM_tableInColumnWithIndex:2 atRowIndex:0 asTableClass:[WrongNameTable class]], @"should return nil because wrong name");
-        XCTAssertNil([table RLM_tableInColumnWithIndex:2 atRowIndex:0 asTableClass:[WrongTypeTable class]], @"should return nil because wrong type");
+        {
+            RLMTable *subtable = table.lastRow[@"RLMTestSubtable"];
+            RLMTestObj *obj = subtable.firstRow;
+            XCTAssertEqual([obj[@"age"] integerValue], (NSInteger)42, @"Sub table row should be 42");
+        }
     }];
 }
 
