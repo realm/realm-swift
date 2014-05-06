ec5094fac822f56d401af91345465dc13a92f7ad
Jesper Zuschlag <jesper@zuschlag.dk>
2014-05-06 17:37:56 +0200

    Adding unit test for JSON serialization on Realm

--- src/realm/objc/test/group.m
+++ src/realm/objc/test/group.m
@@ -14,6 +14,16 @@ REALM_TABLE_2(TestTableRealm,
               First,  String,
               Second, Int)
 
+REALM_TABLE_8(TestTableRealmJson,
+              BoolCol,   Bool,
+              IntCol,    Int,
+              FloatCol,  Float,
+              DoubleCol, Double,
+              StringCol, String,
+              BinaryCol, Binary,
+              DateCol,   Date,
+              MixedCol,  Mixed)
+
 @interface MACTestRealm : RLMTestCase
 
 @end
@@ -55,6 +65,28 @@ REALM_TABLE_2(TestTableRealm,
         [realm createTableWithName:@"tableName"];
     }];
     XCTAssertEqual([[self realmWithTestPath] tableCount], (NSUInteger)1, @"1 table added");
+}
+
+- (void)testToJSONString {
+
+    RLMRealm *realm = [self realmWithTestPath];
+
+    [realm beginWriteTransaction];
+    [realm createTableWithName:@"test" asTableClass:[TestTableRealmJson class]];
+    TestTableRealmJson *table = [realm tableWithName:@"test" asTableClass:[TestTableRealmJson class]];
+    
+    
+    const char bin[4] = { 0, 1, 2, 3 };
+    NSData *binary = [[NSData alloc] initWithBytes:bin length:sizeof bin];
+    
+    NSDate *date = (NSDate *)[NSDate dateWithString:@"2014-05-17 13:15:10 +0100"];
+    [table addRow:@[@YES, @1234, @((float)12.34), @1234.5678, @"I'm just a String", binary, @((int)[date timeIntervalSince1970]), @"I'm also a string in a mixed column"]];
+
+    [realm commitWriteTransaction];
+    
+    NSString *result = [realm toJSONString];
+    
+    XCTAssertEqualObjects(result, @"{\"test\":[{\"BoolCol\":true,\"IntCol\":1234,\"FloatCol\":1.2340000e+01,\"DoubleCol\":1.2345678000000000e+03,\"StringCol\":\"I'm just a String\",\"BinaryCol\":\"00010203\",\"DateCol\":\"2014-05-17 12:15:10\",\"MixedCol\":\"I'm also a string in a mixed column\"}]}", @"JSON string expected to one 8-column row");
 }
 
 @end
