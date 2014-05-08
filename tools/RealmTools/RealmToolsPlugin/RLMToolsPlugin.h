96aa0b9d5b687aa879c275136da5ed390e21e24b
Fiel Guhit <fg@tightdb.com>
2014-05-07 20:57:54 -0700

    Configured build settings

---
+++ tools/RealmTools/RealmToolsPlugin/RLMToolsPlugin.h
@@ -0,0 +1,13 @@
+//
+//  RLMToolsPlugin.h
+//  RealmTools
+//
+//  Created by Fiel Guhit on 5/7/14.
+//  Copyright (c) 2014 Realm.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RLMToolsPlugin : NSObject
+
+@end
---
+++ tools/RealmTools/RealmToolsPlugin/RLMToolsPlugin.m
@@ -0,0 +1,18 @@
+//
+//  RLMToolsPlugin.m
+//  RealmTools
+//
+//  Created by Fiel Guhit on 5/7/14.
+//  Copyright (c) 2014 Realm.io. All rights reserved.
+//
+
+#import "RLMToolsPlugin.h"
+
+@implementation RLMToolsPlugin
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+	NSLog(@"Realm!");
+}
+
+@end
