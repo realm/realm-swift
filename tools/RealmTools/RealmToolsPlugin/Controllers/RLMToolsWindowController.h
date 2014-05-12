32afb33b13497cad417d69c3729ac117fdd1763d
Fiel Guhit <fg@tightdb.com>
2014-05-12 10:25:13 -0700

    Working

---
+++ tools/RealmTools/RealmToolsPlugin/Controllers/RLMToolsWindowController.h
@@ -0,0 +1,13 @@
+//
+//  RLMToolsWindowController.h
+//  RealmTools
+//
+//  Created by Fiel Guhit on 5/12/14.
+//  Copyright (c) 2014 Realm.io. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RLMToolsWindowController : NSWindowController
+
+@end
---
+++ tools/RealmTools/RealmToolsPlugin/Controllers/RLMToolsWindowController.m
@@ -0,0 +1,14 @@
+//
+//  RLMToolsWindowController.m
+//  RealmTools
+//
+//  Created by Fiel Guhit on 5/12/14.
+//  Copyright (c) 2014 Realm.io. All rights reserved.
+//
+
+#import "RLMToolsWindowController.h"
+
+@implementation RLMToolsWindowController
+
+
+@end
