a5f5c230a96c574ca1bd62e55f09bdda6d100430
Fiel Guhit <fg@tightdb.com>
2014-05-12 09:35:43 -0700

    Re-added target for xcode plugin

--- tools/RealmTools/RealmToolsPlugin/RLMToolsPlugin.m
+++ tools/RealmTools/RealmToolsPlugin/RLMToolsPlugin.m
@@ -2,7 +2,7 @@
 //  RLMToolsPlugin.m
 //  RealmTools
 //
-//  Created by Fiel Guhit on 5/7/14.
+//  Created by Fiel Guhit on 5/12/14.
 //  Copyright (c) 2014 Realm.io. All rights reserved.
 //
 
@@ -10,74 +10,36 @@
 
 @interface RLMToolsPlugin ()
 
-@property (strong, nonatomic) NSBundle *bundle;
+@property (strong, nonatomic) NSBundle *plugin;
 
 @end
 
-
 @implementation RLMToolsPlugin
 
-static RLMToolsPlugin *sharedPlugin = nil;
-
-+ (void)pluginDidLoad:(NSBundle *)plugin
++(void)pluginDidLoad:(NSBundle *)plugin
 {
-	static dispatch_once_t onceToken;
+	NSLog(@"    Realm Plugin Loaded");
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         sharedPlugin = [[self alloc] initWithBundle:plugin];
     });
-}
-
-+ (RLMToolsPlugin *)sharedPlugin
-{
-    return sharedPlugin;
 }
 
 - (id)initWithBundle:(NSBundle *)plugin
 {
     self = [super init];
     if (self) {
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notificationListener:) name:nil object:nil];
-        
-        _bundle = plugin;
-        [self addRealmToolsToWindow];
+        _plugin = plugin;
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidFinishLaunching:) name:NSApplicationDidFinishLaunchingNotification object:nil];
     }
     
     return self;
 }
 
-- (void)dealloc
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-}
-
-- (void)notificationListener:(NSNotification *)notification
-{
-//    if ([[notification name] length] >= 2 && [[[notification name] substringWithRange:NSMakeRange(0, 2)] isEqualTo:@"NS"])
-//		return;
-//	else
-//		NSLog(@"  Notification: %@", [notification name]);
-}
-
-- (void)addRealmToolsToWindow
-{
-    static NSString *const WindowMenuTitle = @"Window";
-    static NSString *const RealmMenuTitle = @"Realm Tools";
-    
-    NSMenu *menu = [NSApp mainMenu];
-    NSMenuItem *windowMenu = [menu itemWithTitle:WindowMenuTitle];
-    if (windowMenu) {
-        [windowMenu.submenu addItem:[NSMenuItem separatorItem]];
-        
-        NSMenuItem *realmMenu = [[NSMenuItem alloc] initWithTitle:RealmMenuTitle action:@selector(realmToolsClicked:) keyEquivalent:@""];
-        realmMenu.target = self;
-        
-        [windowMenu.submenu addItem:realmMenu];
-    }
-}
-
-- (void)realmToolsClicked:(id)sender
-{
-    NSLog(@"Present UI");
+    NSLog(@"    Application Did Finish Launching");
 }
 
 @end
