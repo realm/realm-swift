cce267a18f50c38a13c42384c7fd735d6fa8cda8
Ari Lazier <alazier@gmail.com>
2014-05-13 08:11:27 -0700

    remove garbage files

--- examples/RealmTableViewExample/DerivedData/RealmTableViewExample/Build/Intermediates/RealmTableViewExample.build/Debug-iphonesimulator/RealmTableViewExample.build/Objects-normal/i386/AppDelegate.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/ari/Src/TightDB/tightdb_objc/examples/RealmTableViewExample/RealmTableViewExample/AppDelegate.m \
-  /Users/ari/Src/TightDB/tightdb_objc/examples/RealmTableViewExample/RealmTableViewExample/AppDelegate.h \
-  /Users/ari/Src/TightDB/tightdb_objc/examples/RealmTableViewExample/RealmTableViewExample/TableViewController.h
