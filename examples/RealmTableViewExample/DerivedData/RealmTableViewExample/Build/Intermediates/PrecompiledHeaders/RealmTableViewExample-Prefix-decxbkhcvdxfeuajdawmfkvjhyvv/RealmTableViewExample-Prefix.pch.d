e09c35c1f1b1bd61d15fe885c8a394620baa7d99
Ari Lazier <alazier@gmail.com>
2014-05-13 08:11:27 -0700

    remove garbage files

--- examples/RealmTableViewExample/DerivedData/RealmTableViewExample/Build/Intermediates/PrecompiledHeaders/RealmTableViewExample-Prefix-decxbkhcvdxfeuajdawmfkvjhyvv/RealmTableViewExample-Prefix.pch.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/ari/Src/TightDB/tightdb_objc/examples/RealmTableViewExample/RealmTableViewExample/RealmTableViewExample-Prefix.pch
--- examples/RealmTableViewExample/DerivedData/RealmTableViewExample/Build/Intermediates/RealmTableViewExample.build/Debug-iphonesimulator/RealmTableViewExample.build/Objects-normal/i386/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ari/Src/TightDB/tightdb_objc/examples/RealmTableViewExample/RealmTableViewExample/main.m \
-  /Users/ari/Src/TightDB/tightdb_objc/examples/RealmTableViewExample/RealmTableViewExample/AppDelegate.h
