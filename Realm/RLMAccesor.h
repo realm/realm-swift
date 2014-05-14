69b08a3f53a66d649c7b80c525f2a2d5ce664b68
Ari Lazier <alazier@gmail.com>
2014-05-14 10:50:09 -0700

    move accessor and class generation code to RLMAccessor

---
+++ Realm/RLMAccesor.h
@@ -0,0 +1,13 @@
+//
+//  RLMAccesor.h
+//  Realm
+//
+//  Created by Ari Lazier on 5/14/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RLMAccesor : NSObject
+
+@end
---
+++ Realm/RLMAccesor.mm
@@ -0,0 +1,13 @@
+//
+//  RLMAccesor.m
+//  Realm
+//
+//  Created by Ari Lazier on 5/14/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import "RLMAccesor.h"
+
+@implementation RLMAccesor
+
+@end
