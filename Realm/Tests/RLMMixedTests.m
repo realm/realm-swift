2a8f2aa9373c09db426cfac051e359b5321580a3
Kenneth Geisshirt <kg@realm.io>
2014-05-15 10:31:34 +0200

    Checking that the values are correct.

--- Realm/Tests/RLMMixedTests.m
+++ Realm/Tests/RLMMixedTests.m
@@ -12,7 +12,6 @@
 
 
 @interface RLMMixedTests : RLMTestCase
-@property RLMRealm *realm;
 @end
 
 @implementation RLMMixedTests
@@ -21,13 +20,11 @@
 
 - (void)setUp {
     [super setUp];
-
-    [[NSFileManager defaultManager] removeItemAtPath:RLMDefaultRealmPath() error:nil];
-    _realm = [self realmWithTestPath];
+    [[NSFileManager defaultManager] removeItemAtPath:RLMTestRealmPath() error:nil];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [[NSFileManager defaultManager] removeItemAtPath:RLMTestRealmPath() error:nil];
     [super tearDown];
 }
 
@@ -35,35 +32,47 @@
 
 - (void)testMixedInsert {
     const char *data = "Hello World";
+    RLMRealm *realm = [self realmWithTestPath];
 
     // FIXME: add object with subtable
-    [_realm beginWriteTransaction];
-    [MixedObject createInRealm:_realm withObject:@[@YES, @"Jens", @50]];
-    [MixedObject createInRealm:_realm withObject:@[@YES, @10, @52]];
-    [MixedObject createInRealm:_realm withObject:@[@YES, @3.1f, @53]];
-    [MixedObject createInRealm:_realm withObject:@[@YES, @3.1, @54]];
-    [MixedObject createInRealm:_realm withObject:@[@YES, [NSDate date], @55]];
-    [MixedObject createInRealm:_realm withObject:@[@YES, [NSData dataWithBytes:(void *)data length:strlen(data)], @56]];
-    [_realm commitWriteTransaction];
+    [realm beginWriteTransaction];
+    [MixedObject createInRealm:realm withObject:@[@YES, @"Jens", @50]];
+    [MixedObject createInRealm:realm withObject:@[@YES, @10, @52]];
+    [MixedObject createInRealm:realm withObject:@[@YES, @3.1f, @53]];
+    [MixedObject createInRealm:realm withObject:@[@YES, @3.1, @54]];
+    [MixedObject createInRealm:realm withObject:@[@YES, [NSDate date], @55]];
+    [MixedObject createInRealm:realm withObject:@[@YES, [NSData dataWithBytes:(void *)data length:strlen(data)], @56]];
+    [realm commitWriteTransaction];
 
-    RLMArray *objects = [_realm objects:MixedObject.class where:nil];
+    RLMArray *objects = [realm objects:MixedObject.class where:nil];
     XCTAssertEqual(objects.count, (NSUInteger)6, @"6 rows excepted");
     XCTAssertTrue([[objects objectAtIndex:0] isKindOfClass:[MixedObject class]], @"MixedObject expected");
     XCTAssertTrue([[objects objectAtIndex:0][@"other"] isKindOfClass:[NSString class]], @"NSString expected");
+    XCTAssertTrue([[objects objectAtIndex:0][@"other"] isEqualToString:@"Jens"], @"'Jens' expected");
+
     XCTAssertTrue([[objects objectAtIndex:1][@"other"] isKindOfClass:[NSNumber class]], @"NSNumber expected");
+    XCTAssertEqual([[objects objectAtIndex:1][@"other"] longLongValue], (long long)10, @"'10' expected");
+
     XCTAssertTrue([[objects objectAtIndex:2][@"other"] isKindOfClass:[NSNumber class]], @"NSNumber expected");
+    XCTAssertEqual([[objects objectAtIndex:2][@"other"] floatValue], (float)3.1, @"'3.1' expected");
+
     XCTAssertTrue([[objects objectAtIndex:3][@"other"] isKindOfClass:[NSNumber class]], @"NSNumber expected");
+    XCTAssertEqual([[objects objectAtIndex:3][@"other"] doubleValue], (double)3.1, @"'3.1' expected");
+
     XCTAssertTrue([[objects objectAtIndex:4][@"other"] isKindOfClass:[NSDate class]], @"NSDate expected");
+
     XCTAssertTrue([[objects objectAtIndex:5][@"other"] isKindOfClass:[NSData class]], @"NSData expected");
 }
 
 - (void)testMixedValidate {
-    [_realm beginWriteTransaction];
-    XCTAssertThrows(([MixedObject createInRealm:_realm withObject:@[@YES, @[@1, @2], @7]]), @"Mixed cannot be an NSArray");
-    XCTAssertThrows(([MixedObject createInRealm:_realm withObject:@[@YES, @{@"key": @7}, @11]]), @"Mixed cannot be an NSDictionary");
-    [_realm commitWriteTransaction];
+    RLMRealm *realm = [self realmWithTestPath];
+
+    [realm beginWriteTransaction];
+    XCTAssertThrows(([MixedObject createInRealm:realm withObject:@[@YES, @[@1, @2], @7]]), @"Mixed cannot be an NSArray");
+    XCTAssertThrows(([MixedObject createInRealm:realm withObject:@[@YES, @{@"key": @7}, @11]]), @"Mixed cannot be an NSDictionary");
 
     XCTAssertEqual([MixedObject allObjects].count, (NSUInteger)0, @"0 rows expected");
+    [realm commitWriteTransaction];
 }
 
 
