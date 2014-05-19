95d27ac64831aad0b824e3bd6cda41fb815633af
Jesper Zuschlag <jesper@zuschlag.dk>
2014-05-19 15:59:11 +0200

    Clean up and extend test suite

--- Realm/Tests/QueryTests.m
+++ Realm/Tests/QueryTests.m
@@ -19,7 +19,9 @@
 ////////////////////////////////////////////////////////////////////////////
 
 #import "RLMTestCase.h"
+#import "Realm.h"
 #import "XCTestCase+AsyncTesting.h"
+#import "RLMUtil.h"
 
 @interface PersonQueryObject : RLMObject
 @property (nonatomic, copy) NSString *name;
@@ -30,16 +32,27 @@
 @end
 
 @interface TestQueryObject : RLMObject
-@property (nonatomic, copy) NSString *recordTag;
 @property (nonatomic, assign) NSInteger int1;
 @property (nonatomic, assign) NSInteger int2;
 @property (nonatomic, assign) float float1;
 @property (nonatomic, assign) float float2;
 @property (nonatomic, assign) double double1;
 @property (nonatomic, assign) double double2;
+@property (nonatomic, copy) NSString *recordTag;
 @end
 
 @implementation TestQueryObject
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"int1 = %lu, int2 = %lu", self.int1, self.int2];
+}
+
+- (NSString *)debugDescription
+{
+    return [self description];
+}
+
 @end
 
 @interface RLMQueryTests : RLMTestCase
@@ -114,54 +127,113 @@
     XCTAssertEqualObjects([some[0] name], @"Ari", @"Ari should be first results");
 }
 
-- (void)testTwoColumnComparisonQuery {
-    RLMRealm *realm = [self realmWithTestPath];
+- (void)testTwoColumnComparisonQuery
+{
+    [[NSFileManager defaultManager] removeItemAtPath:RLMDefaultRealmPath()
+                                               error:nil];
+    RLMRealm *realm = [RLMRealm defaultRealm];
     
     [realm beginWriteTransaction];
     
-    [TestQueryObject createInRealm:realm withObject:@[@"Instance 0", @1, @2, @23.0, @1.7,  @0.0,  @5.55]];
-    [TestQueryObject createInRealm:realm withObject:@[@"Instance 1", @1, @3, @-5.3, @4.21, @1.0,  @4.44]];
-    [TestQueryObject createInRealm:realm withObject:@[@"Instance 2", @2, @2, @1.0,  @3.55, @99.9, @6.66]];
-    [TestQueryObject createInRealm:realm withObject:@[@"Instance 3", @3, @6, @4.21, @1.0,  @1.0,  @7.77]];
-    [TestQueryObject createInRealm:realm withObject:@[@"Instance 2", @4, @5, @23.0, @23.0, @7.4,  @8.88]];
-    [TestQueryObject createInRealm:realm withObject:@[@"Instance 3", @15, @8, @1.0,  @66.0, @1.01, @9.99]];
+    [TestQueryObject createInRealm:realm withObject:@[@1, @2, @23.0, @1.7,  @0.0,  @5.55, @"Instance 0"]];
+    [TestQueryObject createInRealm:realm withObject:@[@1, @3, @-5.3, @4.21, @1.0,  @4.44, @"Instance 1"]];
+    [TestQueryObject createInRealm:realm withObject:@[@2, @2, @1.0,  @3.55, @99.9, @6.66, @"Instance 2"]];
+    [TestQueryObject createInRealm:realm withObject:@[@3, @6, @4.21, @1.0,  @1.0,  @7.77, @"Instance 3"]];
+    [TestQueryObject createInRealm:realm withObject:@[@4, @5, @23.0, @23.0, @7.4,  @8.88, @"Instance 4"]];
+    [TestQueryObject createInRealm:realm withObject:@[@15, @8, @1.0,  @66.0, @1.01, @9.99, @"Instance 5"]];
+    [TestQueryObject createInRealm:realm withObject:@[@15, @15, @1.0,  @66.0, @1.01, @9.99, @"Instance 6"]];
     
     [realm commitWriteTransaction];
-    
-    RLMArray *queryResult;
-    
-    queryResult = [TestQueryObject objectsWhere:@"int1 == int2"];
-    XCTAssertEqual(queryResult.count, 1, @"Expecting 1 result");
-    
-    queryResult = [TestQueryObject objectsWhere:@"int1 != int2"];
-    XCTAssertEqual(queryResult.count, 5, @"Expecting 5 result");
-    
-    queryResult = [TestQueryObject objectsWhere:@"int1 > int2"];
-    XCTAssertEqual(queryResult.count, 1, @"Expecting 1 result");
-    
-    queryResult = [TestQueryObject objectsWhere:@"int1 < int2"];
-    XCTAssertEqual(queryResult.count, 4, @"Expecting 4 result");
-    
-    queryResult = [TestQueryObject objectsWhere:@"int1 >= int2"];
-    XCTAssertEqual(queryResult.count, 2, @"Expecting 2 result");
-    
-    queryResult = [TestQueryObject objectsWhere:@"int1 <= int2"];
-    XCTAssertEqual(queryResult.count, 5, @"Expecting 5 result");
 
-    queryResult = [TestQueryObject objectsWhere:@"float1 == int2"];
-    queryResult = [TestQueryObject objectsWhere:@"float1 != int2"];
-    queryResult = [TestQueryObject objectsWhere:@"float1 > int2"];
-    queryResult = [TestQueryObject objectsWhere:@"float1 < int2"];
-    queryResult = [TestQueryObject objectsWhere:@"float1 >= int2"];
-    queryResult = [TestQueryObject objectsWhere:@"float1 <= int2"];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"int1 == int1"  expectedCount:7];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"int1 == int2"  expectedCount:2];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"int1 != int2"  expectedCount:5];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"int1 > int2"   expectedCount:1];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"int1 < int2"   expectedCount:4];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"int1 >= int2"  expectedCount:3];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"int1 <= int2"  expectedCount:6];
+    
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"float1 == float1"  expectedCount:7];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"float1 == float2"  expectedCount:1];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"float1 != float2"  expectedCount:6];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"float1 > float2"   expectedCount:2];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"float1 < float2"   expectedCount:4];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"float1 >= float2"  expectedCount:3];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"float1 <= float2"  expectedCount:5];
+    
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"double1 == double1" expectedCount:7];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"double1 == double2" expectedCount:0];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"double1 != double2" expectedCount:7];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"double1 > double2" expectedCount:1];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"double1 < double2" expectedCount:6];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"double1 >= double2" expectedCount:1];
+    [self executeTwoColumnKeypathRealmComparisonQueryWithClass:[TestQueryObject class] predicate:@"double1 <= double2" expectedCount:6];
+                
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"int1 == int1"  expectedCount:7];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"int1 == int2"  expectedCount:2];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"int1 != int2"  expectedCount:5];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"int1 > int2"   expectedCount:1];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"int1 < int2"   expectedCount:4];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"int1 >= int2"  expectedCount:3];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"int1 <= int2"  expectedCount:6];
 
-    queryResult = [TestQueryObject objectsWhere:@"double1 == double2"];
-    queryResult = [TestQueryObject objectsWhere:@"double1 != double2"];
-    queryResult = [TestQueryObject objectsWhere:@"double1 > double2"];
-    queryResult = [TestQueryObject objectsWhere:@"double1 < double2"];
-    queryResult = [TestQueryObject objectsWhere:@"double1 >= double2"];
-    queryResult = [TestQueryObject objectsWhere:@"double1 <= double2"];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"float1 == float1"  expectedCount:7];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"float1 == float2"  expectedCount:1];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"float1 != float2"  expectedCount:6];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"float1 > float2"   expectedCount:2];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"float1 < float2"   expectedCount:4];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"float1 >= float2"  expectedCount:3];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"float1 <= float2"  expectedCount:5];
 
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"double1 == double1" expectedCount:7];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"double1 == double2" expectedCount:0];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"double1 != double2" expectedCount:7];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"double1 > double2" expectedCount:1];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"double1 < double2" expectedCount:6];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"double1 >= double2" expectedCount:1];
+    [self executeTwoColumnKeypathComparisonQueryWithPredicate:@"double1 <= double2" expectedCount:6];
+/*
+    [self executeInvalidTwoColumnKeypathRealmComparisonQuery:[TestQueryObject class] predicate:@"int1 == float1" expectedCount:0];
+    [self executeInvalidTwoColumnKeypathRealmComparisonQuery:[TestQueryObject class] predicate:@"float2 >= double1" expectedCount:0];
+    [self executeInvalidTwoColumnKeypathRealmComparisonQuery:[TestQueryObject class] predicate:@"double2 <= int2" expectedCount:0];
+    [self executeInvalidTwoColumnKeypathRealmComparisonQuery:[TestQueryObject class] predicate:@"int2 != recordTag" expectedCount:0];
+    [self executeInvalidTwoColumnKeypathRealmComparisonQuery:[TestQueryObject class] predicate:@"float1 > recordTag" expectedCount:0];
+    [self executeInvalidTwoColumnKeypathRealmComparisonQuery:[TestQueryObject class] predicate:@"double1 < recordTag" expectedCount:0];
+*/
+}
+
+- (void)executeTwoColumnKeypathRealmComparisonQueryWithClass:(Class)class predicate:(NSString *)predicate expectedCount:(NSUInteger)expectedCount
+{
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    
+    RLMArray *queryResult = [realm objects:class
+                                     where:predicate];
+    NSUInteger actualCount = queryResult.count;
+    XCTAssertEqual(actualCount, expectedCount, @"Predicate: %@, Expecting %lu result(s), found %lu", predicate, expectedCount, actualCount);
+}
+
+- (void)executeTwoColumnKeypathComparisonQueryWithPredicate:(NSString *)predicate expectedCount:(NSUInteger)expectedCount
+{
+    RLMArray *queryResult = [TestQueryObject objectsWhere:predicate];
+    NSUInteger actualCount = queryResult.count;
+    XCTAssertEqual(actualCount, expectedCount, @"Predicate: %@, Expecting %lu result(s), found %lu", predicate, expectedCount, actualCount);
+}
+
+- (void)executeInvalidTwoColumnKeypathRealmComparisonQuery:(Class)class predicate:(NSString *)predicate expectedCount:(NSUInteger)expectedCount
+{
+    @try {
+        RLMRealm *realm = [RLMRealm defaultRealm];
+        
+        RLMArray *queryResult = [realm objects:class
+                                         where:predicate];
+        NSUInteger actualCount = queryResult.count;
+#pragma unused(actualCount)
+        
+        XCTFail(@"Predicate: %@ - exception expected.", predicate);
+    }
+    @catch (NSException *exception) {
+        
+    }
 }
 
 @end
