9c0f86bd71ef3ec5ee60ddee8b75f576e66b44b5
Ari Lazier <alazier@gmail.com>
2014-05-20 06:46:46 -0700

    more cleanup

--- Realm/RLMProperty_Private.h
+++ Realm/RLMProperty_Private.h
@@ -23,6 +23,13 @@
 // private property interface
 @interface RLMProperty ()
 
+// initializer
+-(instancetype)initWithName:(NSString *)name type:(RLMPropertyType)type column:(NSUInteger)column;
+
+// creates an RLMProperty object from a runtime property
++(instancetype)propertyForObjectProperty:(objc_property_t)prop column:(NSUInteger)column;
+
+// private properties
 @property (nonatomic, readonly) NSUInteger column;
 @property (nonatomic, readonly) char objcType;
 @property (nonatomic, readonly) id defaultValue;
@@ -30,12 +37,6 @@
 // getter and setter names
 @property (nonatomic, copy) NSString * getterName;
 @property (nonatomic, copy) NSString * setterName;
-
-// creates a tdb property object from a runtime property
-+(instancetype)propertyForObjectProperty:(objc_property_t)prop column:(NSUInteger)column;
-
-// initializer
--(instancetype)initWithName:(NSString *)name type:(RLMPropertyType)type column:(NSUInteger)column;
 
 @end
 
