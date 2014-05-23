cd16c986e061c253dd1dda66f7ee013a2da2adbf
Fiel Guhit <fg@tightdb.com>
2014-05-23 14:30:03 -0700

    Removed adding default values to RLMProperty objects

--- Realm/RLMObjectSchema.mm
+++ Realm/RLMObjectSchema.mm
@@ -20,7 +20,6 @@
 
 #import "RLMObjectSchema.h"
 #import "RLMUtil.h"
-#import "RLMObject.h"
 #import "RLMProperty_Private.h"
 #import <tightdb/table.hpp>
 
@@ -55,17 +54,11 @@
     unsigned int count;
     objc_property_t *props = class_copyPropertyList(objectClass, &count);
     
-    // create array of RLMProperties
-    NSDictionary *defaults = nil;
-    if ([objectClass respondsToSelector:@selector(defaultPropertyValues)]) {
-        defaults = [objectClass defaultPropertyValues];
-    }
-    
+    // create array of RLMProperties    
     NSMutableArray *propArray = [NSMutableArray arrayWithCapacity:count];
     for (unsigned int i = 0; i < count; i++) {
         RLMProperty *prop = [RLMProperty propertyForObjectProperty:props[i] column:propArray.count];
         if (prop) {
-            prop.defaultValue = defaults[prop.name];
             [propArray addObject:prop];
         }
     }
