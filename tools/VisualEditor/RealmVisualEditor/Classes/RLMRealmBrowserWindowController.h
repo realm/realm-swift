d8c7d3344f9dc6b5e20ce455f0eac9d5e206c008
Jesper Zuschlag <jesper@zuschlag.dk>
2014-06-02 16:18:42 +0200

    Adding skeleton NSWindowController

---
+++ tools/VisualEditor/RealmVisualEditor/Classes/RLMRealmBrowserWindowController.h
@@ -0,0 +1,13 @@
+//
+//  RLMRealmBrowserWindowController.h
+//  RealmVisualEditor
+//
+//  Created by Jesper Zuschlag on 02/06/14.
+//  Copyright (c) 2014 Realm inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RLMRealmBrowserWindowController : NSWindowController
+
+@end
---
+++ tools/VisualEditor/RealmVisualEditor/Classes/RLMRealmBrowserWindowController.m
@@ -0,0 +1,18 @@
+//
+//  RLMRealmBrowserWindowController.m
+//  RealmVisualEditor
+//
+//  Created by Jesper Zuschlag on 02/06/14.
+//  Copyright (c) 2014 Realm inc. All rights reserved.
+//
+
+#import "RLMRealmBrowserWindowController.h"
+
+@implementation RLMRealmBrowserWindowController
+
+- (void)windowDidLoad
+{
+
+}
+
+@end
