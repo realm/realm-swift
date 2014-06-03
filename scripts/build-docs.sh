41b37d15b69ac91431cece1ef21ba23e8906fa58
Tim Anglade <timanglade@gmail.com>
2014-06-03 14:17:17 -0700

    Fix doc generation

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -45,8 +45,8 @@ appledoc \
     --no-create-html \
     --create-docset \
     --publish-docset \
-    --docset-feed-url "http://realm.io/docs/ios/${realm_version}/realm.atom" \
-    --docset-package-url "http://realm.io/docs/ios/${realm_version}/realm" \
+    --docset-feed-url "http://realm.io/docs/ios/${realm_version}/api/realm.atom" \
+    --docset-package-url "http://realm.io/docs/ios/${realm_version}/api/realm" \
     --docset-package-filename "realm" \
     --docset-atom-filename "realm.atom" \
     --docset-bundle-filename "realm.docset" \
@@ -76,10 +76,11 @@ cat >${SRCROOT}/docs/output/${realm_version}/realm.xml <<EOF
 <entry>
     <version>${realm_version}</version>
     <sha1>$(sha1sum -b docs/output/${realm_version}/realm.tgz | cut -c 1-40)</sha1>
-    <url>http://static.realm.io/docs/ios/${realm_version}/realm.tgz</url>
+    <url>http://static.realm.io/docs/ios/${realm_version}/api/realm.tgz</url>
 </entry>
 EOF
 
 mv ${SRCROOT}/docs/output/${realm_version}/publish/* ${SRCROOT}/docs/output/${realm_version}/
 rm -rf ${SRCROOT}/docs/output/${realm_version}/publish/
-rm -rf ${SRCROOT}/docs/output/${realm_version}/realm.docset
+
+echo "Generated Docs under docs/output/${realm_version}/"
