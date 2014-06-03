1227d7404ac94b992de5051164ed00acc3da1eef
Tim Anglade <timanglade@gmail.com>
2014-06-03 16:54:03 -0700

    Clean up framework generation & tests

--- scripts/build-framework.sh
+++ scripts/build-framework.sh
@@ -7,12 +7,16 @@ SF_COMBINED_PATH="${BUILD_DIR}/${CONFIGURATION}/libRealm-combined.a"
 
 # very simple structure - it doesn't follow Apple's documentation
 /bin/rm -rf "${SF_FRAMEWORK_PATH}"
-/bin/mkdir -p "${SF_FRAMEWORK_PATH}/Headers"
+/bin/mkdir -p "${SF_FRAMEWORK_PATH}/Versions/A/Headers"
 
 # Step 1 - copy combined binary into framework
-xcrun cp ${SF_COMBINED_PATH} "${SF_FRAMEWORK_PATH}/${PRODUCT_NAME}"
+xcrun cp ${SF_COMBINED_PATH} "${SF_FRAMEWORK_PATH}/Versions/A/${PRODUCT_NAME}"
 
 # Step 2 - copy headers into framework
-xcrun cp -R "${BUILT_PRODUCTS_DIR}/include/Realm" "${SF_FRAMEWORK_PATH}/Headers"
+xcrun cp -R "${BUILT_PRODUCTS_DIR}/include/Realm" "${SF_FRAMEWORK_PATH}/Versions/A/Headers"
+
+/bin/ln -s "${SF_FRAMEWORK_PATH}/Versions/A/${PRODUCT_NAME}" "${SF_FRAMEWORK_PATH}/${PRODUCT_NAME}"
+/bin/ln -s "${SF_FRAMEWORK_PATH}/Versions/A/Headers" "${SF_FRAMEWORK_PATH}/Headers"
+
 
 
