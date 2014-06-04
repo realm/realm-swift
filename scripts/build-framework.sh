a4cc05879dddda2a329184b816eec70bfd772c58
Tim Anglade <timanglade@gmail.com>
2014-06-03 23:32:34 -0700

    Fix Product>Archive for iOS

--- scripts/build-framework.sh
+++ scripts/build-framework.sh
@@ -13,8 +13,11 @@ SF_COMBINED_PATH="${BUILD_DIR}/${CONFIGURATION}/libRealm-combined.a"
 xcrun cp ${SF_COMBINED_PATH} "${SF_FRAMEWORK_PATH}/${PRODUCT_NAME}"
 
 # Step 2 - copy headers into framework
-xcrun cp -R "${BUILT_PRODUCTS_DIR}/include/Realm" "${SF_FRAMEWORK_PATH}/Headers"
-
+if [[ "$CONFIGURATION" = "Debug" ]]; then
+	xcrun cp -R "${BUILT_PRODUCTS_DIR}/include/Realm" "${SF_FRAMEWORK_PATH}/Headers"
+else
+	xcrun cp -R "${BUILT_PRODUCTS_DIR}/../Release-iphonesimulator/include/Realm" "${SF_FRAMEWORK_PATH}/Headers"
+fi
 cd "${SF_FRAMEWORK_PATH}"
 /bin/mkdir -p "Versions/A/Headers"
 /bin/mv Headers/Realm/* Versions/A/Headers/
