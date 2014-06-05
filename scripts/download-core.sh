f0cd2695983f302f3495da0b4c22c21e5c711f99
Kenneth Geisshirt <kg@realm.io>
2014-06-05 12:58:46 +0200

    Readding a target for downloading core.

--- scripts/download-core.sh
+++ scripts/download-core.sh
@@ -1,5 +1,8 @@
+# Change version when new core is released
+REALM_CORE_VERSION=0.20.0
+
 if ! [ -d core ]; then
-    /usr/bin/curl -s http://static.realm.io/downloads/core/realm-core-${REALM_CORE_VERSION}.zip -o /tmp/core-${REALM_CORE_VERSION}.zip
+    /usr/bin/curl -s http://static.realm.io/downloads/core/core-${REALM_CORE_VERSION}.zip -o /tmp/core-${REALM_CORE_VERSION}.zip
     /bin/rm -rf ${SRCROOT}/core
     cd ${SRCROOT}
     /usr/bin/unzip /tmp/core-${REALM_CORE_VERSION}.zip
