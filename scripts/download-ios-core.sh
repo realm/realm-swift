7c6e1c341961032dbca8a4d0299bf489e98d8bb7
Ari Lazier <alazier@gmail.com>
2014-06-06 17:06:07 -0700

    set proper version

--- scripts/download-ios-core.sh
+++ scripts/download-ios-core.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Download and unpack core library
+REALM_CORE_VERSION=0.21.0
 
 if ! [ -d core ]; then
-	/usr/bin/curl -s http://static.realm.io/downloads/core/realm-core-${REALM_CORE_VERSION}.zip -o /tmp/core-${REALM_CORE_VERSION}.zip  
-	/bin/rm -rf ${SRCROOT}/core
+	/usr/bin/curl -s http://static.realm.io/downloads/core/realm-core-${REALM_CORE_VERSION}.zip -o /tmp/core-${REALM_CORE_VERSION}.zip || exit 1 
+	/bin/rm -rf ${SRCROOT}/core  || exit 1 
 	cd ${SRCROOT}
-	/usr/bin/unzip /tmp/core-${REALM_CORE_VERSION}.zip
-	 /bin/rm -f /tmp/core-${REALM_CORE_VERSION}.zip
-   	mv realm-core core
+	/usr/bin/unzip /tmp/core-${REALM_CORE_VERSION}.zip || exit 1 
+	 /bin/rm -f /tmp/core-${REALM_CORE_VERSION}.zip || exit 1 
+   	mv realm-core core || exit 1 
 fi
 
