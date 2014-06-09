ef176c8f7b68a59b101364b004c6896fd66f25fe
Jesper Zuschlag <jesper@zuschlag.dk>
2014-06-09 12:50:44 +0200

    Adding linked objects to the test database.

--- tools/VisualEditor/RealmVisualEditor/Classes/RLMApplicationDelegate.m
+++ tools/VisualEditor/RealmVisualEditor/Classes/RLMApplicationDelegate.m
@@ -43,18 +43,20 @@
 
 @property (nonatomic, readonly) NSInteger integerValue;
 @property (nonatomic, readonly) BOOL boolValue;
+@property (nonatomic, readonly) RealmTestClass1 *objectRefence;
 
-+ (instancetype)instanceWithInt:(NSInteger)integerValue bool:(BOOL)boolValue;
++ (instancetype)instanceWithInt:(NSInteger)integerValue bool:(BOOL)boolValue objectRef:(RealmTestClass1 *)objRef;
 
 @end
 
 @implementation RealmTestClass2
 
-+ (instancetype)instanceWithInt:(NSInteger)integerValue bool:(BOOL)boolValue
++ (instancetype)instanceWithInt:(NSInteger)integerValue bool:(BOOL)boolValue objectRef:(RealmTestClass1 *)objRef
 {
     RealmTestClass2 *result = [[RealmTestClass2 alloc] init];
     result->_integerValue = integerValue;
     result->_boolValue = boolValue;
+    result->_objectRefence = objRef;
     return result;
 }
 
@@ -91,24 +93,43 @@
     
     [realm beginWriteTransaction];
     
-    [realm addObject:[RealmTestClass1 instanceWithInt:10    bool:YES float:123.456 double:123456.789 string:@"ten"      date:[NSDate date]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:20    bool:NO  float:23.4561 double:987654.321 string:@"twenty"   date:[NSDate distantPast]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:30    bool:YES float:3.45612 double:1234.56789 string:@"thirty"   date:[NSDate distantFuture]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:40    bool:NO  float:.456123 double:9876.54321 string:@"fourty"   date:[[NSDate date] dateByAddingTimeInterval:-60.0 * 60.0 * 24.0 * 7.0]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:50    bool:YES float:654.321 double:123.456789 string:@"fifty"    date:[[NSDate date] dateByAddingTimeInterval:+60.0 * 60.0 * 24.0 * 7.0]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:60    bool:NO  float:6543.21 double:987.654321 string:@"sixty"    date:[[NSDate date] dateByAddingTimeInterval:-60.0 * 60.0 * 24.0 * 1.0]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:70    bool:YES float:65432.1 double:12.3456789 string:@"seventy"  date:[[NSDate date] dateByAddingTimeInterval:+60.0 * 60.0 * 24.0 * 1.0]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:80    bool:NO  float:654321. double:98.7654321 string:@"eighty"   date:[[NSDate date] dateByAddingTimeInterval:-60.0 * 60.0 * 12.0 * 1.0]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:90    bool:YES float:123.456 double:1.23456789 string:@"ninety"   date:[[NSDate date] dateByAddingTimeInterval:+60.0 * 60.0 * 12.0 * 1.0]]];
-    [realm addObject:[RealmTestClass1 instanceWithInt:100   bool:NO  float:123.456 double:9.87654321 string:@"hundred"  date:[[NSDate date] dateByAddingTimeInterval:+60.0 *  5.0 *  1.0 * 1.0]]];
+    RealmTestClass1 *tc1_0 = [RealmTestClass1 instanceWithInt:10    bool:YES float:123.456 double:123456.789 string:@"ten"      date:[NSDate date]];
+    RealmTestClass1 *tc1_1 = [RealmTestClass1 instanceWithInt:20    bool:NO  float:23.4561 double:987654.321 string:@"twenty"   date:[NSDate distantPast]];
+    RealmTestClass1 *tc1_2 = [RealmTestClass1 instanceWithInt:30    bool:YES float:3.45612 double:1234.56789 string:@"thirty"   date:[NSDate distantFuture]];
+    RealmTestClass1 *tc1_3 = [RealmTestClass1 instanceWithInt:40    bool:NO  float:.456123 double:9876.54321 string:@"fourty"   date:[[NSDate date] dateByAddingTimeInterval:-60.0 * 60.0 * 24.0 * 7.0]];
+    RealmTestClass1 *tc1_4 = [RealmTestClass1 instanceWithInt:50    bool:YES float:654.321 double:123.456789 string:@"fifty"    date:[[NSDate date] dateByAddingTimeInterval:+60.0 * 60.0 * 24.0 * 7.0]];
+    RealmTestClass1 *tc1_5 = [RealmTestClass1 instanceWithInt:60    bool:NO  float:6543.21 double:987.654321 string:@"sixty"    date:[[NSDate date] dateByAddingTimeInterval:-60.0 * 60.0 * 24.0 * 1.0]];
+    RealmTestClass1 *tc1_6 = [RealmTestClass1 instanceWithInt:70    bool:YES float:65432.1 double:12.3456789 string:@"seventy"  date:[[NSDate date] dateByAddingTimeInterval:+60.0 * 60.0 * 24.0 * 1.0]];
+    RealmTestClass1 *tc1_7 = [RealmTestClass1 instanceWithInt:80    bool:NO  float:654321. double:98.7654321 string:@"eighty"   date:[[NSDate date] dateByAddingTimeInterval:-60.0 * 60.0 * 12.0 * 1.0]];
+    RealmTestClass1 *tc1_8 = [RealmTestClass1 instanceWithInt:90    bool:YES float:123.456 double:1.23456789 string:@"ninety"   date:[[NSDate date] dateByAddingTimeInterval:+60.0 * 60.0 * 12.0 * 1.0]];
+    RealmTestClass1 *tc1_9 = [RealmTestClass1 instanceWithInt:100   bool:NO  float:123.456 double:9.87654321 string:@"hundred"  date:[[NSDate date] dateByAddingTimeInterval:+60.0 *  5.0 *  1.0 * 1.0]];
     
-    [realm addObject:[RealmTestClass2 instanceWithInt:1111  bool:YES]];
-    [realm addObject:[RealmTestClass2 instanceWithInt:2211  bool:YES]];
-    [realm addObject:[RealmTestClass2 instanceWithInt:3322  bool:YES]];
-    [realm addObject:[RealmTestClass2 instanceWithInt:4433  bool:NO]];
-    [realm addObject:[RealmTestClass2 instanceWithInt:5544  bool:YES]];
-    [realm addObject:[RealmTestClass2 instanceWithInt:6655  bool:YES]];
-    [realm addObject:[RealmTestClass2 instanceWithInt:7766  bool:NO]];
+    RealmTestClass2 *tc2_0 = [RealmTestClass2 instanceWithInt:1111  bool:YES objectRef:tc1_0];
+    RealmTestClass2 *tc2_1 = [RealmTestClass2 instanceWithInt:2211  bool:YES objectRef:tc1_2];
+    RealmTestClass2 *tc2_2 = [RealmTestClass2 instanceWithInt:3322  bool:YES objectRef:tc1_4];
+    RealmTestClass2 *tc2_3 = [RealmTestClass2 instanceWithInt:4433  bool:NO  objectRef:tc1_6];
+    RealmTestClass2 *tc2_4 = [RealmTestClass2 instanceWithInt:5544  bool:YES objectRef:tc1_8];
+    RealmTestClass2 *tc2_5 = [RealmTestClass2 instanceWithInt:6655  bool:YES objectRef:nil];
+    RealmTestClass2 *tc2_6 = [RealmTestClass2 instanceWithInt:7766  bool:NO  objectRef:tc1_0];
+    
+    [realm addObject:tc1_0];
+    [realm addObject:tc1_1];
+    [realm addObject:tc1_2];
+    [realm addObject:tc1_3];
+    [realm addObject:tc1_4];
+    [realm addObject:tc1_5];
+    [realm addObject:tc1_6];
+    [realm addObject:tc1_7];
+    [realm addObject:tc1_8];
+    [realm addObject:tc1_9];
+    
+    [realm addObject:tc2_0];
+    [realm addObject:tc2_1];
+    [realm addObject:tc2_2];
+    [realm addObject:tc2_3];
+    [realm addObject:tc2_4];
+    [realm addObject:tc2_5];
+    [realm addObject:tc2_6];
     
     [realm commitWriteTransaction];
 }
