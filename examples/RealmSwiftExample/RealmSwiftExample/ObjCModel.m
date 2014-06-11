94203252139cb7dbee0287d2be6d214265465c07
JP Simard <jp@jpsim.com>
2014-06-11 15:43:53 -0700

    added swift example

---
+++ examples/RealmSwiftExample/RealmSwiftExample/ObjCModel.m
@@ -0,0 +1,13 @@
+//
+//  ObjCModel.m
+//  RealmSwiftExample
+//
+//  Created by JP Simard on 6/10/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import "ObjCModel.h"
+
+@implementation ObjCModel
+
+@end
