13ad7bd17d695b91503b368ef901fdba204cf5b2
mekjaer <mk@tightdb.com>
2014-06-11 12:20:27 +0200

    added correct comment in header

--- examples/RealmSimpleExample/RealmSimpleExample/main.m
+++ examples/RealmSimpleExample/RealmSimpleExample/main.m
@@ -1,10 +1,22 @@
+////////////////////////////////////////////////////////////////////////////
 //
-//  main.m
-//  RealmSimpleExample
+// TIGHTDB CONFIDENTIAL
+// __________________
 //
-//  Created by Morten Kjaer on 10/06/14.
-//  Copyright (c) 2014 Tightdb Denmark ApS. All rights reserved.
+//  [2011] - [2014] TightDB Inc
+//  All Rights Reserved.
 //
+// NOTICE:  All information contained herein is, and remains
+// the property of TightDB Incorporated and its suppliers,
+// if any.  The intellectual and technical concepts contained
+// herein are proprietary to TightDB Incorporated
+// and its suppliers and may be covered by U.S. and Foreign Patents,
+// patents in process, and are protected by trade secret or copyright law.
+// Dissemination of this information or reproduction of this material
+// is strictly forbidden unless prior written permission is obtained
+// from TightDB Incorporated.
+//
+////////////////////////////////////////////////////////////////////////////
 
 #import <UIKit/UIKit.h>
 
