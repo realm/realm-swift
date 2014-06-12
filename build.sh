7d3373ff7ea860f0ca224a6f305da630f1e0c140
Kenneth Geisshirt <kg@realm.io>
2014-06-12 12:38:55 +0200

    Minor updates - instead of just commenting a pull request

--- build.sh
+++ build.sh
@@ -1,45 +1,56 @@
 #!/bin/sh
+PATH=/usr/local/bin:/usr/bin:/bin
 
-# Usage: sh build.sh [command] [xcmode]
-# 
-#  command (required):
-#   download_core: downloads tightdb core
-#   ios: builds iOS framework with debug configuration
-#   osx: builds OSX framework with debug configuration
-#   test-ios: builds and tests iOS framework with debug configuration
-#   test-osx: builds and tests OSX framework with debug configuration
-#   docs: builds docs in docs/output
-#   examples: builds all examples in examples/
-#   verify: cleans core/ and docs/output/, then runs docs, test-all and examples
-# 
-#  xcmode (optional): xcodebuild (default), xcpretty or xctool
+usage() {
+cat <<EOF
+Usage: sh $0 command [xcmode]
+
+command:
+  download_core: downloads tightdb core
+  ios: builds iOS framework with debug configuration
+  osx: builds OSX framework with debug configuration
+  test-ios: builds and tests iOS framework with debug configuration
+  test-osx: builds and tests OSX framework with debug configuration
+  docs: builds docs in docs/output
+  examples: builds all examples in examples/
+  verify: cleans core/ and docs/output/, then runs docs, test-all and examples
+
+xcmode (optional): xcodebuild (default), xcpretty or xctool
+EOF
+}
 
 ######################################
 # Variables
 ######################################
-
-PATH=/usr/local/bin:/usr/bin:$PATH
 REALM_CORE_VERSION=latest
-COMMAND=$1
-XCMODE=$2
+COMMAND="$1"
+XCMODE="$2"
 : ${XCMODE:=xcodebuild} # must be one of: xcodebuild (default), xcpretty, xctool
+
+######################################
+# Validate input
+######################################
+if [ -z "$COMMAND" ]; then
+    usage
+    exit 1
+fi
 
 ######################################
 # Xcode Helpers
 ######################################
 
-xc(){
-	if [[ "$XCMODE" == "xcodebuild" ]]; then
-		xcodebuild $1
-	elif [[ "$XCMODE" == "xcpretty" ]]; then
-		xcodebuild $1 | xcpretty
-	elif [[ "$XCMODE" == "xctool" ]]; then
-		xctool $1
-	fi
+xc() {
+        if [[ "$XCMODE" == "xcodebuild" ]]; then
+                xcodebuild $1 || exit 1
+        elif [[ "$XCMODE" == "xcpretty" ]]; then
+                xcodebuild $1 | xcpretty || exit 1
+        elif [[ "$XCMODE" == "xctool" ]]; then
+                xctool $1 || exit 1
+        fi
 }
 
-xcrealm(){
-	xc "-project Realm.xcodeproj $1"
+xcrealm() {
+        xc "-project Realm.xcodeproj $1"
 }
 
 ######################################
@@ -47,11 +58,11 @@ xcrealm(){
 ######################################
 
 if [[ "$COMMAND" == "download_core" ]]; then
-	if ! [ -d core ]; then
-	    curl -s http://static.realm.io/downloads/core/realm-core-${REALM_CORE_VERSION}.zip -o /tmp/core-${REALM_CORE_VERSION}.zip
-	    unzip /tmp/core-${REALM_CORE_VERSION}.zip
-	    rm -f /tmp/core-${REALM_CORE_VERSION}.zip
-	fi
+        if ! [ -d core ]; then
+            curl -s "http://static.realm.io/downloads/core/realm-core-${REALM_CORE_VERSION}.zip" -o "/tmp/core-${REALM_CORE_VERSION}.zip" || exit 1
+            unzip "/tmp/core-${REALM_CORE_VERSION}.zip" || exit 1
+            rm -f "/tmp/core-${REALM_CORE_VERSION}.zip" || exit 1
+        fi
 fi
 
 ######################################
@@ -59,11 +70,11 @@ fi
 ######################################
 
 if [[ "$COMMAND" == "ios" ]]; then
-	xcrealm "-scheme iOS"
+        xcrealm "-scheme iOS"
 fi
 
 if [[ "$COMMAND" == "osx" ]]; then
-	xcrealm "-scheme OSX"
+        xcrealm "-scheme OSX"
 fi
 
 ######################################
@@ -71,24 +82,24 @@ fi
 ######################################
 
 if [[ "$COMMAND" == "test-ios" ]]; then
-	xcrealm "-scheme iOS -sdk iphonesimulator test"
+        xcrealm "-scheme iOS -sdk iphonesimulator test"
 fi
 
 if [[ "$COMMAND" == "test-osx" ]]; then
-	xcrealm "-scheme OSX test"
+        xcrealm "-scheme OSX test"
 fi
 
 if [[ "$COMMAND" == "test-all" ]]; then
-	xcrealm "-scheme iOS -configuration Debug -sdk iphonesimulator clean test"
-	xcrealm "-scheme iOS -configuration Release -sdk iphonesimulator clean test"
-	xcrealm "-scheme OSX -configuration Debug clean test"
-	xcrealm "-scheme OSX -configuration Release clean test"
+        xcrealm "-scheme iOS -configuration Debug -sdk iphonesimulator clean test"
+        xcrealm "-scheme iOS -configuration Release -sdk iphonesimulator clean test"
+        xcrealm "-scheme OSX -configuration Debug clean test"
+        xcrealm "-scheme OSX -configuration Release clean test"
 fi
 
 if [[ "$COMMAND" == "verify" ]]; then
-	sh build.sh docs
-	sh build.sh test-all $2
-	sh build.sh examples $2
+        sh build.sh docs
+        sh build.sh test-all "$2"
+        sh build.sh examples "$2"
 fi
 
 ######################################
@@ -96,7 +107,7 @@ fi
 ######################################
 
 if [[ "$COMMAND" == "docs" ]]; then
-	sh scripts/build-docs.sh
+        sh scripts/build-docs.sh || exit 1
 fi
 
 ######################################
@@ -104,8 +115,10 @@ fi
 ######################################
 
 if [[ "$COMMAND" == "examples" ]]; then
-	cd examples
-	xc "-project RealmTableViewExample/RealmTableViewExample.xcodeproj -scheme RealmTableViewExample clean build"
-	xc "-project RealmSimpleExample/RealmSimpleExample.xcodeproj -scheme RealmSimpleExample clean build"
+    (
+        cd examples
+        xc "-project RealmTableViewExample/RealmTableViewExample.xcodeproj -scheme RealmTableViewExample clean build"
+        xc "-project RealmSimpleExample/RealmSimpleExample.xcodeproj -scheme RealmSimpleExample clean build"
 	xc "-project RealmPerformanceExample/RealmPerformanceExample.xcodeproj -scheme RealmPerformanceExample clean build"
+    )
 fi
