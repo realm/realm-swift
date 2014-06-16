5beea61627cc209a872d5e5eb8a8de22fc68d360
bmunkholm <bmunkholm@users.noreply.github.com>
2014-06-16 20:45:53 +0200

    Update Realm.h

--- Realm/Realm.h
+++ Realm/Realm.h
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////
 //
-// Copyright 2014 Realm.io
+// Copyright 2014 Realm Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
