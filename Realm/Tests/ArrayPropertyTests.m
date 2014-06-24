4406bae2d7315be5303343d4b7705781febf06c3
JP Simard <jp@jpsim.com>
2014-06-24 14:07:11 -0700

    fixed merge issue

--- Realm/Tests/ArrayPropertyTests.m
+++ Realm/Tests/ArrayPropertyTests.m
@@ -114,7 +114,7 @@
     XCTAssertEqualObjects([array.array[0] stringCol], @"a", @"First element should have property value 'a'");
     XCTAssertEqualObjects([array.array[1] stringCol], @"a", @"Second element should have property value 'a'");
     
-    IntArrayPropertyObject *intArray = [[IntArrayPropertyObject alloc] init];
+    ArrayPropertyObject *intArray = [[ArrayPropertyObject alloc] init];
     IntObject *intObj = [[IntObject alloc] init];
     intObj.intCol = 1;
     [intArray.array addObject:intObj];
