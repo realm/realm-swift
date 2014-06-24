e578b20ed57e0c468621593c9c8c8483bfc7489b
mekjaer <mk@tightdb.com>
2014-06-24 15:49:57 +0200

    simple example in swift added

---
+++ examples/RealmSwiftSimpleExample/RealmSwiftSimpleExample/AppDelegate.swift
@@ -0,0 +1,86 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2014 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+import UIKit
+import Realm
+
+class Dog: RLMObject {
+    var name = ""
+    var age = 0
+}
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // Override point for customization after application launch.
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.makeKeyAndVisible()
+        let rootVC = UIViewController()
+        self.window!.rootViewController = rootVC
+        
+        deleteRealmFile()
+        
+        // Create a standalone object
+        var mydog = Dog()
+        
+        // Set & read properties
+        mydog.name = "Rex";
+        mydog.age = 9;
+        println("Name of dog: \(mydog.name)")
+        
+        // Realms are used to group data together
+        let realm = RLMRealm.defaultRealm() // Create realm pointing to default file
+        
+        // Save your object
+        realm.beginWriteTransaction()
+        realm.addObject(mydog)
+        realm.commitWriteTransaction()
+        
+        // Query
+        var results = realm.objects(Dog.className(), withPredicate: NSPredicate(format: "name contains 'x'"))
+        
+        // Queries are chainable!
+        var results2 = results.objectsWithPredicate(NSPredicate(format: "age > 8"))
+        println("Number of dogs: \(results.count)")
+
+        // Thread-safety
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,	0),	{
+            let otherRealm = RLMRealm.defaultRealm()
+            var otherResults = otherRealm.objects(Dog.className(), withPredicate:NSPredicate(format:"name contains 'Rex'"))
+            println("Number of dogs \(otherResults.count)")
+            });
+        
+        return true
+    }
+    
+    
+    func deleteRealmFile() {
+
+        let documentsPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+
+        var path = documentsPaths.stringByAppendingPathComponent("default.realm")
+        NSFileManager.defaultManager().removeItemAtPath(path, error: nil)
+    }
+}
+
---
+++ examples/RealmSwiftSimpleExample/RealmSwiftSimpleExampleTests/RealmSwiftSimpleExampleTests.swift
@@ -0,0 +1,35 @@
+//
+//  RealmSwiftSimpleExampleTests.swift
+//  RealmSwiftSimpleExampleTests
+//
+//  Created by Morten Kjaer on 24/06/14.
+//  Copyright (c) 2014 Tightdb Denmark ApS. All rights reserved.
+//
+
+import XCTest
+
+class RealmSwiftSimpleExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
