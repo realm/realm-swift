50dcca58150248625e91b159ae77aa2c016a8233
JP Simard <jp@jpsim.com>
2014-06-25 09:43:52 -0700

    added Swift example. uses objc model

---
+++ examples/swift/RealmSwiftTableViewExample/RealmSwiftTableViewExample/DemoObject.m
@@ -0,0 +1,27 @@
+//
+//  DemoObject.m
+//  RealmSwiftTableViewExample
+//
+//  Created by JP Simard on 6/25/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import "DemoObject.h"
+
+@implementation DemoObject
+
+// Specify default values for properties
+
+//+ (NSDictionary *)defaultPropertyValues
+//{
+//    return @{};
+//}
+
+// Specify properties to ignore (Realm won't persist these)
+
+//+ (NSArray *)ignoredProperties
+//{
+//    return @[];
+//}
+
+@end
