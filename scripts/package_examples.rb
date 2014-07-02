8ca15f3195dd9e40ea2737042025e0180b561e07
JP Simard <jp@jpsim.com>
2014-07-02 15:10:21 -0700

    Added scripts/package_examples.rb and updated RealmSwiftTableViewExample to support it

---
+++ scripts/package_examples.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+require 'xcodeproj'
+
+##########################
+# Example Projects
+##########################
+
+class ExampleProject
+  attr_accessor :path, :uuids_to_remove
+end
+
+def example_projects
+  # RealmSwiftTableViewExample
+  realmSwiftTableViewExample = ExampleProject.new
+  realmSwiftTableViewExample.path = "examples/swift/RealmSwiftTableViewExample/RealmSwiftTableViewExample.xcodeproj"
+  realmSwiftTableViewExample.uuids_to_remove = [
+    "E8C5DD58195025B50055C3B8",
+    "E870C959195B32A300163667",
+    "E870C95A195B32A300163667",
+    "E870C951195B328E00163667",
+    "E870C952195B328E00163667",
+    "E870C953195B328E00163667",
+    "E870C954195B328E00163667",
+    "E870C955195B328E00163667",
+    "E870C956195B328E00163667",
+    "E870C957195B328E00163667",
+    "E870C958195B328E00163667",
+    "E870C94B195B328E00163667"
+  ]
+
+  # Return all example projects
+  [
+    realmSwiftTableViewExample
+  ]
+end
+
+##########################
+# Functions
+##########################
+
+def remove_uuids_from_example(example)
+  project = Xcodeproj::Project.open(example.path)
+
+  objects_to_remove = project.objects.select { |o| example.uuids_to_remove.include?(o.uuid) }
+
+  objects_to_remove.each do |object|
+    object.remove_from_project
+  end
+
+  project.save
+end
+
+def add_framework_file_reference(example)
+  project = Xcodeproj::Project.open(example.path)
+  
+  obj = project.new(Xcodeproj::Project::PBXFileReference)
+  obj.path = "../../Realm.framework"
+  obj.name = "Realm.framework"
+  project.main_group << obj
+
+  project.save
+end
+
+def add_framework(example)
+  project = Xcodeproj::Project.open(example.path)
+  
+  # Add file reference
+  obj = project.new(Xcodeproj::Project::PBXFileReference)
+  obj.path = "../../Realm.framework"
+  obj.name = "Realm.framework"
+  project.main_group << obj
+
+  target = project.targets.first
+
+  # Add to copy build phase
+  target.resources_build_phase.add_file_reference(obj)
+
+  # Add to frameworks build phase
+  target.frameworks_build_phase.add_file_reference(obj)
+
+  project.save
+end
+
+##########################
+# Script
+##########################
+
+example_projects.each do |example|
+  remove_uuids_from_example(example)
+  add_framework(example)
+end
