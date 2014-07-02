4256374d339eec8c71491a7a852bb2e1b1aa1533
JP Simard <jp@jpsim.com>
2014-07-02 15:22:12 -0700

    now packaging RealmSwiftSimpleExample

--- scripts/package_examples.rb
+++ scripts/package_examples.rb
@@ -29,26 +29,27 @@ def example_projects
   ]
 
   # RealmSwiftTableViewExample
-  realmSwiftTableViewExample = ExampleProject.new
-  realmSwiftTableViewExample.path = "examples/swift/RealmSwiftTableViewExample/RealmSwiftTableViewExample.xcodeproj"
-  realmSwiftTableViewExample.uuids_to_remove = [
-    "E8C5DD58195025B50055C3B8",
-    "E870C959195B32A300163667",
-    "E870C95A195B32A300163667",
-    "E870C951195B328E00163667",
-    "E870C952195B328E00163667",
-    "E870C953195B328E00163667",
-    "E870C954195B328E00163667",
-    "E870C955195B328E00163667",
-    "E870C956195B328E00163667",
-    "E870C957195B328E00163667",
-    "E870C958195B328E00163667",
-    "E870C94B195B328E00163667"
+  realmSwiftSimpleExample = ExampleProject.new
+  realmSwiftSimpleExample.path = "examples/swift/RealmSwiftSimpleExample/RealmSwiftSimpleExample.xcodeproj"
+  realmSwiftSimpleExample.uuids_to_remove = [
+    "4D1E47A8195C1BB40005280D",
+    "4D1E47AA195C1BB40005280D",
+    "4D1E47AC195C1BB40005280D",
+    "4D1E47AE195C1BB40005280D",
+    "4D1E47B0195C1BC00005280D",
+    "4D1E47A1195C1BB40005280D",
+    "4D1E47A2195C1BB40005280D",
+    "4D1E47A9195C1BB40005280D",
+    "4D1E47AB195C1BB40005280D",
+    "4D1E47AD195C1BB40005280D",
+    "4D1E47AF195C1BB40005280D",
+    "4D1E47B1195C1BC00005280D"
   ]
 
   # Return all example projects
   [
-    realmSwiftTableViewExample
+    realmSwiftTableViewExample,
+    realmSwiftSimpleExample
   ]
 end
 
