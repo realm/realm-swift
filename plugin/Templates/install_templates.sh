2743b4e3515f5554b9f6103219e93e339e4b4568
JP Simard <jp@jpsim.com>
2014-07-16 16:44:00 -0700

    - file/class templates are now installed for both iOS/OSX - fixed improper usage of RLM_OBJECT_PROTOCOL in docs

--- plugin/Templates/install_templates.sh
+++ plugin/Templates/install_templates.sh
@@ -8,29 +8,34 @@ mkdir -p "$FILE_TEMPLATES_DIR"
 
 for dir in "file_templates/*/"
 do
-    cp -R ${dir%*/} "$FILE_TEMPLATES_DIR"
+  cp -R ${dir%*/} "$FILE_TEMPLATES_DIR"
 done
 
 # Class Templates
 
 XCODE_DIR=$(xcode-select -p)
-OBJC_CLASS_TEMPLATES_DIR="$XCODE_DIR/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File Templates/Cocoa Touch/Objective-C class.xctemplate"
-TEMPLATE_INFO_PLIST_PATH="$OBJC_CLASS_TEMPLATES_DIR/TemplateInfo.plist"
+IOS_CLASS_TEMPLATES_DIR="$XCODE_DIR/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File Templates/Cocoa Touch/Objective-C class.xctemplate"
+OSX_CLASS_TEMPLATES_DIR="$XCODE_DIR/Library/Xcode/Templates/File Templates/Cocoa/Objective-C class.xctemplate"
 
 for dir in "class_templates/*/"
 do
-    cp -R ${dir%*/} "$OBJC_CLASS_TEMPLATES_DIR"
+  for classTemplatesDir in "$IOS_CLASS_TEMPLATES_DIR" "$OSX_CLASS_TEMPLATES_DIR"
+  do
+    mkdir -p "$classTemplatesDir"
+    cp -R ${dir%*/} "$classTemplatesDir"
 
     PLIST_BUDDY=/usr/libexec/PlistBuddy
     class=$(basename $dir)
 
-    echo "Installing '$class' class template"
+    echo "Installing '$class' class template in '$classTemplatesDir'"
 
-    $PLIST_BUDDY -c "Print :Options:1:Values:" "$TEMPLATE_INFO_PLIST_PATH" | grep $class >/dev/null
+    INFO_PLIST_PATH="$classTemplatesDir/TemplateInfo.plist"
+    $PLIST_BUDDY -c "Print :Options:1:Values:" "$INFO_PLIST_PATH" | grep $class >/dev/null
     rc=$?
-	if [[ $rc != 0 ]] ; then
-    	$PLIST_BUDDY -c "Add :Options:1:Values: string '$class'" "$TEMPLATE_INFO_PLIST_PATH"
+    if [[ $rc != 0 ]] ; then
+      $PLIST_BUDDY -c "Add :Options:1:Values: string '$class'" "$INFO_PLIST_PATH"
     fi
+  done
 done
 
 echo "Installed templates"
