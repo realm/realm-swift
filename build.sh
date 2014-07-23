d9e60f01385efcff6267c6a524a1758dde600af1
JP Simard <jp@jpsim.com>
2014-07-22 17:38:15 -0700

    revert to custom derived data

--- build.sh
+++ build.sh
@@ -67,12 +67,12 @@ fi
 
 xcode5() {
     ln -s /Applications/Xcode.app/Contents/Developer/usr/bin build/bin || exit 1
-    PATH=./build/bin:$PATH xcodebuild $@
+    PATH=./build/bin:$PATH xcodebuild -IDECustomDerivedDataLocation=build/DerivedData $@
 }
 
 xcode6() {
     ln -s /Applications/Xcode6-Beta3.app/Contents/Developer/usr/bin build/bin || exit 1
-    PATH=./build/bin:$PATH xcodebuild $@
+    PATH=./build/bin:$PATH xcodebuild -IDECustomDerivedDataLocation=build/DerivedData $@
 }
 
 xcode() {
