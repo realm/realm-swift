51355b5fa7bf24901ce333aa27839135091754cd
Thomas Goyne <tg@realm.io>
2014-08-05 14:57:48 -0700

    Build with Xcode6 beta 5

--- build.sh
+++ build.sh
@@ -71,7 +71,7 @@ xcode5() {
 }
 
 xcode6() {
-    ln -s /Applications/Xcode6-Beta4.app/Contents/Developer/usr/bin build/bin || exit 1
+    ln -s /Applications/Xcode6-Beta5.app/Contents/Developer/usr/bin build/bin || exit 1
     PATH=./build/bin:$PATH xcodebuild -IDECustomDerivedDataLocation=build/DerivedData $@
 }
 
