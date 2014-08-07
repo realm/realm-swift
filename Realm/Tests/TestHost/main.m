661a505a12a40d9f5dffde2171d5456257e748dc
Thomas Goyne <tg@realm.io>
2014-08-06 18:28:48 -0700

    Add support for running Objective-C tests on devices
    Swift tests are exploding horribly on iOS 7.0, and I have been unable to
    get Xcode to actually deploy working apps to iOS 8.0, so no swift
    testing on devices for now.

---
+++ Realm/Tests/TestHost/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestHost
+//
+//  Created by Thomas Goyne on 8/6/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+@end
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
