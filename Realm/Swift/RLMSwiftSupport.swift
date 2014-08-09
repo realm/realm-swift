ae4034e3401fd758b88d0a6f5aed5dc57a70b37f
Alec Larson <alec.stanford.larson@gmail.com>
2014-08-08 21:47:27 -0700

    Update RLMSwiftSupport.swift
    I was getting an error `Ambiguous use of NSNotFound`. My change fixes this!
    
    Cheers! :v:

--- Realm/Swift/RLMSwiftSupport.swift
+++ Realm/Swift/RLMSwiftSupport.swift
@@ -21,7 +21,7 @@ import Foundation
 @objc public class RLMSwiftSupport {
 
     public class func isSwiftClassName(className: NSString) -> Bool {
-        return className.rangeOfString(".").location != NSNotFound
+        return className.rangeOfString(".").location != Foundation.NSNotFound
     }
 
     public class func demangleClassName(className: NSString) -> NSString {
