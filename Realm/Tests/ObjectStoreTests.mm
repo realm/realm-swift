799c6760f2bda316d300be49245be98afcdbd918
Oleksandr Shturmov <os@realm.io>
2014-08-12 08:16:42 +0200

    Remove comment of unused method

---
+++ Realm/Tests/ObjectStoreTests.mm
@@ -0,0 +1,13 @@
+//
+//  ObjectStoreTests.m
+//  Realm
+//
+//  Created by Oleksandr Shturmov on 06/08/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import "ObjectStoreTests.h"
+
+@implementation ObjectStoreTests
+
+@end
