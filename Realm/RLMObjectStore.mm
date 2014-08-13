c4d5a913c7e5adab90ba94de28e251193806bb46
Kristian Spangsege <kristian.spangsege@gmail.com>
2014-08-13 13:54:05 +0200

    Updates necessitated by change core Group API (https://github.com/Tightdb/tightdb/pull/515)

--- Realm/RLMObjectStore.mm
+++ Realm/RLMObjectStore.mm
@@ -34,12 +34,12 @@ static inline tightdb::TableRef RLMTableForObjectClass(RLMRealm *realm,
                                                        NSString *className,
                                                        bool &created) {
     NSString *tableName = realm.schema.tableNamesForClass[className];
-    return realm.group->get_table(tableName.UTF8String, created);
+    return realm.group->get_or_add_table(tableName.UTF8String, &created);
 }
 static inline tightdb::TableRef RLMTableForObjectClass(RLMRealm *realm,
                                                        NSString *className) {
     NSString *tableName = realm.schema.tableNamesForClass[className];
-    return realm.group->get_table(tableName.UTF8String);
+    return realm.group->get_or_add_table(tableName.UTF8String);
 }
 
 
