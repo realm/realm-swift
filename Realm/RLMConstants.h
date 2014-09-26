9c24e12e6aea0b618184d281e869fa5e3bd0ca97
JP Simard <jp@jpsim.com>
2014-09-26 11:08:30 -0700

    integer property indexes don't work currently

--- Realm/RLMConstants.h
+++ Realm/RLMConstants.h
@@ -22,7 +22,7 @@
 
 typedef NS_OPTIONS(NSUInteger, RLMPropertyAttributes) {
 /**
- Create an index for this property for improved search performance. Only integer and strings properties
+ Create an index for this property for improved search performance. Only string properties
  can be indexed. Returning this for properties of any other type will have no effect.
  */
     RLMPropertyAttributeIndexed = 1 << 2,
