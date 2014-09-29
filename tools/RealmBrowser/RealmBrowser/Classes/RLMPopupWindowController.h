e2583669adf00b7c49cf47e179f9f1fc1919272c
Gustaf Kugelberg <kugel@kth.se>
2014-09-29 10:37:00 +0200

    Added popup wc

---
+++ tools/RealmBrowser/RealmBrowser/Classes/RLMPopupWindowController.h
@@ -0,0 +1,13 @@
+//
+//  RLMPopupWindowController.h
+//  RealmBrowser
+//
+//  Created by Gustaf Kugelberg on 26/09/14.
+//  Copyright (c) 2014 Realm inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RLMPopupWindowController : NSWindowController
+
+@end
---
+++ tools/RealmBrowser/RealmBrowser/Classes/RLMPopupWindowController.m
@@ -0,0 +1,23 @@
+//
+//  RLMPopupWindowController.m
+//  RealmBrowser
+//
+//  Created by Gustaf Kugelberg on 26/09/14.
+//  Copyright (c) 2014 Realm inc. All rights reserved.
+//
+
+#import "RLMPopupWindowController.h"
+
+@interface RLMPopupWindowController ()
+
+@end
+
+@implementation RLMPopupWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
