690068a69f8548097e36d9d4c921a8b178d69b24
JP Simard <jp@jpsim.com>
2014-10-01 14:33:09 -0700

    added Swift source files

---
+++ Realm/Swift/RealmSwift.h
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2014 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmSwift.
+FOUNDATION_EXPORT double RealmSwiftVersionNumber;
+
+//! Project version string for RealmSwift.
+FOUNDATION_EXPORT const unsigned char RealmSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmSwift/PublicHeader.h>
+
+
