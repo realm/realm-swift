ul7290/realm-cocoa

Implement generic List properties

Realm/RLMProperty.mm
@@ -267,6 +267,26 @@
     return self;
 }
 
+- (instancetype)initSwiftListPropertyWithName:(NSString *)name
+                                         ivar:(Ivar)ivar
+                              objectClassName:(NSString *)objectClassName
+{
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+
+    _name = name;
+    _type = RLMPropertyTypeArray;
+    _objectClassName = objectClassName;
+    _objcType = 't';
+    _ivar = ivar;
+
+    // no obj-c property for generic lists, and thus no getter/setter names
+
+    return self;
+}
+
 
 -(BOOL)isEqualToProperty:(RLMProperty *)prop {
     return [_name isEqualToString:prop.name] && _type == prop.type && prop.isPrimary == _isPrimary &&

Realm/RLMProperty_Private.h
@@ -31,21 +31,26 @@
                                  property:(objc_property_t)property
                                  instance:(RLMObject *)objectInstance;
 
+- (instancetype)initSwiftListPropertyWithName:(NSString *)name
+                                         ivar:(Ivar)ivar
+                              objectClassName:(NSString *)objectClassName;
+
 // private setters
 @property (nonatomic, assign) NSUInteger column;
 @property (nonatomic, readwrite, assign) RLMPropertyType type;
+@property (nonatomic, copy) NSString *objectClassName;
 
 // private properties
 @property (nonatomic, copy) NSString *objcRawType;
 @property (nonatomic, assign) char objcType;
+@property (nonatomic, assign) BOOL isPrimary;
+@property (nonatomic, assign) Ivar ivar;
 
 // getter and setter names
 @property (nonatomic, copy) NSString *getterName;
 @property (nonatomic, copy) NSString *setterName;
 @property (nonatomic) SEL getterSel;
 @property (nonatomic) SEL setterSel;
-@property (nonatomic, copy) NSString *objectClassName;
-@property (nonatomic, assign) BOOL isPrimary;
 
 @end
 

realm/realm-swift

Realm/RLMProperty.mm
@@ -274,6 +274,26 @@
     return self;
 }
 
+- (instancetype)initSwiftListPropertyWithName:(NSString *)name
+                                         ivar:(Ivar)ivar
+                              objectClassName:(NSString *)objectClassName
+{
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+
+    _name = name;
+    _type = RLMPropertyTypeArray;
+    _objectClassName = objectClassName;
+    _objcType = 't';
+    _swiftListIvar = ivar;
+
+    // no obj-c property for generic lists, and thus no getter/setter names
+
+    return self;
+}
+
 
 -(BOOL)isEqualToProperty:(RLMProperty *)prop {
     return [_name isEqualToString:prop.name] && _type == prop.type && prop.isPrimary == _isPrimary &&

Realm/RLMProperty_Private.h
@@ -31,22 +31,27 @@
                                  property:(objc_property_t)property
                                  instance:(RLMObject *)objectInstance;
 
+- (instancetype)initSwiftListPropertyWithName:(NSString *)name
+                                         ivar:(Ivar)ivar
+                              objectClassName:(NSString *)objectClassName;
+
 // private setters
 @property (nonatomic, assign) NSUInteger column;
 @property (nonatomic, readwrite, assign) RLMPropertyType type;
 @property (nonatomic, readwrite) RLMPropertyAttributes attributes;
+@property (nonatomic, copy) NSString *objectClassName;
 
 // private properties
 @property (nonatomic, copy) NSString *objcRawType;
 @property (nonatomic, assign) char objcType;
+@property (nonatomic, assign) BOOL isPrimary;
+@property (nonatomic, assign) Ivar swiftListIvar;
 
 // getter and setter names
 @property (nonatomic, copy) NSString *getterName;
 @property (nonatomic, copy) NSString *setterName;
 @property (nonatomic) SEL getterSel;
 @property (nonatomic) SEL setterSel;
-@property (nonatomic, copy) NSString *objectClassName;
-@property (nonatomic, assign) BOOL isPrimary;
 
 @end
 