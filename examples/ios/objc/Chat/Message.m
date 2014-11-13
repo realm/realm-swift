17b7b73232e3c3afa1af86bd1666ef2c10bec558
JP Simard <jp@jpsim.com>
2014-11-12 18:17:42 -0800

    moved SyncedRealmExample to examples/ios/objc/RealmExamples.xcodeproj

---
+++ examples/ios/objc/Chat/Message.m
@@ -0,0 +1,13 @@
+//
+//  Message.m
+//  RealmExamples
+//
+//  Created by JP Simard on 11/12/14.
+//  Copyright (c) 2014 Realm. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+
+@end
---
+++ examples/ios/objc/Chat/UIColor+ChatColors.m
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2014 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+#import "UIColor+ChatColors.h"
+
+@implementation UIColor (ChatColors)
+
++ (UIColor *)pinkColor
+{
+    return [UIColor colorWithRed:253.0/255.0 green:183.0/255.0 blue:186.0/255.0 alpha:255.0/255.0];
+}
+
+@end
