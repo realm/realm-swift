c632c49b68d55697d9368dc73f4fc36068c20fb0
Matthew Robinson <matt@zensunni.org>
2014-12-12 12:01:01 +0800

    Moved installation of file templates from privileged script to a new build phase

--- plugin/Templates/install_templates.sh
+++ plugin/Templates/install_templates.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 PATH=/bin:/usr/bin:/usr/libexec
 
-# User File Templates
-
-FILE_TEMPLATES_DIR="$HOME/Library/Developer/Xcode/Templates/File Templates/Realm"
-mkdir -p "$FILE_TEMPLATES_DIR"
-
-for dir in "file_templates/*/"
-do
-  cp -R ${dir%*/} "$FILE_TEMPLATES_DIR"
-done
-
 # Class Templates
 
 XCODE_DIR=$(xcode-select -p)
