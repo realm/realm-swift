b975819d383df8ceb4a654e6946b8ac4d6bc7fd0
Thomas Goyne <tg@realm.io>
2015-01-02 10:34:20 -0800

    Fix some overly long object lifetimes
    Delay autoreleasing realms until after all validation is done, and avoid
    autoreleasing realms involved in migrations at all.

--- Realm/RLMMigration.mm
+++ Realm/RLMMigration.mm
@@ -47,22 +47,22 @@
 
 @implementation RLMMigration
 
-+ (instancetype)migrationForRealm:(RLMRealm *)realm key:(NSData *)key error:(NSError **)error {
-    RLMMigration *migration = [RLMMigration new];
-    
-    // create rw realm to migrate with current on disk table
-    migration->_realm = realm;
-    
-    // create read only realm used during migration with current on disk schema
-    migration->_oldRealm = [[RLMMigrationRealm alloc] initWithPath:realm.path key:key readOnly:NO inMemory:NO error:error];
-    if (migration->_oldRealm) {
-        RLMRealmSetSchema(migration->_oldRealm, [RLMSchema dynamicSchemaFromRealm:migration->_oldRealm], true);
+- (instancetype)initWithRealm:(RLMRealm *)realm key:(NSData *)key error:(NSError **)error {
+    self = [super init];
+    if (self) {
+        // create rw realm to migrate with current on disk table
+        _realm = realm;
+
+        // create read only realm used during migration with current on disk schema
+        _oldRealm = [[RLMMigrationRealm alloc] initWithPath:realm.path key:key readOnly:NO inMemory:NO error:error];
+        if (_oldRealm) {
+            RLMRealmSetSchema(_oldRealm, [RLMSchema dynamicSchemaFromRealm:_oldRealm], true);
+        }
+        if (error && *error) {
+            return nil;
+        }
     }
-    if (error && *error) {
-        return nil;
-    }
-    
-    return migration;
+    return self;
 }
 
 - (RLMSchema *)oldSchema {
