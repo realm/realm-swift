0c9f4175fb2b32a414c5335a9cd339373a99762a
Ari Lazier <alazier@gmail.com>
2015-01-08 15:54:39 -0800

    private Realm.Private module

--- Realm/RLMObjectStore.h
+++ Realm/RLMObjectStore.h
@@ -90,8 +90,9 @@ RLMObjectBase *RLMCreateObjectInRealmWithValue(RLMRealm *realm, NSString *classN
 
 // Create accessors
 RLMObjectBase *RLMCreateObjectAccessor(__unsafe_unretained RLMRealm *realm,
-                                       __unsafe_unretained RLMObjectSchema *objectSchema,
-                                       NSUInteger index);
+				       __unsafe_unretained RLMObjectSchema *objectSchema,
+				       NSUInteger index);
+
 
 
 #ifdef __cplusplus
