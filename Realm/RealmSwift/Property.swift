71c86f060d53c5e6a370fd5e1566f3e6cbaad10b
JP Simard <jp@jpsim.com>
2015-01-21 14:42:46 -0800

    Updated docs for Property.swift

--- Realm/RealmSwift/Property.swift
+++ Realm/RealmSwift/Property.swift
@@ -18,13 +18,6 @@
 
 import Realm
 
-// MARK: Equatable
-
-/// Returns whether the two properties are equal.
-public func ==(lhs: Property, rhs: Property) -> Bool {
-    return lhs.rlmProperty.isEqualToProperty(rhs.rlmProperty)
-}
-
 /**
 This class represents properties persisted to Realm in an ObjectSchema.
 
@@ -33,26 +26,42 @@ introspecting the database's schema.
 
 These properties map to columns in the core database.
 */
-public class Property: Equatable {
+public final class Property {
+
     // MARK: Properties
 
-    var rlmProperty: RLMProperty
+    /// Wrapped `RLMProperty`.
+    internal var rlmProperty: RLMProperty
 
-    /// Returns the name of the property.
+    /// Property name.
     public var name: String { return rlmProperty.name }
 
-    /// Returns the type of the property.
+    /// Property type.
     public var type: PropertyType { return rlmProperty.type }
 
-    /// Returns whether the property is indexed.
+    /// Whether this property is indexed.
     public var indexed: Bool { return rlmProperty.indexed }
 
-    /// Returns the name of the class the property stores.
+    /// Object class name - specify object types for `Object` and `List` properties.
     public var objectClassName: String { return rlmProperty.objectClassName }
 
     // MARK: Initializers
 
-    init(rlmProperty: RLMProperty) {
+    /**
+    Create a `Property` from an `RLMProperty`.
+
+    :param: rlmProperty `RLMProperty`.
+    */
+    internal init(rlmProperty: RLMProperty) {
         self.rlmProperty = rlmProperty
     }
+}
+
+// MARK: Equatable
+
+extension Property: Equatable {}
+
+/// Returns whether the two properties are equal.
+public func ==(lhs: Property, rhs: Property) -> Bool {
+    return lhs.rlmProperty.isEqualToProperty(rhs.rlmProperty)
 }
