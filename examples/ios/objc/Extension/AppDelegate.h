632bbd64bd0b77f4234b2c21ed206b5af26b369f
Samuel E. Giddins <segiddins@segiddins.me>
2015-01-22 13:45:02 -0800

    Add example app that has an extension

---
+++ examples/ios/objc/Extension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Extension
+//
+//  Created by Samuel Giddins on 1/21/15.
+//  Copyright (c) 2015 Realm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ examples/ios/objc/Extension/Tick.m
@@ -0,0 +1,13 @@
+//
+//  Tick.m
+//  RealmExamples
+//
+//  Created by Samuel Giddins on 1/21/15.
+//  Copyright (c) 2015 Realm. All rights reserved.
+//
+
+#import "Tick.h"
+
+@implementation Tick
+
+@end
