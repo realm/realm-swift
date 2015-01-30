78f4cdd4f368f4299428235561c277ddcc6a944e
Ari Lazier <alazier@gmail.com>
2015-01-30 13:15:30 -0800

    osx demo

---
+++ examples/osx/objc/Draw/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Draw
+//
+//  Created by Ari Lazier on 1/29/15.
+//  Copyright (c) 2015 Realm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ examples/osx/objc/Draw/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  Draw
+//
+//  Created by Ari Lazier on 1/29/15.
+//  Copyright (c) 2015 Realm. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+}
+
+@end
