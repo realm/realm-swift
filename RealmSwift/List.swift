fa49f9cacf3ae7e37c5052b9d42a164ae8477da4
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-03 14:10:55 -0800

    Add SortDescriptor.swift

--- RealmSwift/List.swift
+++ RealmSwift/List.swift
@@ -164,7 +164,18 @@ public final class List<T: Object>: ListBase, SequenceType {
     :returns: `Results` containing list elements sorted by the given property.
     */
     public func sorted(property: String, ascending: Bool = true) -> Results<T> {
-        return Results<T>(_rlmArray.sortedResultsUsingProperty(property, ascending: ascending))
+        return sorted([SortDescriptor(property: property, ascending: ascending)])
+    }
+
+    /**
+    Returns `Results` with elements sorted by the given sort descriptors.
+
+    :param: sortDescriptors `SortDescriptor`s to sort by.
+
+    :returns: `Results` with elements sorted by the given sort descriptors.
+    */
+    public func sorted<S: SequenceType where S.Generator.Element == SortDescriptor>(sortDescriptors: S) -> Results<T> {
+        return Results<T>(_rlmArray.sortedResultsUsingDescriptors(map(sortDescriptors) { $0.rlmSortDescriptorValue }))
     }
 
     // MARK: Sequence Support
