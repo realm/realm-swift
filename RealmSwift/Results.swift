0ea3c60263e9a1fc179c13c031d99fc8efc7b3c5
Samuel Giddins <segiddins@segiddins.me>
2015-02-03 13:48:21 -0800

    swift documentation

--- RealmSwift/Results.swift
+++ RealmSwift/Results.swift
@@ -18,7 +18,9 @@
 
 import Realm
 
-// Types which can be used for min()/max()
+// MARK: MinMaxType
+
+/// Types which can be used for min()/max().
 public protocol MinMaxType {}
 extension Double: MinMaxType {}
 extension Float: MinMaxType {}
@@ -28,7 +30,9 @@ extension Int64: MinMaxType {}
 extension Int: MinMaxType {}
 extension NSDate: MinMaxType {}
 
-// Types which can be used for average/sum
+// MARK: AddableType
+
+/// Types which can be used for average()/sum().
 public protocol AddableType {}
 extension Double: AddableType {}
 extension Float: AddableType {}
@@ -37,13 +41,28 @@ extension Int32: AddableType {}
 extension Int64: AddableType {}
 extension Int: AddableType {}
 
+/**
+`Results` is an auto-updating container type in Realm returned from object
+queries.
+
+Results can be queried with the same predicates as `List<T>` and you can chain queries to further
+filter query results.
+
+Results cannot be created directly.
+*/
 public final class Results<T: Object>: Printable, SequenceType {
-    let rlmResults: RLMResults
 
     // MARK: Properties
 
+    let rlmResults: RLMResults
+
+    /// Returns the Realm these results are associated with.
     public var realm: Realm { return Realm(rlmRealm: rlmResults.realm) }
+
+    /// Returns a human-readable description of the objects contained in these results.
     public var description: String { return rlmResults.description }
+
+    /// Returns the number of objects in these results.
     public var count: Int { return Int(rlmResults.count) }
 
     // MARK: Initializers
@@ -54,66 +73,157 @@ public final class Results<T: Object>: Printable, SequenceType {
 
     // MARK: Index Retrieval
 
+    /**
+    Returns the index of the given object, or `nil` if the object is not in the results.
+
+    :param: object The object whose index is being queried.
+
+    :returns: The index of the given object, or `nil` if the object is not in the results.
+    */
     public func indexOf(object: T) -> Int? {
         return notFoundToNil(rlmResults.indexOfObject(unsafeBitCast(object, RLMObject.self)))
     }
 
+    /**
+    Returns the index of the first object matching the given predicate,
+    or `nil` if the object is not in the results.
+
+    :param: predicate The predicate to filter the objects.
+
+    :returns: The index of the given object, or `nil` if the object is not in the results.
+    */
     public func indexOf(predicate: NSPredicate) -> Int? {
         return notFoundToNil(rlmResults.indexOfObjectWithPredicate(predicate))
     }
 
+    /**
+    Returns the index of the first object matching the given predicate,
+    or `nil` if the object is not in the results.
+
+    :param: predicateFormat The predicate format string which can accept variable arguments.
+
+    :returns: The index of the given object, or `nil` if the object is not in the results.
+    */
     public func indexOf(predicateFormat: String, _ args: CVarArgType...) -> Int? {
         return notFoundToNil(rlmResults.indexOfObjectWithPredicate(NSPredicate(format: predicateFormat, arguments: getVaList(args))))
     }
 
     // MARK: Object Retrieval
 
+    /**
+    Returns the object at the given `index`.
+
+    :param: index The index.
+
+    :returns: The object at the given `index`.
+    */
     public subscript(index: Int) -> T {
         get {
             return rlmResults[UInt(index)] as T
         }
     }
 
+    /// Returns the first object in the results, or `nil` if empty.
     public var first: T? { return rlmResults.firstObject() as T? }
 
+    /// Returns the last object in the results, or `nil` if empty.
     public var last: T? { return rlmResults.lastObject() as T? }
 
-    // MARK: Subarray Retrieval
+    // MARK: Filtering
 
+    /**
+    Filters the results to the objects that match the given predicate.
+
+    :param: predicateFormat The predicate format string which can accept variable arguments.
+
+    :returns: Results containing objects that match the given predicate.
+    */
     public func filter(predicateFormat: String, _ args: CVarArgType...) -> Results<T> {
         return Results<T>(rlmResults.objectsWithPredicate(NSPredicate(format: predicateFormat, arguments: getVaList(args))))
     }
 
+    /**
+    Filters the results to the objects that match the given predicate.
+
+    :param: predicate The predicate to filter the objects.
+
+    :returns: Results containing objects that match the given predicate.
+    */
     public func filter(predicate: NSPredicate) -> Results<T> {
         return Results<T>(rlmResults.objectsWithPredicate(predicate))
     }
 
     // MARK: Sorting
 
+    /**
+    Returns `Results` with elements sorted by the given property name.
+
+    :param: property  The property name to sort by.
+    :param: ascending The direction to sort by.
+
+    :returns: `Results` with elements sorted by the given property name.
+    */
     public func sorted(property: String, ascending: Bool = true) -> Results<T> {
         return Results<T>(rlmResults.sortedResultsUsingProperty(property, ascending: ascending))
     }
 
     // MARK: Aggregate Operations
 
+    /**
+    Returns the minimum value of the given property.
+
+    :warning: Only names of properties of a type conforming to the `MinMaxType` protocol can be used.
+
+    :param: property The name of a property conforming to `MinMaxType` to look for a minimum on.
+
+    :returns: The minimum value for the property amongst objects in the Results, or `nil` if the Results is empty.
+    */
     public func min<U: MinMaxType>(property: String) -> U {
         return rlmResults.minOfProperty(property) as U
     }
 
+    /**
+    Returns the maximum value of the given property.
+
+    :warning: Only names of properties of a type conforming to the `MinMaxType` protocol can be used.
+
+    :param: property The name of a property conforming to `MinMaxType` to look for a maximum on.
+
+    :returns: The maximum value for the property amongst objects in the Results, or `nil` if the Results is empty.
+    */
     public func max<U: MinMaxType>(property: String) -> U {
         return rlmResults.maxOfProperty(property) as U
     }
 
+    /**
+    Returns the sum of the given property for objects in the Results.
+
+    :warning: Only names of properties of a type conforming to the `AddableType` protocol can be used.
+
+    :param: property The name of a property conforming to `AddableType` to calculate sum on.
+
+    :returns: The sum of the given property over all objects in the Results.
+    */
     public func sum<U: AddableType>(property: String) -> U {
         return rlmResults.sumOfProperty(property) as AnyObject as U
     }
 
+    /**
+    Returns the average of the given property for objects in the Results.
+
+    :warning: Only names of properties of a type conforming to the `AddableType` protocol can be used.
+
+    :param: property The name of a property conforming to `AddableType` to calculate average on.
+
+    :returns: The average of the given property over all objects in the Results.
+    */
     public func average<U: AddableType>(property: String) -> U {
         return rlmResults.averageOfProperty(property) as AnyObject as U
     }
 
     // MARK: Sequence Support
 
+    /// Returns a `GeneratorOf<T>` that yields successive elements in the results.
     public func generate() -> GeneratorOf<T> {
         var i: UInt = 0
         return GeneratorOf<T>() {
@@ -123,14 +233,5 @@ public final class Results<T: Object>: Printable, SequenceType {
                 return self.rlmResults[i++] as? T
             }
         }
-    }
-
-    // MARK: Private stuff
-
-    private func notFoundToNil(index: UInt) -> Int? {
-        if index == UInt(NSNotFound) {
-            return nil
-        }
-        return Int(index)
     }
 }
