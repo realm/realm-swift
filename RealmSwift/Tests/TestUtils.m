7ffe0e4e0772aacd1c010083f768e6106438bbaf
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-03 17:30:38 -0800

    Add assertThrows to RealmSwift tests

---
+++ RealmSwift/Tests/TestUtils.m
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2015 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+#import "TestUtils.h"
+
+@import XCTest;
+
+void RLMAssertThrows(XCTestCase *self, dispatch_block_t block, NSString *message, NSString *fileName, NSUInteger lineNumber) {
+    BOOL __didThrow = NO;
+    @try {
+        block();
+    }
+    @catch (...) {
+        __didThrow = YES;
+    }
+    if (!__didThrow) {
+        NSString *prefix = @"The given expression failed to throw an exception";
+        message = message ? [NSString stringWithFormat:@"%@ (%@)",  prefix, message] : prefix;
+        [self recordFailureWithDescription:message inFile:fileName atLine:lineNumber expected:NO];
+    }
+}
