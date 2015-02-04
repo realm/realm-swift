00d9aebcd5b765cf1172d34cf0579df78c11e864
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-03 17:30:38 -0800

    Add assertThrows to RealmSwift tests

--- RealmSwift/Tests/TestCase.swift
+++ RealmSwift/Tests/TestCase.swift
@@ -68,4 +68,8 @@ class TestCase: XCTestCase {
 
         deleteRealmFiles()
     }
+
+    func assertThrows<T>(block: @autoclosure () -> T, _ message: String? = nil, fileName: String = __FILE__, lineNumber: UInt = __LINE__) {
+        RLMAssertThrows(self, { _ = block() }, message, fileName, lineNumber);
+    }
 }
