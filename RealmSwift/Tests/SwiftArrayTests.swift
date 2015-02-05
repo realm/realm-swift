3f3f5358e7afeacb29e4938c78c0b93ff3680c50
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-05 12:07:14 -0800

    [Realm] Make realm a parameter on the objects() function instead of a method on Realm

--- RealmSwift/Tests/SwiftArrayTests.swift
+++ RealmSwift/Tests/SwiftArrayTests.swift
@@ -42,7 +42,7 @@ class SwiftArrayTests: TestCase {
 
         realm.commitWrite()
 
-        let results = realm.objects(SwiftAggregateObject).filter("intCol < 100")
+        let results = objects(SwiftAggregateObject.self, inRealm: realm).filter("intCol < 100")
         XCTAssertEqual(results.count, Int(10), "10 objects added")
 
         var totalSum = 0
@@ -67,7 +67,7 @@ class SwiftArrayTests: TestCase {
 
         realm.commitWrite()
 
-        let description: NSString = realm.objects(SwiftEmployeeObject).description
+        let description: NSString = objects(SwiftEmployeeObject.self, inRealm: realm).description
 
         XCTAssertTrue(description.rangeOfString("name").location != Foundation.NSNotFound, "property names should be displayed when calling \"description\" on RLMArray")
         XCTAssertTrue(description.rangeOfString("Mary").location != Foundation.NSNotFound, "property values should be displayed when calling \"description\" on RLMArray")
@@ -104,7 +104,7 @@ class SwiftArrayTests: TestCase {
 
         let company = SwiftCompanyObject()
         realm.add(company)
-        company.employees.append(realm.objects(SwiftEmployeeObject))
+        company.employees.append(objects(SwiftEmployeeObject.self, inRealm: realm))
 
         realm.commitWrite()
 
