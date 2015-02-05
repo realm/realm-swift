3b22e0479f014bb2ba84b2af6bc0318739ae925f
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-05 12:12:39 -0800

    [Realm] add setEncryptionKey

--- RealmSwift/Realm.swift
+++ RealmSwift/Realm.swift
@@ -69,6 +69,25 @@ public func defaultRealm() -> Realm {
 }
 
 /**
+Set the encryption key to use when opening Realms at a certain path.
+
+This can be used as an alternative to explicitly passing the key to
+`Realm(path:, encryptionKey:, readOnly:, error:)` each time a Realm instance is
+needed. The encryption key will be used any time a Realm is opened with
+`Realm(path:)` or `defaultRealm()`.
+
+If you do not want Realm to hold on to your encryption keys any longer than
+needed, then use `Realm(path:, encryptionKey:, readOnly:, error:)` rather than this
+method.
+
+:param: encryptionKey 64-byte encryption key to use, or `nil` to unset.
+:param: path          Realm path to set the encryption key for.
++*/
+public func setEncryptionKey(encryptionKey: NSData?, forRealmsAtPath path: String = defaultRealmPath) {
+    RLMRealm.setEncryptionKey(encryptionKey, forRealmsAtPath: path)
+}
+
+/**
 A Realm instance (also referred to as "a realm") represents a Realm
 database.
 
