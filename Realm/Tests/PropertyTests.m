50f203d8d9569e5c259673f989bd91823059ce8e
Ari Lazier <alazier@gmail.com>
2015-02-05 10:32:34 -0800

    use -dynamic extension for dynamic tests so it uses the correct framework

--- Realm/Tests/PropertyTests.m
+++ Realm/Tests/PropertyTests.m
@@ -1,44 +1,21 @@
-////////////////////////////////////////////////////////////////////////////
 //
-// Copyright 2015 Realm Inc.
+//  RLMPropertyTests.m
+//  Realm
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//  Created by Samuel Giddins on 2/2/15.
+//  Copyright (c) 2015 Realm. All rights reserved.
 //
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-////////////////////////////////////////////////////////////////////////////
 
 #import "RLMTestCase.h"
 
 #import <objc/runtime.h>
 #import "RLMProperty_Private.h"
-#import "RLMObject.h"
 
-@interface PropertyTests : RLMTestCase
+@interface RLMPropertyTests : RLMTestCase
 
 @end
 
-@implementation PropertyTests
-
-- (void)testDescription {
-    AllTypesObject *object = [[AllTypesObject alloc] init];
-    RLMProperty *property = object.objectSchema[@"objectCol"];
-
-    XCTAssertEqualObjects(property.description, @"objectCol {\n"
-                                                @"\ttype = object;\n"
-                                                @"\tobjectClassName = StringObject;\n"
-                                                @"\tindexed = NO;\n"
-                                                @"\tisPrimary = NO;\n"
-                                                @"}");
-}
+@implementation RLMPropertyTests
 
 - (void)testTwoPropertiesAreEqual {
     const char *name = "intCol";
