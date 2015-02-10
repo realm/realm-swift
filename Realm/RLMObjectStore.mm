164e86e6dfb92bb75b4b7e4227addfb756560d00
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-09 17:08:39 -0800

    [RLMObjectStore] Remind users to call setSchemaVersion when opening with wrong version

--- Realm/RLMObjectStore.mm
+++ Realm/RLMObjectStore.mm
@@ -242,7 +242,8 @@ NSError *RLMUpdateRealmToSchemaVersion(RLMRealm *realm, NSUInteger newVersion, R
 
         // validate versions
         if (oldVersion > newVersion && oldVersion != RLMNotVersioned) {
-            NSString *reason = [NSString stringWithFormat:@"Realm at path '%@' has version number %lu which is greater than the current schema version %lu",
+            NSString *reason = [NSString stringWithFormat:@"Realm at path '%@' has version number %lu which is greater than the current schema version %lu. "
+                                                          @"You must call setSchemaVersion: or setDefaultRealmSchemaVersion: before accessing an upgraded Realm.",
                                                           realm.path, (unsigned long)oldVersion, (unsigned long)newVersion];
             @throw RLMException(reason, @{@"path" : realm.path});
         }
