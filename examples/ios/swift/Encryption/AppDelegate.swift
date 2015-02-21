28bb1bfd46f1d76caa16d99d6b7fcda4985743c3
Isaac Roldan <isaac.roldan@gmail.com>
2015-02-21 03:21:42 +0100

    fixed swift code, adapted to swift 1.2

--- examples/ios/swift/Encryption/AppDelegate.swift
+++ examples/ios/swift/Encryption/AppDelegate.swift
@@ -20,12 +20,13 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow!
+   
+    var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        self.window.rootViewController = ViewController()
-        self.window.makeKeyAndVisible()
+        self.window!.rootViewController = ViewController()
+        self.window!.makeKeyAndVisible()
 
         return true
     }
