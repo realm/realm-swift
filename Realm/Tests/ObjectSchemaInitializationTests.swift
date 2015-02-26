3724e0678f9fce9b854ac3fbb456e8059ee4362b
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-26 14:30:21 -0800

    [Object] Fix schema initialization tests and add tests for indexedProperties

--- Realm/Tests/ObjectSchemaInitializationTests.swift
+++ Realm/Tests/ObjectSchemaInitializationTests.swift
@@ -18,16 +18,94 @@
 
 import XCTest
 import RealmSwift
+import Realm.Private
 import Foundation
 
 class ObjectSchemaInitializationTests: TestCase {
     func testAllValidTypes() {
+        let object = SwiftObject()
+        let objectSchema = object.objectSchema
+
+        let noSuchCol = objectSchema["noSuchCol"]
+        XCTAssertNil(noSuchCol)
+
+        let boolCol = objectSchema["boolCol"]
+        XCTAssertNotNil(boolCol)
+        XCTAssertEqual(boolCol!.name, "boolCol")
+        XCTAssertEqual(boolCol!.type, PropertyType.Bool)
+        XCTAssertFalse(boolCol!.indexed)
+        XCTAssertNil(boolCol!.objectClassName)
+
+        let intCol = objectSchema["intCol"]
+        XCTAssertNotNil(intCol)
+        XCTAssertEqual(intCol!.name, "intCol")
+        XCTAssertEqual(intCol!.type, PropertyType.Int)
+        XCTAssertFalse(intCol!.indexed)
+        XCTAssertNil(intCol!.objectClassName)
+
+        let floatCol = objectSchema["floatCol"]
+        XCTAssertNotNil(floatCol)
+        XCTAssertEqual(floatCol!.name, "floatCol")
+        XCTAssertEqual(floatCol!.type, PropertyType.Float)
+        XCTAssertFalse(floatCol!.indexed)
+        XCTAssertNil(floatCol!.objectClassName)
+
+        let doubleCol = objectSchema["doubleCol"]
+        XCTAssertNotNil(doubleCol)
+        XCTAssertEqual(doubleCol!.name, "doubleCol")
+        XCTAssertEqual(doubleCol!.type, PropertyType.Double)
+        XCTAssertFalse(doubleCol!.indexed)
+        XCTAssertNil(doubleCol!.objectClassName)
+
+        let stringCol = objectSchema["stringCol"]
+        XCTAssertNotNil(stringCol)
+        XCTAssertEqual(stringCol!.name, "stringCol")
+        XCTAssertEqual(stringCol!.type, PropertyType.String)
+        XCTAssertFalse(stringCol!.indexed)
+        XCTAssertNil(stringCol!.objectClassName)
+
+        let binaryCol = objectSchema["binaryCol"]
+        XCTAssertNotNil(binaryCol)
+        XCTAssertEqual(binaryCol!.name, "binaryCol")
+        XCTAssertEqual(binaryCol!.type, PropertyType.Data)
+        XCTAssertFalse(binaryCol!.indexed)
+        XCTAssertNil(binaryCol!.objectClassName)
+
+        let dateCol = objectSchema["dateCol"]
+        XCTAssertNotNil(dateCol)
+        XCTAssertEqual(dateCol!.name, "dateCol")
+        XCTAssertEqual(dateCol!.type, PropertyType.Date)
+        XCTAssertFalse(dateCol!.indexed)
+        XCTAssertNil(dateCol!.objectClassName)
+
+        let objectCol = objectSchema["objectCol"]
+        XCTAssertNotNil(objectCol)
+        XCTAssertEqual(objectCol!.name, "objectCol")
+        XCTAssertEqual(objectCol!.type, PropertyType.Object)
+        XCTAssertFalse(objectCol!.indexed)
+        XCTAssertEqual(objectCol!.objectClassName!, "SwiftBoolObject")
+
+        let arrayCol = objectSchema["arrayCol"]
+        XCTAssertNotNil(arrayCol)
+        XCTAssertEqual(arrayCol!.name, "arrayCol")
+        XCTAssertEqual(arrayCol!.type, PropertyType.Array)
+        XCTAssertFalse(arrayCol!.indexed)
+        XCTAssertEqual(objectCol!.objectClassName!, "SwiftBoolObject")
     }
 
     func testInvalidObjects() {
+        let schema = RLMObjectSchema(forObjectClass: SwiftFakeObjectSubclass.self) // Should be able to get a schema for a non-RLMObjectBase subclass
+        XCTAssertEqual(schema.properties.count, 1)
+
+        assertThrows(RLMObjectSchema(forObjectClass: SwiftObjectWithNSURL.self), "Should throw when not ignoring a property of a type we can't persist")
+        RLMObjectSchema(forObjectClass: SwiftObjectWithEnum.self) // Shouldn't throw when not ignoring a property of a type we can't persist if it's not dynamic
+        RLMObjectSchema(forObjectClass: SwiftObjectWithStruct.self) // Shouldn't throw when not ignoring a property of a type we can't persist if it's not dynamic
+        assertThrows(RLMObjectSchema(forObjectClass: SwiftObjectWithDatePrimaryKey.self), "Should throw when setting a non int/string primary key")
     }
 
     func testPrimaryKey() {
+        XCTAssertNil(SwiftObject().objectSchema.primaryKeyProperty, "Object should default to having no primary key property")
+        XCTAssertEqual(SwiftPrimaryStringObject().objectSchema.primaryKeyProperty!.name, "stringCol")
     }
 
     func testIgnoredProperties() {
@@ -38,5 +116,61 @@ class ObjectSchemaInitializationTests: TestCase {
     }
 
     func testIndexedProperties() {
+        XCTAssertTrue(SwiftIndexedPropertiesObject().objectSchema["stringCol"]!.indexed)
+
+        let unindexibleSchema = RLMObjectSchema(forObjectClass: SwiftObjectWithUnindexibleProperties.self)
+        for propName in SwiftObjectWithUnindexibleProperties.indexedProperties() {
+            XCTAssertFalse(unindexibleSchema[propName]!.indexed, "Shouldn't mark unindexible property '\(propName)' as indexed")
+        }
+    }
+}
+
+class SwiftFakeObject : NSObject {
+    dynamic class func primaryKey() -> String! { return nil }
+    dynamic class func ignoredProperties() -> [String] { return [] }
+    dynamic class func indexedProperties() -> [String] { return [] }
+}
+
+class SwiftObjectWithNSURL : SwiftFakeObject {
+    dynamic var URL: NSURL = NSURL(string: "http://realm.io")!
+}
+
+enum SwiftEnum {
+    case Case1
+    case Case2
+}
+
+class SwiftObjectWithEnum : SwiftFakeObject {
+    var swiftEnum = SwiftEnum.Case1
+}
+
+class SwiftObjectWithStruct : SwiftFakeObject {
+    var swiftStruct = SortDescriptor(property: "prop")
+}
+
+class SwiftObjectWithDatePrimaryKey : SwiftFakeObject {
+    dynamic var date = NSDate()
+
+    dynamic override class func primaryKey() -> String! {
+        return "date"
+    }
+}
+
+class SwiftFakeObjectSubclass : SwiftFakeObject {
+    dynamic var dateCol = NSDate()
+}
+
+class SwiftObjectWithUnindexibleProperties : SwiftFakeObject {
+    dynamic var boolCol = false
+    dynamic var intCol = 123
+    dynamic var floatCol = 1.23 as Float
+    dynamic var doubleCol = 12.3
+    dynamic var binaryCol = "a".dataUsingEncoding(NSUTF8StringEncoding)!
+    dynamic var dateCol = NSDate(timeIntervalSince1970: 1)
+    dynamic var objectCol = SwiftBoolObject()
+    let arrayCol = List<SwiftBoolObject>()
+
+    dynamic override class func indexedProperties() -> [String] {
+        return ["boolCol", "intCol", "floatCol", "doubleCol", "binaryCol", "dateCol", "objectCol", "arrayCol"]
     }
 }
