ca3c26781fe1cfd0b79e8bb671cbe454ac03ca65
Samuel E. Giddins <segiddins@segiddins.me>
2015-03-02 14:10:50 -0800

    [RLMProperty] Terminate attributes in -description with a semicolon

--- Realm/RLMProperty.mm
+++ Realm/RLMProperty.mm
@@ -312,7 +312,7 @@
 }
 
 - (NSString *)description {
-    return [NSString stringWithFormat:@"%@ {\n\ttype = %@\n\tobjectClassName = %@\n\tindexed = %@\n\tisPrimary = %@\n}", self.name, RLMTypeToString(self.type), self.objectClassName, self.indexed ? @"YES" : @"NO", self.isPrimary ? @"YES" : @"NO"];
+    return [NSString stringWithFormat:@"%@ {\n\ttype = %@;\n\tobjectClassName = %@;\n\tindexed = %@;\n\tisPrimary = %@;\n}", self.name, RLMTypeToString(self.type), self.objectClassName, self.indexed ? @"YES" : @"NO", self.isPrimary ? @"YES" : @"NO"];
 }
 
 @end
