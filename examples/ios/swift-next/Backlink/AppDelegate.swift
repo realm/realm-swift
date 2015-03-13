7fe0429a9052fd37cc6f226586e0abf07969c767
Samuel E. Giddins <segiddins@segiddins.me>
2015-03-13 14:53:45 -0700

    Remove extraneous cast in RealmSwift Backlink example

--- examples/ios/swift-next/Backlink/AppDelegate.swift
+++ examples/ios/swift-next/Backlink/AppDelegate.swift
@@ -26,7 +26,7 @@ class Dog: Object {
     var owners: [Person] {
         // Realm doesn't persist this property because it only has a getter defined
         // Define "owners" as the inverse relationship to Person.dogs
-        return linkingObjects(Person.self, forProperty: "dogs") as [Person]
+        return linkingObjects(Person.self, forProperty: "dogs")
     }
 }
 
