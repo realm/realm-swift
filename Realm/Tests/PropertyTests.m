505758df4b8f8bac03f362e5777baaa6dcd2befe
Samuel E. Giddins <segiddins@segiddins.me>
2015-03-16 13:16:13 -0700

    [PropertyTests] Revert unintentional changes

--- Realm/Tests/PropertyTests.m
+++ Realm/Tests/PropertyTests.m
@@ -1,21 +1,43 @@
+////////////////////////////////////////////////////////////////////////////
 //
-//  RLMPropertyTests.m
-//  Realm
+// Copyright 2015 Realm Inc.
 //
-//  Created by Samuel Giddins on 2/2/15.
-//  Copyright (c) 2015 Realm. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
 
 #import "RLMTestCase.h"
 
 #import <objc/runtime.h>
 #import "RLMProperty_Private.h"
 
-@interface RLMPropertyTests : RLMTestCase
+@interface PropertyTests : RLMTestCase
 
 @end
 
-@implementation RLMPropertyTests
+@implementation PropertyTests
+
+- (void)testDescription {
+    AllTypesObject *object = [[AllTypesObject alloc] init];
+    RLMProperty *property = object.objectSchema[@"objectCol"];
+
+    XCTAssertEqualObjects(property.description, @"objectCol {\n"
+                                                @"\ttype = object;\n"
+                                                @"\tobjectClassName = StringObject;\n"
+                                                @"\tindexed = NO;\n"
+                                                @"\tisPrimary = NO;\n"
+                                                @"}");
+}
 
 - (void)testTwoPropertiesAreEqual {
     const char *name = "intCol";
