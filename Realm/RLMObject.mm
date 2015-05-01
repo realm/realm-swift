rkawajiri/realm-cocoa

properly initialize standalone circular object graphs

Realm/RLMObject.mm
@@ -40,7 +40,7 @@
 }
 
 - (instancetype)initWithObject:(id)object {
-    return [super initWithObject:object schema:RLMSchema.sharedSchema];
+    return [super initWithObject:object schema:RLMSchema.sharedSchema valueToAccessorMapping:[NSMapTable strongToStrongObjectsMapTable]];
 }
 
 + (instancetype)createInDefaultRealmWithObject:(id)object {

Realm/RLMObjectBase.mm
@@ -58,11 +58,15 @@ const NSUInteger RLMDescriptionMaxDepth = 5;
     return self;
 }
 
-- (instancetype)initWithObject:(id)value schema:(RLMSchema *)schema {
+- (instancetype)initWithObject:(id)value schema:(RLMSchema *)schema valueToAccessorMapping:(NSMapTable *)valueToAccessorMapping {
+    if (id existing = [valueToAccessorMapping objectForKey:value]) {
+        return existing;
+    }
     self = [self init];
+    [valueToAccessorMapping setObject:self forKey:value];
     if (NSArray *array = RLMDynamicCast<NSArray>(value)) {
         // validate and populate
-        array = RLMValidatedArrayForObjectSchema(array, _objectSchema, schema);
+        array = RLMValidatedArrayForObjectSchema(array, _objectSchema, schema, nil, valueToAccessorMapping);
         NSArray *properties = _objectSchema.properties;
         for (NSUInteger i = 0; i < array.count; i++) {
             [self setValue:array[i] forKeyPath:[properties[i] name]];
@@ -70,7 +74,7 @@ const NSUInteger RLMDescriptionMaxDepth = 5;
     }
     else {
         // assume our object is an NSDictionary or a an object with kvc properties
-        NSDictionary *dict = RLMValidatedDictionaryForObjectSchema(value, _objectSchema, schema);
+        NSDictionary *dict = RLMValidatedDictionaryForObjectSchema(value, _objectSchema, schema, false, nil, valueToAccessorMapping);
         for (NSString *name in dict) {
             id val = dict[name];
             // strip out NSNull before passing values to standalone setters

Realm/RLMObject_Private.h
@@ -29,7 +29,7 @@
 }
 
 // standalone initializer
-- (instancetype)initWithObject:(id)value schema:(RLMSchema *)schema;
+- (instancetype)initWithObject:(id)value schema:(RLMSchema *)schema valueToAccessorMapping:(NSMapTable *)valueToAccessorMapping;
 
 // live accessor initializer
 - (instancetype)initWithRealm:(__unsafe_unretained RLMRealm *const)realm

Realm/RLMUtil.mm
@@ -168,7 +168,7 @@ static inline id RLMValidatedRealmObject(id obj, RLMSchema *schema, RLMObjectSch
         return _RLMCreateObjectInRealmWithValue(realm, objectSchema.className, obj, RLMCreationOptionsAllowCopy | RLMCreationOptionsUpdateOrCreate, valueToAccessorMapping);
     }
     else {
-        return [[objectSchema.objectClass alloc] initWithObject:obj schema:schema];
+        return [[objectSchema.objectClass alloc] initWithObject:obj schema:schema valueToAccessorMapping:valueToAccessorMapping];
     }
 }
 

Realm/Tests/ObjectTests.m
@@ -1002,6 +1002,19 @@ RLM_ARRAY_TYPE(PrimaryEmployeeObject);
     [realm commitWriteTransaction];
 }
 
+- (void)testInitCircularObject
+{
+    RLMRealm *realm = [self realmWithTestPath];
+
+    NSMutableDictionary *value = [NSMutableDictionary dictionaryWithObject:@"data" forKey:@"data"];
+    value[@"next"] = @{@"data": @"other data", @"next": value};
+
+    [realm beginWriteTransaction];
+    CircleObject *standalone = [[CircleObject alloc] initWithObject:value];
+    [realm commitWriteTransaction];
+    XCTAssertEqual(standalone.next.next, standalone);
+}
+
 - (void)testObjectDescription
 {
     RLMRealm *realm = [RLMRealm defaultRealm];

RealmSwift/Object.swift
@@ -76,7 +76,7 @@ public class Object: RLMObjectBase, Equatable, Printable {
                     thrown if any required properties are not present and no default is set.
     */
     public init(value: AnyObject) {
-        super.init(object: value, schema: RLMSchema.sharedSchema())
+        super.init(object: value, schema: RLMSchema.sharedSchema(), valueToAccessorMapping: NSMapTable.strongToStrongObjectsMapTable())
     }
 
 
@@ -160,8 +160,8 @@ public class Object: RLMObjectBase, Equatable, Printable {
     WARNING: This is an internal initializer not intended for public use.
     :nodoc:
     */
-    public override init(object: AnyObject, schema: RLMSchema) {
-        super.init(object: object, schema: schema)
+    public override init(object: AnyObject, schema: RLMSchema, valueToAccessorMapping: NSMapTable?) {
+        super.init(object: object, schema: schema, valueToAccessorMapping: valueToAccessorMapping)
     }
 
     /**

realm/realm-swift

Realm/RLMObject.mm
@@ -40,7 +40,7 @@
 }
 
 - (instancetype)initWithValue:(id)value {
-    return [super initWithValue:value schema:RLMSchema.sharedSchema];
+    return [super initWithValue:value schema:RLMSchema.sharedSchema valueToAccessorMapping:[NSMapTable strongToStrongObjectsMapTable]];
 }
 
 - (instancetype)initWithObject:(id)object {

Realm/RLMObjectBase.mm
@@ -58,11 +58,15 @@ const NSUInteger RLMDescriptionMaxDepth = 5;
     return self;
 }
 
-- (instancetype)initWithValue:(id)value schema:(RLMSchema *)schema {
+- (instancetype)initWithValue:(id)value schema:(RLMSchema *)schema valueToAccessorMapping:(NSMapTable *)valueToAccessorMapping {
+    if (id existing = [valueToAccessorMapping objectForKey:value]) {
+        return existing;
+    }
     self = [self init];
+    [valueToAccessorMapping setObject:self forKey:value];
     if (NSArray *array = RLMDynamicCast<NSArray>(value)) {
         // validate and populate
-        array = RLMValidatedArrayForObjectSchema(array, _objectSchema, schema);
+        array = RLMValidatedArrayForObjectSchema(array, _objectSchema, schema, nil, valueToAccessorMapping);
         NSArray *properties = _objectSchema.properties;
         for (NSUInteger i = 0; i < array.count; i++) {
             [self setValue:array[i] forKeyPath:[properties[i] name]];
@@ -70,7 +74,7 @@ const NSUInteger RLMDescriptionMaxDepth = 5;
     }
     else {
         // assume our object is an NSDictionary or a an object with kvc properties
-        NSDictionary *dict = RLMValidatedDictionaryForObjectSchema(value, _objectSchema, schema);
+        NSDictionary *dict = RLMValidatedDictionaryForObjectSchema(value, _objectSchema, schema, false, nil, valueToAccessorMapping);
         for (NSString *name in dict) {
             id val = dict[name];
             // strip out NSNull before passing values to standalone setters

Realm/RLMObject_Private.h
@@ -29,7 +29,7 @@
 }
 
 // standalone initializer
-- (instancetype)initWithValue:(id)value schema:(RLMSchema *)schema;
+- (instancetype)initWithValue:(id)value schema:(RLMSchema *)schema valueToAccessorMapping:(NSMapTable *)valueToAccessorMapping;
 
 // live accessor initializer
 - (instancetype)initWithRealm:(__unsafe_unretained RLMRealm *const)realm

Realm/RLMUtil.mm
@@ -168,7 +168,7 @@ static inline id RLMValidatedRealmObject(id obj, RLMSchema *schema, RLMObjectSch
         return _RLMCreateObjectInRealmWithValue(realm, objectSchema.className, obj, RLMCreationOptionsAllowCopy | RLMCreationOptionsUpdateOrCreate, valueToAccessorMapping);
     }
     else {
-        return [[objectSchema.objectClass alloc] initWithValue:obj schema:schema];
+        return [[objectSchema.objectClass alloc] initWithValue:obj schema:schema valueToAccessorMapping:valueToAccessorMapping];
     }
 }
 

Realm/Tests/ObjectTests.m
@@ -1002,6 +1002,19 @@ RLM_ARRAY_TYPE(PrimaryEmployeeObject);
     [realm commitWriteTransaction];
 }
 
+- (void)testInitCircularObject
+{
+    RLMRealm *realm = [self realmWithTestPath];
+
+    NSMutableDictionary *value = [NSMutableDictionary dictionaryWithObject:@"data" forKey:@"data"];
+    value[@"next"] = @{@"data": @"other data", @"next": value};
+
+    [realm beginWriteTransaction];
+    CircleObject *standalone = [[CircleObject alloc] initWithValue:value];
+    [realm commitWriteTransaction];
+    XCTAssertEqual(standalone.next.next, standalone);
+}
+
 - (void)testObjectDescription
 {
     RLMRealm *realm = [RLMRealm defaultRealm];

RealmSwift/Object.swift
@@ -76,7 +76,7 @@ public class Object: RLMObjectBase, Equatable, Printable {
                     thrown if any required properties are not present and no default is set.
     */
     public init(value: AnyObject) {
-        super.init(value: value, schema: RLMSchema.sharedSchema())
+        super.init(value: value, schema: RLMSchema.sharedSchema(), valueToAccessorMapping: NSMapTable.strongToStrongObjectsMapTable())
     }
 
 
@@ -160,8 +160,8 @@ public class Object: RLMObjectBase, Equatable, Printable {
     WARNING: This is an internal initializer not intended for public use.
     :nodoc:
     */
-    public override init(value: AnyObject, schema: RLMSchema) {
-        super.init(value: value, schema: schema)
+    public override init(value: AnyObject, schema: RLMSchema, valueToAccessorMapping: NSMapTable?) {
+        super.init(value: value, schema: schema, valueToAccessorMapping: valueToAccessorMapping)
     }
 
     /**