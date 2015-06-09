dddb610e2563c67b0caf182e5ae4a9d471fd2a40
JP Simard <jp@jpsim.com>
2015-06-09 12:51:19 -0700

    [RLMSupport.swift] Removed explict typealias that could be inferred

--- Realm/Swift/RLMSupport.swift
+++ Realm/Swift/RLMSupport.swift
@@ -37,8 +37,6 @@ public class RLMGenerator: GeneratorType {
         self.collection = collection
     }
 
-    typealias Element = RLMObject
-
     public func next() -> RLMObject? {
         if i >= collection.count {
             return .None
