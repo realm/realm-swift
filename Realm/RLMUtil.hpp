3351c6dce5f4d2a3e3f60f992416a9409e73a64a
Samuel E. Giddins <segiddins@segiddins.me>
2015-06-23 15:07:09 -0700

    [RLMUtil] Add comment for weird casting in RLMBinaryDataForNSData

--- Realm/RLMUtil.hpp
+++ Realm/RLMUtil.hpp
@@ -131,6 +131,9 @@ static inline NSData *RLMBinaryDataToNSData(realm::BinaryData binaryData) {
 }
 
 static inline realm::BinaryData RLMBinaryDataForNSData(__unsafe_unretained NSData *const data) {
+    // this is necessary to ensure that the empty NSData isn't treated by core as the null realm::BinaryData
+    // because data.bytes == 0 when data.length == 0
+    // the casting bit ensures that we create a data with a non-null pointer
     auto bytes = static_cast<const char *>(data.bytes) ?: static_cast<char *>((__bridge void *)data);
     return realm::BinaryData(bytes, data.length);
 }
