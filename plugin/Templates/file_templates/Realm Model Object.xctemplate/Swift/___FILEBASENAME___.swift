cbf80b1ce5cd7bb59295e244c5b803622e8a8970
ZU3CO <zubco.pavel@gmail.com>
2015-07-11 15:00:39 +0300

    Added Swift File Template.

---
+++ plugin/Templates/file_templates/Realm Model Object.xctemplate/Swift/___FILEBASENAME___.swift
@@ -0,0 +1,18 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import RealmSwift
+
+class ___FILEBASENAMEASIDENTIFIER___: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
