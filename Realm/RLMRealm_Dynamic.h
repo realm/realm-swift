5c21cc1343a836ac764eddb7ab27ae59f99e72d0
Adam Fish <af@realm.io>
2015-07-21 16:47:10 -0700

    Reworked the RLMRealm_Dynamic header to only include the ability to retrieve objects via className string from a Realm and to create an object with a className string.
    The full Realm constructor was moved to RLMRealm_Private and the ObjectSchema and Property methods were moved to RLMObjectSchema_Private.h and RLMProperty_Private.h

--- Realm/RLMRealm_Dynamic.h
+++ Realm/RLMRealm_Dynamic.h
@@ -25,42 +25,6 @@
 
 @interface RLMRealm (Dynamic)
 
-/**
- This method is useful only in specialized circumstances, for example, when opening Realm files
- retrieved externally that contain a different schema than defined in your application.
- If you are simply building an app on Realm you should consider using:
- [defaultRealm]([RLMRealm defaultRealm]) or [realmWithPath:]([RLMRealm realmWithPath:])
- 
- Obtains an `RLMRealm` instance with persistence to a specific file path with
- options.
- 
- @warning This method is useful only in specialized circumstances.
-
- @param path         Path to the file you want the data saved in.
- @param key          64-byte key to use to encrypt the data.
- @param readonly     `BOOL` indicating if this Realm is read-only (must use for read-only files)
- @param inMemory     `BOOL` indicating if this Realm is in-memory
- @param dynamic      `BOOL` indicating if this Realm is dynamic
- @param customSchema `RLMSchema` object representing the schema for the Realm
- @param outError     If an error occurs, upon return contains an `NSError` object
-                     that describes the problem. If you are not interested in
-                     possible errors, pass in NULL.
-
- @return An `RLMRealm` instance.
- 
- @see RLMRealm defaultRealm
- @see RLMRealm realmWithPath:
- @see RLMRealm realmWithPath:readOnly:error:
- @see RLMRealm realmWithPath:encryptionKey:readOnly:error:
- */
-+ (instancetype)realmWithPath:(NSString *)path
-                          key:(NSData *)key
-                     readOnly:(BOOL)readonly
-                     inMemory:(BOOL)inMemory
-                      dynamic:(BOOL)dynamic
-                       schema:(RLMSchema *)customSchema
-                        error:(NSError **)outError;
-
 /**---------------------------------------------------------------------------------------
  *  @name Getting Objects from a Realm
  * ---------------------------------------------------------------------------------------
@@ -145,46 +109,4 @@
  */
 -(RLMObject *)createObject:(NSString *)className withValue:(id)value;
 
-@end
-
-@interface RLMObjectSchema (Dynamic)
-/**
- This method is useful only in specialized circumstances, for example, when accessing objects
- in a Realm produced externally. If you are simply building an app on Realm, it is not recommened 
- to use this method as an [RLMObjectSchema](RLMObjectSchema) is generated automatically for every [RLMObject](RLMObject) subclass.
- 
- Initialize an RLMObjectSchema with classname, objectClass, and an array of properties
- 
- @warning This method is useful only in specialized circumstances.
-
- @param objectClassName     The name of the class used to refer to objects of this type.
- @param objectClass         The objective-c class used when creating instances of this type.
- @param properties          An array RLMProperty describing the persisted properties for this type.
-
- @return    An initialized instance of RLMObjectSchema.
- */
-- (instancetype)initWithClassName:(NSString *)objectClassName objectClass:(Class)objectClass properties:(NSArray *)properties;
-@end
-
-@interface RLMProperty (Dynamic)
-/**
- This method is useful only in specialized circumstances, for example, in conjunction with 
- [RLMObjectSchema initWithClassName:objectClass:properties:]. If you are simply building an 
- app on Realm, it is not recommened to use this method.
- 
- Initialize an RLMProperty
- 
- @warning This method is useful only in specialized circumstances.
-
- @param name            The property name.
- @param type            The property type.
- @param objectClassName The object type used for Object and Array types.
-
- @return    An initialized instance of RLMProperty.
- */
-- (instancetype)initWithName:(NSString *)name
-                        type:(RLMPropertyType)type
-             objectClassName:(NSString *)objectClassName
-                     indexed:(BOOL)indexed
-                    optional:(BOOL)optional;
 @end
