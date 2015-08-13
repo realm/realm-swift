isabella232/realm-cocoa

Use indexInSource: for valueForKey:

Realm/RLMArray_Private.hpp
@@ -37,6 +37,8 @@ namespace realm {
 
 @protocol RLMFastEnumerable
 @property (nonatomic, readonly) RLMRealm *realm;
+@property (nonatomic, readonly) RLMObjectSchema *objectSchema;
+@property (nonatomic, readonly) NSUInteger count;
 
 - (NSUInteger)indexInSource:(NSUInteger)index;
 - (realm::TableView)tableView;
@@ -59,6 +61,8 @@ namespace realm {
 // LinkView backed RLMArray subclass
 //
 @interface RLMArrayLinkView : RLMArray <RLMFastEnumerable>
+@property (nonatomic, strong) RLMObjectSchema *objectSchema;
+
 + (instancetype)arrayWithObjectClassName:(NSString *)objectClassName
                                     view:(realm::LinkViewRef)view
                                    realm:(RLMRealm *)realm;

Realm/RLMResults_Private.h
@@ -20,8 +20,6 @@
 
 @class RLMObjectSchema;
 
-@interface RLMResults () {
-  @public
-    RLMObjectSchema *_objectSchema;
-}
+@interface RLMResults ()
+@property (nonatomic, strong) RLMObjectSchema *objectSchema;
 @end

realm/realm-swift

Realm/RLMArray_Private.hpp
@@ -48,6 +48,8 @@ struct RLMSortOrder {
 
 @protocol RLMFastEnumerable
 @property (nonatomic, readonly) RLMRealm *realm;
+@property (nonatomic, readonly) RLMObjectSchema *objectSchema;
+@property (nonatomic, readonly) NSUInteger count;
 
 - (NSUInteger)indexInSource:(NSUInteger)index;
 - (realm::TableView)tableView;
@@ -68,6 +70,8 @@ struct RLMSortOrder {
 // LinkView backed RLMArray subclass
 //
 @interface RLMArrayLinkView : RLMArray <RLMFastEnumerable>
+@property (nonatomic, unsafe_unretained) RLMObjectSchema *objectSchema;
+
 + (RLMArrayLinkView *)arrayWithObjectClassName:(NSString *)objectClassName
                                           view:(realm::LinkViewRef)view
                                          realm:(RLMRealm *)realm

Realm/RLMResults_Private.h
@@ -20,8 +20,6 @@
 
 @class RLMObjectSchema;
 
-@interface RLMResults () {
-  @public
-    RLMObjectSchema *_objectSchema;
-}
+@interface RLMResults ()
+@property (nonatomic, unsafe_unretained) RLMObjectSchema *objectSchema;
 @end