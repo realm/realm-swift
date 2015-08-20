zhoufusong/realm-cocoa

Added two methods to Realm (RealmSwift):
dynamicObjects(className: String) -> Results<DynamicObject>

dynamicObjectForPrimaryKey(className: String, key: AnyObject) -> DynamicObject?

Includes tests and documentation.

RealmSwift-swift1.2/Realm.swift
@@ -306,8 +306,8 @@ public final class Realm {
     /**
     Deletes the given objects from this Realm.
 
-    :param: object The objects to be deleted. This can be a `List<Object>`, `Results<Object>`,
-                   or any other enumerable SequenceType which generates Object.
+    :param: object The objects to be deleted. This can be a `List`, `Results`,
+                   or any other enumerable `SequenceType` which generates `Object`.
     */
     public func delete<S: SequenceType where S.Generator.Element: Object>(objects: S) {
         for obj in objects {
@@ -318,8 +318,8 @@ public final class Realm {
     /**
     Deletes the given objects from this Realm.
 
-    :param: object The objects to be deleted. This can be a `List<Object>`, `Results<Object>`,
-                   or any other enumerable SequenceType which generates Object.
+    :param: object The objects to be deleted. This can be a `List`, `Results`,
+                   or any other enumerable `SequenceType` which generates `Object`.
     
     :nodoc:
     */
@@ -330,8 +330,8 @@ public final class Realm {
     /**
     Deletes the given objects from this Realm.
 
-    :param: object The objects to be deleted. This can be a `List<Object>`, `Results<Object>`,
-                   or any other enumerable SequenceType which generates Object.
+    :param: object The objects to be deleted. This can be a `List`, `Results`,
+                   or any other enumerable `SequenceType` which generates `Object`.
     
     :nodoc:
     */
@@ -358,6 +358,23 @@ public final class Realm {
     public func objects<T: Object>(type: T.Type) -> Results<T> {
         return Results<T>(RLMGetObjects(rlmRealm, T.className(), nil))
     }
+    
+    /**
+    This method is useful only in specialized circumstances, for example, when building
+    components that integrate with Realm. If you are simply building an app on Realm, it is
+    recommended to use the typed method `objects(type:)`.
+    
+    Returns dynamic untyped objects for a given class name in the Realm
+    
+    :warning: This method is useful only in specialized circumstances.
+    
+    :param: className  The class name of the objects to be returned.
+    
+    :returns: All objects matching class name as dynamic objects
+    */
+    public func dynamicObjects(className: String) -> Results<DynamicObject> {
+        return Results<DynamicObject>(RLMGetObjects(rlmRealm, className, nil))
+    }
 
     /**
     Get an object with the given primary key.
@@ -375,6 +392,30 @@ public final class Realm {
     */
     public func objectForPrimaryKey<T: Object>(type: T.Type, key: AnyObject) -> T? {
         return unsafeBitCast(RLMGetObject(rlmRealm, type.className(), key), Optional<T>.self)
+    }
+    
+    /**
+    This method is useful only in specialized circumstances, for example, when building
+    components that integrate with Realm. If you are simply building an app on Realm, it is
+    recommended to use the typed method `objectForPrimaryKey(type:key:)`.
+    
+    Get a dynamic object with the given primary key.
+    
+    Returns `nil` if no object exists with the given primary key.
+    
+    This method requires that `primaryKey()` be overridden on the given subclass.
+    
+    :see: Object.primaryKey()
+    
+    :warning: This method is useful only in specialized circumstances.
+    
+    :param: className  The class name of the object to be returned.
+    :param: key        The primary key of the desired object.
+    
+    :returns: An object of type `type` or `nil` if an object with the given primary key does not exist.
+    */
+    public func dynamicObjectForPrimaryKey(className: String, key: AnyObject) -> DynamicObject? {
+        return unsafeBitCast(RLMGetObject(rlmRealm, className, key), Optional<DynamicObject>.self)
     }
 
 

realm/realm-swift

RealmSwift-swift1.2/Realm.swift
@@ -338,8 +338,8 @@ public final class Realm {
     /**
     Deletes the given objects from this Realm.
 
-    :param: object The objects to be deleted. This can be a `List<Object>`, `Results<Object>`,
-                   or any other enumerable SequenceType which generates Object.
+    :param: objects The objects to be deleted. This can be a `List`, `Results`,
+                    or any other enumerable `SequenceType` which generates `Object`.
     */
     public func delete<S: SequenceType where S.Generator.Element: Object>(objects: S) {
         for obj in objects {
@@ -350,8 +350,7 @@ public final class Realm {
     /**
     Deletes the given objects from this Realm.
 
-    :param: object The objects to be deleted. This can be a `List<Object>`, `Results<Object>`,
-                   or any other enumerable SequenceType which generates Object.
+    :param: objects The objects to be deleted. Must be `List<Object>`.
 
     :nodoc:
     */
@@ -362,8 +361,7 @@ public final class Realm {
     /**
     Deletes the given objects from this Realm.
 
-    :param: object The objects to be deleted. This can be a `List<Object>`, `Results<Object>`,
-                   or any other enumerable SequenceType which generates Object.
+    :param: objects The objects to be deleted. Must be `Results<Object>`.
 
     :nodoc:
     */
@@ -390,6 +388,23 @@ public final class Realm {
     public func objects<T: Object>(type: T.Type) -> Results<T> {
         return Results<T>(RLMGetObjects(rlmRealm, T.className(), nil))
     }
+    
+    /**
+    This method is useful only in specialized circumstances, for example, when building
+    components that integrate with Realm. If you are simply building an app on Realm, it is
+    recommended to use the typed method `objects(type:)`.
+    
+    Returns dynamic untyped objects for a given class name in the Realm
+    
+    :warning: This method is useful only in specialized circumstances.
+    
+    :param: className  The class name of the objects to be returned.
+    
+    :returns: All objects matching class name as dynamic objects
+    */
+    public func dynamicObjects(className: String) -> Results<DynamicObject> {
+        return Results<DynamicObject>(RLMGetObjects(rlmRealm, className, nil))
+    }
 
     /**
     Get an object with the given primary key.
@@ -407,6 +422,30 @@ public final class Realm {
     */
     public func objectForPrimaryKey<T: Object>(type: T.Type, key: AnyObject) -> T? {
         return unsafeBitCast(RLMGetObject(rlmRealm, type.className(), key), Optional<T>.self)
+    }
+    
+    /**
+    This method is useful only in specialized circumstances, for example, when building
+    components that integrate with Realm. If you are simply building an app on Realm, it is
+    recommended to use the typed method `objectForPrimaryKey(type:key:)`.
+    
+    Get a dynamic object with the given primary key.
+    
+    Returns `nil` if no object exists with the given primary key.
+    
+    This method requires that `primaryKey()` be overridden on the given subclass.
+    
+    :see: Object.primaryKey()
+    
+    :warning: This method is useful only in specialized circumstances.
+    
+    :param: className  The class name of the object to be returned.
+    :param: key        The primary key of the desired object.
+    
+    :returns: An object of type `type` or `nil` if an object with the given primary key does not exist.
+    */
+    public func dynamicObjectForPrimaryKey(className: String, key: AnyObject) -> DynamicObject? {
+        return unsafeBitCast(RLMGetObject(rlmRealm, className, key), Optional<DynamicObject>.self)
     }
 
 