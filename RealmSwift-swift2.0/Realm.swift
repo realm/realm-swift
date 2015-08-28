a2eef0c7c7fbeda42f637d4ac09e62606f70367b
JP Simard <jp@jpsim.com>
2015-08-28 14:41:20 -0700

    [SwiftLint] add .swiftlint.yml, address some style issues

--- RealmSwift-swift2.0/Realm.swift
+++ RealmSwift-swift2.0/Realm.swift
@@ -572,7 +572,7 @@ public final class Realm {
 extension Realm: Equatable { }
 
 /// Returns whether the two realms are equal.
-public func ==(lhs: Realm, rhs: Realm) -> Bool {
+public func == (lhs: Realm, rhs: Realm) -> Bool {
     return lhs.rlmRealm == rhs.rlmRealm
 }
 
