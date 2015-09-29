e55e946c8e81f38bba23a9d9c560247969338e7a
Thomas Goyne <tg@realm.io>
2015-09-29 15:42:46 -0700

    Update requiredProperties documentation to reflect that everything but Array supports nil now

--- Realm/RLMObject.h
+++ Realm/RLMObject.h
@@ -282,11 +282,13 @@ RLM_ASSUME_NONNULL_BEGIN
  Implement to return an array of property names that should not allow storing nil.
 
  By default, all properties of a type that support storing nil are considered optional properties.
- To require that an object in a Realm always have a non-nil value for a property, add the name of the property to the array returned from this method.
-
- Currently only String, Data, and Object properties support storing nil, and all other properties are implicitly treated as if they were required properties.
- Support for additional types will come in the future.
+ To require that an object in a Realm always have a non-nil value for a property,
+ add the name of the property to the array returned from this method.
  
+ Currently Object properties cannot be required. Array and NSNumber properties
+ can, but it makes little sense to do so: arrays do not support storing nil, and
+ if you want a non-optional number you should instead use the primitive type.
+
  @return    NSArray of property names that are required.
  */
 + (NSArray *)requiredProperties;
