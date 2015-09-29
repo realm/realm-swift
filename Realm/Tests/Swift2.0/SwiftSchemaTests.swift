1e29124e9bfa089d39bb8cf9f8c09f0995b16f66
Thomas Goyne <tg@realm.io>
2015-09-29 11:42:53 -0700

    Support using initWithValue: in Swift property defaults
    If an object has a property linking to another object and that property is
    initialized using `initWithValue:`, that initializer may get run during schema
    initialization before the linked class's schema is initialized and thus throw
    an exception. We don't actually use any of the property values of linked
    objects during schema init, so just do nothing in `initWithValue:` if the
    class's schema has not been initialized.

---
+++ Realm/Tests/Swift2.0/SwiftSchemaTests.swift
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2015 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+import XCTest
+import Realm
+
+class InitLinkedToClass: RLMObject {
+    dynamic var value = SwiftIntObject(value: [0])
+}
+
+class SwiftSchemaTests: RLMMultiProcessTestCase {
+    func testWorksAtAll() {
+        if isParent {
+            XCTAssertEqual(0, runChildAndWait(), "Tests in child process failed")
+        }
+    }
+
+    func testSchemaInitWithLinkedToObjectUsingInitWithValue() {
+        if isParent {
+            XCTAssertEqual(0, runChildAndWait(), "Tests in child process failed")
+            return
+        }
+
+        // registerClasses:count: happens to initialize the schemas in the same
+        // order as the classes appear in the array, so this initializes the
+        // schema for `InitLinkedToClass` before `IntObject` to verify that
+        // `initWithValue:` does not throw in that scenario
+        RLMSchema.registerClasses([InitLinkedToClass.self, SwiftIntObject.self], count: 2)
+    }
+}
