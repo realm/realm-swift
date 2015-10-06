279dae458c702a0e70b0e48827d160de0bf3757e
Marius Rackwitz <git@mariusrackwitz.de>
2015-10-06 11:21:17 +0200

    [Doc] Explain how to catch errors in Swift 1.2

--- RealmSwift-swift1.2/Error.swift
+++ RealmSwift-swift1.2/Error.swift
@@ -16,4 +16,22 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-// Intentionally left empty.
+// Intentionally left empty, because this exposes a type conforming to
+// Swift 2.0's ErrorType to be able to catch errors with the builtin
+// error handling in built variants of the Realm Swift binding targeting
+// these newer Swift versions.
+//
+// If you're using Swift 1.2, you will need to use error pointers instead
+// and can rely on the error's code and compare it to constants exposed in
+// RLMConstants.h to differentiate possible error causes, as seen in the
+// example below:
+//
+//     var error: NSError?
+//     let realm = Realm(configuration: Configuration.defaultConfiguration, error: &error)
+//     switch error.code {
+//     case RLMError.IncompatibleLockFile:
+//          #if !(TARGET_IPHONE_SIMULATOR)
+//             print("Make sure that the browser is not attached simultaneously.")
+//          #endif
+//          break
+//     }
