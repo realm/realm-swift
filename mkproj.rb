efe30b605986c9d1f84668bf2e2d478b109ee4bc
Boris Bügling <boris@icculus.org>
2015-10-09 16:10:19 +0200

    Use 'iOS Dynamic' scheme

--- mkproj.rb
+++ mkproj.rb
@@ -7,7 +7,7 @@ require 'xcodeproj'
 
 # Configuration
 compatibility_name = 'Carthage'
-schemes_to_build = ['iOS', 'OSX', 'RealmSwift']
+schemes_to_build = ['iOS Dynamic', 'OSX', 'RealmSwift']
 projects_to_build = ['Realm.xcodeproj', 'RealmSwift.xcodeproj']
 
 
