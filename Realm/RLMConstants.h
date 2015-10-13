a3a3bd1767e69e9c4c128aa22583ff77108ad563
JP Simard <jp@jpsim.com>
2015-10-13 15:08:07 -0700

    remove mention of CGFloat from docs

--- Realm/RLMConstants.h
+++ Realm/RLMConstants.h
@@ -33,9 +33,9 @@ typedef NS_ENUM(int32_t, RLMPropertyType) {
     RLMPropertyTypeInt    = 0,
     /** Boolean type: BOOL, bool, Bool (Swift) */
     RLMPropertyTypeBool   = 1,
-    /** Float type: CGFloat (32bit), float, Float (Swift) */
+    /** Float type: float, Float (Swift) */
     RLMPropertyTypeFloat  = 9,
-    /** Double type: CGFloat (64bit), double, Double (Swift) */
+    /** Double type: double, Double (Swift) */
     RLMPropertyTypeDouble = 10,
 
     ////////////////////////////////
