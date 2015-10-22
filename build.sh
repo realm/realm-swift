c04a422fbcc9e9325564a981740b8d69ae5d08de
Thomas Goyne <tg@realm.io>
2015-10-22 11:41:27 -0700

    Switch from Swift 2.0 to Swift 2.1

--- build.sh
+++ build.sh
@@ -734,7 +734,7 @@ case "$COMMAND" in
         cp -r $(dirname $0)/scripts ${OBJC}
         cd ${OBJC}
         REALM_SWIFT_VERSION=1.2 sh build.sh examples-ios
-        REALM_SWIFT_VERSION=2.0 sh build.sh examples-ios
+        REALM_SWIFT_VERSION=2.1 sh build.sh examples-ios
         sh build.sh examples-osx
         cd ..
         rm -rf ${OBJC}
@@ -745,7 +745,7 @@ case "$COMMAND" in
         cp -r $(dirname $0)/scripts ${SWIFT}
         cd ${SWIFT}
         REALM_SWIFT_VERSION=1.2 sh build.sh examples-ios-swift
-        REALM_SWIFT_VERSION=2.0 sh build.sh examples-ios-swift
+        REALM_SWIFT_VERSION=2.1 sh build.sh examples-ios-swift
         cd ..
         rm -rf ${SWIFT}
         ;;
@@ -759,9 +759,9 @@ case "$COMMAND" in
         move_to_clean_dir build/ios/Realm.framework xcode-6
         rm -rf build
 
-        REALM_SWIFT_VERSION=2.0 sh build.sh prelaunch-simulator
-        REALM_SWIFT_VERSION=2.0 sh build.sh test-ios-static
-        REALM_SWIFT_VERSION=2.0 sh build.sh ios-static
+        REALM_SWIFT_VERSION=2.1 sh build.sh prelaunch-simulator
+        REALM_SWIFT_VERSION=2.1 sh build.sh test-ios-static
+        REALM_SWIFT_VERSION=2.1 sh build.sh ios-static
         move_to_clean_dir build/ios/Realm.framework xcode-7
 
         zip --symlinks -r build/ios/realm-framework-ios.zip xcode-6 xcode-7
@@ -774,8 +774,8 @@ case "$COMMAND" in
         move_to_clean_dir build/ios-dynamic/Realm.framework xcode-6
         rm -rf build
 
-        REALM_SWIFT_VERSION=2.0 sh build.sh prelaunch-simulator
-        REALM_SWIFT_VERSION=2.0 sh build.sh ios-dynamic
+        REALM_SWIFT_VERSION=2.1 sh build.sh prelaunch-simulator
+        REALM_SWIFT_VERSION=2.1 sh build.sh ios-dynamic
         move_to_clean_dir build/ios-dynamic/Realm.framework xcode-7
 
         zip --symlinks -r build/ios-dynamic/realm-dynamic-framework-ios.zip xcode-6 xcode-7
@@ -783,7 +783,7 @@ case "$COMMAND" in
 
     "package-osx")
         cd tightdb_objc
-        REALM_SWIFT_VERSION=2.0 sh build.sh test-osx
+        REALM_SWIFT_VERSION=2.1 sh build.sh test-osx
 
         cd build/DerivedData/Realm/Build/Products/Release
         zip --symlinks -r realm-framework-osx.zip Realm.framework
@@ -804,7 +804,7 @@ case "$COMMAND" in
     "package-osx-swift")
         cd tightdb_objc
         REALM_SWIFT_VERSION=1.2 sh build.sh osx-swift
-        REALM_SWIFT_VERSION=2.0 sh build.sh osx-swift
+        REALM_SWIFT_VERSION=2.1 sh build.sh osx-swift
 
         cd build/osx
         zip --symlinks -r realm-swift-framework-osx.zip swift-1.2 swift-2.0
@@ -812,7 +812,7 @@ case "$COMMAND" in
 
     "package-watchos")
         cd tightdb_objc
-        REALM_SWIFT_VERSION=2.0 sh build.sh watchos
+        REALM_SWIFT_VERSION=2.1 sh build.sh watchos
 
         cd build/watchos
         zip --symlinks -r realm-framework-watchos.zip Realm.framework
@@ -820,7 +820,7 @@ case "$COMMAND" in
 
     "package-watchos-swift")
         cd tightdb_objc
-        REALM_SWIFT_VERSION=2.0 sh build.sh watchos-swift
+        REALM_SWIFT_VERSION=2.1 sh build.sh watchos-swift
 
         cd build/watchos
         zip --symlinks -r realm-swift-framework-watchos.zip RealmSwift.framework Realm.framework
