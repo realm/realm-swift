realm/realm-swift

commit 015ad2b7a738960a76ad24e3c0b93be2e6c8ee9c

Fix a crash when constructing a dynamic object from a Swift type with a RealmOptional property.
Fixes #2859.

RealmSwift-swift1.2/Object.swift
@@ -236,6 +236,11 @@ public class Object: RLMObjectBase, Equatable, Printable {
     internal func listForProperty(prop: RLMProperty) -> RLMListBase {
         return object_getIvar(self, prop.swiftIvar) as! RLMListBase
     }
+
+    // Helper for getting the optional object for a property
+    internal func optionalForProperty(prop: RLMProperty) -> RLMOptionalBase {
+        return object_getIvar(self, prop.swiftIvar) as! RLMOptionalBase
+    }
 }
 
 // MARK: Equatable
@@ -251,6 +256,7 @@ public func == <T: Object>(lhs: T, rhs: T) -> Bool {
 /// :nodoc:
 public final class DynamicObject: Object {
     private var listProperties = [String: List<DynamicObject>]()
+    private var optionalProperties = [String: RLMOptionalBase]()
 
     // Override to create List<DynamicObject> on access
     internal override func listForProperty(prop: RLMProperty) -> RLMListBase {
@@ -260,6 +266,17 @@ public final class DynamicObject: Object {
         let list = List<DynamicObject>()
         listProperties[prop.name] = list
         return list
+    }
+
+    // Override to create RealmOptional on access
+    internal override func optionalForProperty(prop: RLMProperty) -> RLMOptionalBase {
+        if let optional = optionalProperties[prop.name] {
+            return optional
+        }
+        let optional = RLMOptionalBase()
+        optional.property = prop
+        optionalProperties[prop.name] = optional
+        return optional
     }
 
     /// :nodoc:
@@ -315,6 +332,10 @@ public class ObjectUtil: NSObject {
 
     @objc private class func initializeListProperty(object: RLMObjectBase, property: RLMProperty, array: RLMArray) {
         (object as! Object).listForProperty(property)._rlmArray = array
+    }
+
+    @objc private class func initializeOptionalProperty(object: RLMObjectBase, property: RLMProperty) {
+        (object as! Object).optionalForProperty(property).object = object
     }
 
     @objc private class func getOptionalProperties(object: AnyObject) -> NSDictionary {

RealmSwift-swift2.0/Object.swift
@@ -250,6 +250,11 @@ public class Object: RLMObjectBase {
     internal func listForProperty(prop: RLMProperty) -> RLMListBase {
         return object_getIvar(self, prop.swiftIvar) as! RLMListBase
     }
+
+    // Helper for getting the optional object for a property
+    internal func optionalForProperty(prop: RLMProperty) -> RLMOptionalBase {
+        return object_getIvar(self, prop.swiftIvar) as! RLMOptionalBase
+    }
 }
 
 
@@ -258,6 +263,7 @@ public class Object: RLMObjectBase {
 /// :nodoc:
 public final class DynamicObject: Object {
     private var listProperties = [String: List<DynamicObject>]()
+    private var optionalProperties = [String: RLMOptionalBase]()
 
     // Override to create List<DynamicObject> on access
     internal override func listForProperty(prop: RLMProperty) -> RLMListBase {
@@ -267,6 +273,17 @@ public final class DynamicObject: Object {
         let list = List<DynamicObject>()
         listProperties[prop.name] = list
         return list
+    }
+
+    // Override to create RealmOptional on access
+    internal override func optionalForProperty(prop: RLMProperty) -> RLMOptionalBase {
+        if let optional = optionalProperties[prop.name] {
+            return optional
+        }
+        let optional = RLMOptionalBase()
+        optional.property = prop
+        optionalProperties[prop.name] = optional
+        return optional
     }
 
     /// :nodoc:
@@ -313,6 +330,10 @@ public class ObjectUtil: NSObject {
 
     @objc private class func initializeListProperty(object: RLMObjectBase, property: RLMProperty, array: RLMArray) {
         (object as! Object).listForProperty(property)._rlmArray = array
+    }
+
+    @objc private class func initializeOptionalProperty(object: RLMObjectBase, property: RLMProperty) {
+        (object as! Object).optionalForProperty(property).object = object
     }
 
     @objc private class func getOptionalProperties(object: AnyObject) -> NSDictionary {

Realm/RLMObjectBase.mm
@@ -438,6 +438,9 @@ Class RLMObjectUtilClass(BOOL isSwift) {
 + (void)initializeListProperty:(__unused RLMObjectBase *)object property:(__unused RLMProperty *)property array:(__unused RLMArray *)array {
 }
 
++ (void)initializeOptionalProperty:(__unused RLMObjectBase *)object property:(__unused RLMProperty *)property {
+}
+
 + (NSDictionary *)getOptionalProperties:(__unused id)obj {
     return nil;
 }

Realm/RLMObjectStore.mm
@@ -102,10 +102,8 @@ void RLMInitializeSwiftAccessorGenerics(__unsafe_unretained RLMObjectBase *const
                                                                      key:prop.name
                                                             parentSchema:object->_objectSchema];
             [RLMObjectUtilClass(YES) initializeListProperty:object property:prop array:array];
-        } else if (auto ivar = prop.swiftIvar) {
-            auto optional = static_cast<RLMOptionalBase *>(object_getIvar(object, ivar));
-            optional.object = object;
-            optional.property = prop;
+        } else if (prop.swiftIvar) {
+            [RLMObjectUtilClass(YES) initializeOptionalProperty:object property:prop];
         }
     }
 }

Realm/RLMObject_Private.h
@@ -78,6 +78,7 @@ FOUNDATION_EXTERN const NSUInteger RLMDescriptionMaxDepth;
 
 + (NSArray *)getGenericListPropertyNames:(id)obj;
 + (void)initializeListProperty:(RLMObjectBase *)object property:(RLMProperty *)property array:(RLMArray *)array;
++ (void)initializeOptionalProperty:(RLMObjectBase *)object property:(RLMProperty *)property;
 
 + (NSDictionary *)getOptionalProperties:(id)obj;
 + (NSArray *)requiredPropertiesForClass:(Class)cls;

commit 680d6bb7c5956a327a9a8d6a584711131dd12a6b

Ensure that the `property` property of the `RealmOptional` is initialized.
This fixes the tests that 015ad2b7a738960a76ad24e3c0b93be2e6c8ee9c broke.

RealmSwift-swift1.2/Object.swift
@@ -335,7 +335,9 @@ public class ObjectUtil: NSObject {
     }
 
     @objc private class func initializeOptionalProperty(object: RLMObjectBase, property: RLMProperty) {
-        (object as! Object).optionalForProperty(property).object = object
+        let optional = (object as! Object).optionalForProperty(property)
+        optional.property = property
+        optional.object = object
     }
 
     @objc private class func getOptionalProperties(object: AnyObject) -> NSDictionary {

RealmSwift-swift2.0/Object.swift
@@ -333,7 +333,9 @@ public class ObjectUtil: NSObject {
     }
 
     @objc private class func initializeOptionalProperty(object: RLMObjectBase, property: RLMProperty) {
-        (object as! Object).optionalForProperty(property).object = object
+        let optional = (object as! Object).optionalForProperty(property)
+        optional.property = property
+        optional.object = object
     }
 
     @objc private class func getOptionalProperties(object: AnyObject) -> NSDictionary {