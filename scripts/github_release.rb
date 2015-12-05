32840431a42d104e26b805c0d1e2e597ed818e3f
Mark Rowe <mar@realm.io>
2015-12-05 01:13:19 -0800

    Remove unnecessary `-v` that I had added while debugging.

--- scripts/github_release.rb
+++ scripts/github_release.rb
@@ -16,7 +16,7 @@ SWIFT_ZIP = BUILD + "realm-swift-#{VERSION}.zip"
 CARTHAGE_ZIP = BUILD + 'Carthage.framework.zip'
 
 puts 'Creating Carthage release zip'
-system('carthage', 'build', '--no-skip-current', '-v') || exit(1)
+system('carthage', 'build', '--no-skip-current') || exit(1)
 system('carthage', 'archive', 'Realm', '--output', CARTHAGE_ZIP.to_path) || exit(1)
 system('carthage', 'archive', 'RealmSwift', '--output', CARTHAGE_ZIP.to_path) || exit(1)
 
