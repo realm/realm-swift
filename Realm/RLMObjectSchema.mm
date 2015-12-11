44dd870ac0bb0e10057a8a7c56907be176e97c57
Adam Fish <af@realm.io>
2015-12-11 14:59:13 -0800

    Added warning for developer when using primary keys. Explains the current behavior and the expected behavior once primary key support is added.
    Addresses: https://github.com/realm/realm-sync/issues/106

--- Realm/RLMObjectSchema.mm
+++ Realm/RLMObjectSchema.mm
@@ -70,6 +70,7 @@ using namespace realm;
 }
 
 - (void)setPrimaryKeyProperty:(RLMProperty *)primaryKeyProperty {
+    NSLog(@"\n************WARNING:************\n\nPrimary keys do not work as intended with sync yet.\n\nTwo objects with the same primary key created on different devices, will be treated as seperate objects versus the expected behavior of merging the objects together when each device syncs with the other.\n\n********************************");
     _primaryKeyProperty.isPrimary = NO;
     primaryKeyProperty.isPrimary = YES;
     _primaryKeyProperty = primaryKeyProperty;
