0b175882d6927bc07548598cc2165dd96f1c24c0
JP Simard <jp@jpsim.com>
2016-01-05 11:43:39 -0800

    grep -q

--- build.sh
+++ build.sh
@@ -414,7 +414,7 @@ case "$COMMAND" in
 
         SWIFT_VERSION_FILE="RealmSwift/SwiftVersion.swift"
         CONTENTS="let swiftLanguageVersion = \"$version\""
-        if ! grep "$CONTENTS" "$SWIFT_VERSION_FILE"; then
+        if ! grep -q "$CONTENTS" "$SWIFT_VERSION_FILE"; then
             echo "$CONTENTS" > "$SWIFT_VERSION_FILE"
         fi
 
