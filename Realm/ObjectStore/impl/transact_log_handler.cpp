f57b1ed48066a288a9d99c6e396d6fd02c3caacf
Thomas Goyne <tg@realm.io>
2016-01-05 11:20:00 -0800

    Rearrange some lines of code

--- Realm/ObjectStore/impl/transact_log_handler.cpp
+++ Realm/ObjectStore/impl/transact_log_handler.cpp
@@ -421,10 +421,10 @@ public:
     bool set_link(size_t col, size_t row, size_t, size_t) { return mark_dirty(row, col); }
     bool set_null(size_t col, size_t row) { return mark_dirty(row, col); }
     bool nullify_link(size_t col, size_t row, size_t) { return mark_dirty(row, col); }
-    bool insert_substring(size_t col, size_t row, size_t, StringData) { return mark_dirty(row, col); }
-    bool erase_substring(size_t col, size_t row, size_t, size_t) { return mark_dirty(row, col); }
     bool set_int_unique(size_t col, size_t row, int_fast64_t) { return mark_dirty(row, col); }
     bool set_string_unique(size_t col, size_t row, StringData) { return mark_dirty(row, col); }
+    bool insert_substring(size_t col, size_t row, size_t, StringData) { return mark_dirty(row, col); }
+    bool erase_substring(size_t col, size_t row, size_t, size_t) { return mark_dirty(row, col); }
 };
 } // anonymous namespace
 
