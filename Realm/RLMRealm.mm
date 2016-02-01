samritchie/realm-cocoa

Add support for running queries asynchronously

Realm/RLMRealm.mm
@@ -60,14 +60,17 @@ void RLMDisableSyncToDisk() {
 }
 
 // Notification Token
-@interface RLMNotificationToken ()
+@interface RLMRealmNotificationToken : RLMNotificationToken
 @property (nonatomic, strong) RLMRealm *realm;
 @property (nonatomic, copy) RLMNotificationBlock block;
 @end
 
-@implementation RLMNotificationToken
-- (void)dealloc
-{
+@implementation RLMRealmNotificationToken
+- (void)stop {
+    [_realm removeNotification:self];
+}
+
+- (void)dealloc {
     if (_realm || _block) {
         NSLog(@"RLMNotificationToken released without unregistering a notification. You must hold "
               @"on to the RLMNotificationToken returned from addNotificationBlock and call "
@@ -229,14 +232,14 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
     return RLMAutorelease(realm);
 }
 
-+ (SharedRealm)openSharedRealm:(Realm::Config const&)config error:(NSError **)outError {
+void RLMRealmTranslateException(NSError **error) {
     try {
-        return Realm::get_shared_realm(config);
+        throw;
     }
     catch (RealmFileException const& ex) {
         switch (ex.kind()) {
             case RealmFileException::Kind::PermissionDenied:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFilePermissionDenied, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFilePermissionDenied, ex), error);
                 break;
             case RealmFileException::Kind::IncompatibleLockFile: {
                 NSString *err = @"Realm file is currently open in another process "
@@ -245,25 +248,34 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
                                  "architecture. For sharing files between the Realm "
                                  "Browser and an iOS simulator, this means that you "
                                  "must use a 64-bit simulator.";
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorIncompatibleLockFile, File::PermissionDenied(err.UTF8String, "FIXME: ex.get_path()")), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorIncompatibleLockFile, File::PermissionDenied(err.UTF8String, "FIXME: ex.get_path()")), error);
                 break;
             }
             case RealmFileException::Kind::Exists:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileExists, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileExists, ex), error);
                 break;
             case RealmFileException::Kind::AccessError:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileAccessError, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileAccessError, ex), error);
                 break;
             default:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, ex), error);
                 break;
         }
     }
     catch (std::system_error const& ex) {
-        RLMSetErrorOrThrow(RLMMakeError(ex), outError);
+        RLMSetErrorOrThrow(RLMMakeError(ex), error);
     }
     catch (const std::exception &exp) {
-        RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, exp), outError);
+        RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, exp), error);
+    }
+}
+
++ (SharedRealm)openSharedRealm:(Realm::Config const&)config error:(NSError **)outError {
+    try {
+        return Realm::get_shared_realm(config);
+    }
+    catch (...) {
+        RLMRealmTranslateException(outError);
     }
     return nullptr;
 }
@@ -323,6 +335,8 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
         config.migration_function = [](SharedRealm, SharedRealm) { };
     }
 
+    bool beganReadTransaction = false;
+
     // protects the realm cache and accessors cache
     static id initLock = [NSObject new];
     @synchronized(initLock) {
@@ -339,6 +353,8 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
             }
         }
         else {
+            beganReadTransaction = !realm->_realm->is_in_read_transaction();
+
             try {
                 // set/align schema or perform migration if needed
                 RLMSchema *schema = [configuration.customSchema copy];
@@ -370,7 +386,9 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
 
     if (!readOnly) {
         // initializing the schema started a read transaction, so end it
-        [realm invalidate];
+        if (beganReadTransaction) {
+            [realm invalidate];
+        }
         realm->_realm->m_binding_context = RLMCreateBindingContext(realm);
     }
 
@@ -402,7 +420,7 @@ static void CheckReadWrite(RLMRealm *realm, NSString *msg=@"Cannot write to a re
         _notificationHandlers = [NSHashTable hashTableWithOptions:NSPointerFunctionsWeakMemory];
     }
 
-    RLMNotificationToken *token = [[RLMNotificationToken alloc] init];
+    RLMRealmNotificationToken *token = [[RLMRealmNotificationToken alloc] init];
     token.realm = self;
     token.block = block;
     [_notificationHandlers addObject:token];
@@ -411,10 +429,10 @@ static void CheckReadWrite(RLMRealm *realm, NSString *msg=@"Cannot write to a re
 
 - (void)removeNotification:(RLMNotificationToken *)token {
     [self verifyThread];
-    if (token) {
+    if (auto realmToken = RLMDynamicCast<RLMRealmNotificationToken>(token)) {
         [_notificationHandlers removeObject:token];
-        token.realm = nil;
-        token.block = nil;
+        realmToken.realm = nil;
+        realmToken.block = nil;
     }
 }
 
@@ -422,7 +440,7 @@ static void CheckReadWrite(RLMRealm *realm, NSString *msg=@"Cannot write to a re
     NSAssert(!self.readOnly, @"Read-only realms do not have notifications");
 
     // call this realms notification blocks
-    for (RLMNotificationToken *token in [_notificationHandlers allObjects]) {
+    for (RLMRealmNotificationToken *token in [_notificationHandlers allObjects]) {
         if (token.block) {
             token.block(notification, self);
         }

realm/realm-swift

Realm/RLMRealm.mm
@@ -60,14 +60,17 @@ void RLMDisableSyncToDisk() {
 }
 
 // Notification Token
-@interface RLMNotificationToken ()
+@interface RLMRealmNotificationToken : RLMNotificationToken
 @property (nonatomic, strong) RLMRealm *realm;
 @property (nonatomic, copy) RLMNotificationBlock block;
 @end
 
-@implementation RLMNotificationToken
-- (void)dealloc
-{
+@implementation RLMRealmNotificationToken
+- (void)stop {
+    [_realm removeNotification:self];
+}
+
+- (void)dealloc {
     if (_realm || _block) {
         NSLog(@"RLMNotificationToken released without unregistering a notification. You must hold "
               @"on to the RLMNotificationToken returned from addNotificationBlock and call "
@@ -226,14 +229,14 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
     return RLMAutorelease(realm);
 }
 
-+ (SharedRealm)openSharedRealm:(Realm::Config const&)config error:(NSError **)outError {
+void RLMRealmTranslateException(NSError **error) {
     try {
-        return Realm::get_shared_realm(config);
+        throw;
     }
     catch (RealmFileException const& ex) {
         switch (ex.kind()) {
             case RealmFileException::Kind::PermissionDenied:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFilePermissionDenied, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFilePermissionDenied, ex), error);
                 break;
             case RealmFileException::Kind::IncompatibleLockFile: {
                 NSString *err = @"Realm file is currently open in another process "
@@ -243,28 +246,40 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
                                  "Browser and an iOS simulator, this means that you "
                                  "must use a 64-bit simulator.";
                 RLMSetErrorOrThrow(RLMMakeError(RLMErrorIncompatibleLockFile,
-                                                File::PermissionDenied(err.UTF8String, ex.path())), outError);
+                                                File::PermissionDenied(err.UTF8String, ex.path())), error);
                 break;
             }
             case RealmFileException::Kind::NotFound:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileNotFound, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileNotFound, ex), error);
+                break;
+            case RealmFileException::Kind::Exists:
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileExists, ex), error);
                 break;
             case RealmFileException::Kind::AccessError:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileAccess, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileAccess, ex), error);
                 break;
             case RealmFileException::Kind::FormatUpgradeRequired:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileFormatUpgradeRequired, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFileFormatUpgradeRequired, ex), error);
                 break;
             default:
-                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, ex), outError);
+                RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, ex), error);
                 break;
         }
     }
     catch (std::system_error const& ex) {
-        RLMSetErrorOrThrow(RLMMakeError(ex), outError);
+        RLMSetErrorOrThrow(RLMMakeError(ex), error);
     }
     catch (const std::exception &exp) {
-        RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, exp), outError);
+        RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, exp), error);
+    }
+}
+
++ (SharedRealm)openSharedRealm:(Realm::Config const&)config error:(NSError **)outError {
+    try {
+        return Realm::get_shared_realm(config);
+    }
+    catch (...) {
+        RLMRealmTranslateException(outError);
     }
     return nullptr;
 }
@@ -324,6 +339,8 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
         config.migration_function = [](SharedRealm, SharedRealm) { };
     }
 
+    bool beganReadTransaction = false;
+
     // protects the realm cache and accessors cache
     static id initLock = [NSObject new];
     @synchronized(initLock) {
@@ -340,6 +357,8 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
             }
         }
         else {
+            beganReadTransaction = !realm->_realm->is_in_read_transaction();
+
             try {
                 // set/align schema or perform migration if needed
                 RLMSchema *schema = [configuration.customSchema copy];
@@ -371,7 +390,9 @@ static void RLMRealmSetSchemaAndAlign(RLMRealm *realm, RLMSchema *targetSchema) 
 
     if (!readOnly) {
         // initializing the schema started a read transaction, so end it
-        [realm invalidate];
+        if (beganReadTransaction) {
+            [realm invalidate];
+        }
         realm->_realm->m_binding_context = RLMCreateBindingContext(realm);
     }
 
@@ -406,7 +427,7 @@ static void CheckReadWrite(RLMRealm *realm, NSString *msg=@"Cannot write to a re
         _notificationHandlers = [NSHashTable hashTableWithOptions:NSPointerFunctionsWeakMemory];
     }
 
-    RLMNotificationToken *token = [[RLMNotificationToken alloc] init];
+    RLMRealmNotificationToken *token = [[RLMRealmNotificationToken alloc] init];
     token.realm = self;
     token.block = block;
     [_notificationHandlers addObject:token];
@@ -415,10 +436,10 @@ static void CheckReadWrite(RLMRealm *realm, NSString *msg=@"Cannot write to a re
 
 - (void)removeNotification:(RLMNotificationToken *)token {
     [self verifyThread];
-    if (token) {
+    if (auto realmToken = RLMDynamicCast<RLMRealmNotificationToken>(token)) {
         [_notificationHandlers removeObject:token];
-        token.realm = nil;
-        token.block = nil;
+        realmToken.realm = nil;
+        realmToken.block = nil;
     }
 }
 
@@ -426,7 +447,7 @@ static void CheckReadWrite(RLMRealm *realm, NSString *msg=@"Cannot write to a re
     NSAssert(!self.readOnly, @"Read-only realms do not have notifications");
 
     // call this realms notification blocks
-    for (RLMNotificationToken *token in [_notificationHandlers allObjects]) {
+    for (RLMRealmNotificationToken *token in [_notificationHandlers allObjects]) {
         if (token.block) {
             token.block(notification, self);
         }
@@ -458,6 +479,10 @@ static void CheckReadWrite(RLMRealm *realm, NSString *msg=@"Cannot write to a re
     try {
         _realm->commit_transaction();
         return YES;
+    }
+    catch (File::AccessError const& ex) {
+        RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, ex), outError);
+        return NO;
     }
     catch (std::exception const& ex) {
         RLMSetErrorOrThrow(RLMMakeError(RLMErrorFail, ex), outError);