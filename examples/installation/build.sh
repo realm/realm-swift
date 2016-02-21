b0d3973e85f2a637c5223bdd16099ef3726e9eae
JP Simard <jp@jpsim.com>
2016-02-21 14:47:32 -0800

    add watchOS Swift CocoaPods framework installation example

--- examples/installation/build.sh
+++ examples/installation/build.sh
@@ -28,6 +28,7 @@ command:
   test-watchos-objc-cocoapods:     tests watchOS Objective-C CocoaPods example.
   test-watchos-objc-carthage:      tests watchOS Objective-C Carthage example.
   test-watchos-swift-dynamic:      tests watchOS Swift dynamic example.
+  test-watchos-swift-cocoapods:    tests watchOS Swift CocoaPods example.
   test-watchos-swift-carthage:     tests watchOS Swift Carthage example.
 EOF
 }
@@ -109,7 +110,7 @@ case "$COMMAND" in
         ;;
 
     "test-xcode7")
-        for target in ios-swift-dynamic ios-swift-cocoapods osx-swift-dynamic ios-swift-carthage osx-swift-carthage watchos-objc-dynamic test-watchos-objc-cocoapods test-watchos-objc-carthage watchos-swift-dynamic test-watchos-swift-carthage; do
+        for target in ios-swift-dynamic ios-swift-cocoapods osx-swift-dynamic ios-swift-carthage osx-swift-carthage watchos-objc-dynamic test-watchos-objc-cocoapods test-watchos-objc-carthage watchos-swift-dynamic test-watchos-swift-cocoapods test-watchos-swift-carthage; do
             REALM_SWIFT_VERSION=2.1.1 ./build.sh test-$target || exit 1
         done
         ;;
@@ -180,6 +181,10 @@ case "$COMMAND" in
 
     "test-watchos-swift-dynamic")
         xctest watchos swift-$REALM_SWIFT_VERSION DynamicExample
+        ;;
+
+    "test-watchos-swift-cocoapods")
+        xctest watchos swift-$REALM_SWIFT_VERSION CocoaPodsExample
         ;;
 
     "test-watchos-swift-carthage")
