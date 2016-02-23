a183f2f415fb846c1ee369224656bcdc2d7cd911
Simon Ask Ulsnes <su@realm.io>
2016-02-23 13:10:47 +0100

    Updated Draw demo with crypto-based auth.

--- examples/ios/objc/Draw/AppDelegate.m
+++ examples/ios/objc/Draw/AppDelegate.m
@@ -20,6 +20,9 @@
 #import <Realm/Realm.h>
 #import "DrawView.h"
 
+static NSString* identity = @"ewogICAgImlkZW50aXR5IjogImRyYXdfZGVtbyIsCiAgICAiYWNjZXNzIjogWyJkb3dubG9hZCIsICJ1cGxvYWQiXSwKICAgICJhcHBfaWQiOiAiaW8ucmVhbG0uRHJhdyIsCiAgICAiZXhwaXJlcyI6IG51bGwsCiAgICAidGltZXN0YW1wIjogMTQ1NjE1NTQzNgp9Cg==";
+static NSString* signature = @"WlgbZ5kRWddefABP/DnrK02s6xCTvl19L2eEuK1xQn106aVZxAea21I3Y7vA0umUkfjL2LHJmBU6Oh0peqyWOkLS/9EtWxc5GH5LFRtrhvRiL6WJR2+SIFWGIwtCspW+ChzTabU9+pOt3o2CwS68OWvEKW9ZLMiIvCiTZVq4Fe/Cb0NEmgcuc0sin41KveyRUD2EVQxFfASRdeUWPmLZgCzfn/olrrW+0nFBLMvN/MxkdownZzIeXp6tvI5LgAPlMXysmwAF2ORZtFLKghLJU/n92HxayPck2CPmWpXHIrETd4G2LYh18/kluVPdVbJVk7FegMZH2suuyXRb72cDvQ==";
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -28,7 +31,8 @@
     
     RLMRealmConfiguration *configuration = [RLMRealmConfiguration defaultConfiguration];
     configuration.syncServerURL = [NSURL URLWithString:@"realm://hydrogen.fr.sync.realm.io/draw/demo"];
-    configuration.syncIdentity = @"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    configuration.syncIdentity = identity;
+    configuration.syncSignature = signature;
     [RLMRealmConfiguration setDefaultConfiguration:configuration];
 
     [RLMRealm setGlobalSynchronizationLoggingLevel:RLMSyncLogLevelVerbose];
