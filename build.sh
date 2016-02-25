70778c7e1ad8605cfff43adb6313bcc8f82375ab
JP Simard <jp@jpsim.com>
2016-02-25 10:36:16 -0800

    use `getconf DARWIN_USER_TEMP_DIR` rather than `$TMPDIR`
    $TMPDIR isn't always available. Addresses #3258.

--- build.sh
+++ build.sh
@@ -281,7 +281,7 @@ fi
 
 download_core() {
     echo "Downloading dependency: core ${REALM_CORE_VERSION}"
-    TMP_DIR="$TMPDIR/core_bin"
+    TMP_DIR="$(getconf DARWIN_USER_TEMP_DIR)/core_bin"
     mkdir -p "${TMP_DIR}"
     CORE_TMP_TAR="${TMP_DIR}/core-${REALM_CORE_VERSION}.tar.bz2.tmp"
     CORE_TAR="${TMP_DIR}/core-${REALM_CORE_VERSION}.tar.bz2"
@@ -858,7 +858,7 @@ case "$COMMAND" in
             exit 1 # Early exit if not a fat binary
         fi
 
-        TEMPDIR=$(mktemp -d $TMPDIR/realm-bitcode-check.XXXX)
+        TEMPDIR=$(mktemp -d $(getconf DARWIN_USER_TEMP_DIR)/realm-bitcode-check.XXXX)
 
         for arch in $archs; do
             lipo -thin "$arch" "$BINARY" -output "$TEMPDIR/$arch"
@@ -1084,7 +1084,7 @@ case "$COMMAND" in
 
     "package-release")
         LANG="$2"
-        TEMPDIR=$(mktemp -d $TMPDIR/realm-release-package-${LANG}.XXXX)
+        TEMPDIR=$(mktemp -d $(getconf DARWIN_USER_TEMP_DIR)/realm-release-package-${LANG}.XXXX)
 
         cd tightdb_objc
         VERSION=$(sh build.sh get-version)
