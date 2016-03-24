0b7003d24f7fed21e7b5e127a56b98624e72d9d6
Mark Rowe <mar@realm.io>
2016-03-23 18:02:39 -0700

    Keep SwiftLint happy.

--- RealmSwift-swift2.0/Tests/RealmTests.swift
+++ RealmSwift-swift2.0/Tests/RealmTests.swift
@@ -100,7 +100,8 @@ class RealmTests: TestCase {
     #if DEBUG
     func testFileFormatUpgradeRequiredButDisabled() {
         var config = Realm.Configuration()
-        var bundledRealmPath = NSBundle(forClass: RealmTests.self).pathForResource("fileformat-pre-null.realm", ofType: nil)!
+        var bundledRealmPath = NSBundle(forClass: RealmTests.self).pathForResource("fileformat-pre-null.realm",
+                                                                                   ofType: nil)!
         try! NSFileManager.defaultManager.copyItemAtPath(bundledRealmPath, toPath: config.path)
         config.disableFormatUpgrade = true
         assertFails(Error.FileFormatUpgradeRequired) {
