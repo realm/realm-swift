206d612a9e1a5673824c987e75d32130dbdfad01
Marius Rackwitz <git@mariusrackwitz.de>
2016-04-15 13:43:10 +0200

    [Doc] Explain threading and runloop requirement

--- RealmSwift-swift2.0/Realm.swift
+++ RealmSwift-swift2.0/Realm.swift
@@ -445,12 +445,14 @@ public final class Realm {
     Add a notification handler for changes in this Realm.
 
     Notification handlers are called after each write transaction is committed,
-    either on the current thread or other threads. The block is called on the
-    same thread as they were added on, and can only be added on threads which
-    are currently within a run loop. Unless you are specifically creating and
-    running a run loop on a background thread, this normally will only be the
-    main thread.
-     
+    independent from the thread or process.
+
+    The block is called on the same thread as it was added on, and can only
+    be added on threads which are currently within a run loop. Unless you are
+    specifically creating and running a run loop on a background thread, this
+    normally will only be the main thread.
+
+
     You must retain the returned token for as long as you want updates to continue
     to be sent to the block. To stop receiving updates, call stop() on the token.
 
