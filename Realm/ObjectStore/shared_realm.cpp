da37196971c8668983b65fa881a6f7384036bf79
kishikawa katsumi <kishikawakatsumi@mac.com>
2016-04-21 16:42:48 +0900

    Delete the Realm file then re-create with new schema if migration is required

--- Realm/ObjectStore/shared_realm.cpp
+++ Realm/ObjectStore/shared_realm.cpp
@@ -179,7 +179,20 @@ Group *Realm::read_group()
 
 SharedRealm Realm::get_shared_realm(Config config)
 {
-    return RealmCoordinator::get_coordinator(config.path)->get_realm(std::move(config));
+    auto delete_realm_if_migration_needed = config.delete_realm_if_migration_needed;
+    auto coordinator = RealmCoordinator::get_coordinator(config.path);
+    try {
+        return coordinator->get_realm(std::move(config));
+    }
+    catch (SchemaMismatchException const& ex) {
+        if (delete_realm_if_migration_needed) {
+            coordinator->clear_cache();
+            if (remove(coordinator->get_path().c_str()) == 0) {
+                return coordinator->get_realm();
+            }
+        }
+        throw;
+    }
 }
 
 void Realm::update_schema(std::unique_ptr<Schema> schema, uint64_t version)
