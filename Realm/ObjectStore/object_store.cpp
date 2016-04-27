480e30ac94061d98219e39d0cb96536619f11fb7
JP Simard <jp@jpsim.com>
2016-04-27 09:38:56 +0200

    revert to using vector of properties
    making a copy before setting the name to the empty string to support
    multiple objects with properties of the same name

--- Realm/ObjectStore/object_store.cpp
+++ Realm/ObjectStore/object_store.cpp
@@ -26,7 +26,6 @@
 #include <realm/util/assert.hpp>
 
 #include <string.h>
-#include <unordered_map>
 
 using namespace realm;
 
@@ -253,9 +252,9 @@ static void copy_property_values(const Property& source, const Property& destina
 // set references to tables on targetSchema and create/update any missing or out-of-date tables
 // if update existing is true, updates existing tables, otherwise validates existing tables
 // NOTE: must be called from within write transaction
-std::unordered_map<std::string, Property> ObjectStore::create_tables(Group *group, Schema &target_schema, bool update_existing) {
+std::vector<Property> ObjectStore::create_tables(Group *group, Schema &target_schema, bool update_existing) {
     // properties to delete
-    std::unordered_map<std::string, Property> to_delete;
+    std::vector<Property> to_delete;
 
     // first pass to create missing tables
     std::vector<ObjectSchema *> to_update;
@@ -301,7 +300,8 @@ std::unordered_map<std::string, Property> ObjectStore::create_tables(Group *grou
             auto target_prop = target_object_schema->property_for_name(current_prop.name);
             // mark property for deletion
             if (!target_prop) {
-                to_delete[current_prop.name] = current_prop;
+                auto original_property = current_prop;
+                to_delete.push_back(original_property);
                 // Set to empty string to prevent `property_for_name` from matching properties marked for deletion
                 current_prop.name = "";
             }
@@ -371,17 +371,20 @@ std::unordered_map<std::string, Property> ObjectStore::create_tables(Group *grou
     return to_delete;
 }
 
-void ObjectStore::remove_properties(Group *group, Schema &target_schema, std::unordered_map<std::string, Property> to_delete) {
+void ObjectStore::remove_properties(Group *group, Schema &target_schema, std::vector<Property> to_delete) {
     for (auto& target_object_schema : target_schema) {
         TableRef table = table_for_object_type(group, target_object_schema.name);
         ObjectSchema current_schema(group, target_object_schema.name);
         size_t deleted = 0;
         for (auto& current_prop : current_schema.properties) {
             current_prop.table_column -= deleted;
-            if (to_delete.find(current_prop.name) != to_delete.end()) {
-                table->remove_column(current_prop.table_column);
-                ++deleted;
-                current_prop.table_column = npos;
+            for (auto& single_to_delete : to_delete) {
+                if (current_prop.name == single_to_delete.name &&
+                    current_prop.object_type == single_to_delete.object_type) {
+                    table->remove_column(current_prop.table_column);
+                    ++deleted;
+                    current_prop.table_column = npos;
+                }
             }
         }
     }
