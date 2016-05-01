d0df93e3bc5e3e699c35e63d41cc4c2e02224884
Mark Rowe <mrowe@bdash.net.nz>
2016-05-01 13:11:45 -0700

    Have `Results` default its copy constructor.
    The custom implementation was an attempt to ensure that
    `m_has_used_table_view` and `m_wants_background_updates` had appropriate
    intial values. Thomas pointed out that we can remove the reliance on the
    initial values by ensure that `prepare_async` sets
    `m_wants_background_updates`, removing the need for a custom copy
    constructor.

--- Realm/ObjectStore/results.cpp
+++ Realm/ObjectStore/results.cpp
@@ -81,19 +81,7 @@ Results::Results(SharedRealm r, SortOrder s, TableView tv)
     REALM_ASSERT(m_sort.column_indices.size() == m_sort.ascending.size());
 }
 
-Results::Results(const Results& other)
-: m_realm(other.m_realm)
-, m_query(other.m_query)
-, m_table_view(other.m_table_view)
-, m_link_view(other.m_link_view)
-, m_table(other.m_table)
-, m_sort(other.m_sort)
-, m_notifier()
-, m_mode(other.m_mode)
-, m_has_used_table_view(false)
-, m_wants_background_updates(true)
-{
-}
+Results::Results(const Results& other) = default;
 
 #if 0
 // FIXME: TableViewBase::operator= is missing from the core static library.
@@ -519,6 +507,7 @@ void Results::prepare_async()
     }
 
     if (!m_notifier) {
+        m_wants_background_updates = true;
         m_notifier = std::make_shared<_impl::ResultsNotifier>(*this);
         _impl::RealmCoordinator::register_notifier(m_notifier);
     }
