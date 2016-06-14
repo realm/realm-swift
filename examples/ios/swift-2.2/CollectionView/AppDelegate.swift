5dc9543d532c6f87e32a7f99495ba498c6d14311
Marius Rackwitz <git@mariusrackwitz.de>
2016-06-14 13:43:27 -0700

    [Examples ios swift] Add CollectionView example app target

---
+++ examples/ios/swift-2.2/CollectionView/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CollectionView
+//
+//  Created by Marius Rackwitz on 14.6.16.
+//  Copyright © 2016 Realm. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        return true
+    }
+}
+
