6d8a22401731ce2c58433c8983806a30bceb833c
Jaden Geller <jg@realm.io>
2016-08-30 11:07:38 -0700

    Fix force cast to incorrect type `URL` instead of `NSString`

--- RealmSwift/Tests/TestCase.swift
+++ RealmSwift/Tests/TestCase.swift
@@ -96,7 +96,7 @@ class TestCase: XCTestCase {
         // Verify that there are no remaining realm files after the test
         let parentDir = (testDir as NSString).deletingLastPathComponent
         for url in FileManager().enumerator(atPath: parentDir)! {
-            let url = url as! URL
+            let url = url as! NSString
             XCTAssertNotEqual(url.pathExtension, "realm", "Lingering realm file at \(parentDir)/\(url)")
             assert(url.pathExtension != "realm")
         }
