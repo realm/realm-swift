ddcdd6965d970ea09cf8adbe0ebc397548826f3d
JP Simard <jp@jpsim.com>
2016-09-12 13:52:50 -0700

    re-add set -o/-e

--- scripts/reset-simulators.sh
+++ scripts/reset-simulators.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+set -o pipefail
+set -e
+
 source "$(dirname "$0")/swift-version.sh"
 
 while pgrep -q Simulator; do
     # Kill all the current simulator processes as they may be from a
     # different Xcode version
-    pkill Simulator 2>/dev/null
+    pkill Simulator 2>/dev/null || true
     # CoreSimulatorService doesn't exit when sent SIGTERM
-    pkill -9 Simulator 2>/dev/null
+    pkill -9 Simulator 2>/dev/null || true
 done
 
 # Shut down booted simulators
@@ -34,5 +37,5 @@ while xcrun simctl list devices -j | jq -c -r '.devices | flatten | map(select(.
 done
 
 # Wait until the simulator is fully booted by waiting for it to launch SpringBoard
-xcrun simctl launch booted com.apple.springboard
+xcrun simctl launch booted com.apple.springboard || true
 echo "simulator booted"
