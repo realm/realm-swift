d56997e8bb89faf08d1f96a9d5d8932db2f1a223
JP Simard <jp@jpsim.com>
2016-09-13 14:40:35 -0700

    add "Abort trap: 6" to build.sh Xcode error detection

--- build.sh
+++ build.sh
@@ -921,7 +921,7 @@ EOM
 
             failed=0
             sh build.sh verify-$target 2>&1 | tee build/build.log | xcpretty -r junit -o build/reports/junit.xml || failed=1
-            if [ "$failed" = "1" ] && cat build/build.log | grep -E 'DTXProxyChannel|DTXChannel|out of date and needs to be rebuilt'; then
+            if [ "$failed" = "1" ] && cat build/build.log | grep -E 'DTXProxyChannel|DTXChannel|out of date and needs to be rebuilt|Abort trap: 6'; then
                 echo "Known Xcode error detected. Running job again."
                 failed=0
                 sh build.sh verify-$target | tee build/build.log | xcpretty -r junit -o build/reports/junit.xml || failed=1
