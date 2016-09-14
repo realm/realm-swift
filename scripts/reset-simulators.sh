6a37c83bf8596e553834c3e8644ac8536d12602f
JP Simard <jp@jpsim.com>
2016-09-13 21:33:54 -0700

    avoid simctl switch crash and avoid logging pid of springboard

--- scripts/reset-simulators.sh
+++ scripts/reset-simulators.sh
@@ -13,15 +13,21 @@ while pgrep -q Simulator; do
     pkill -9 Simulator 2>/dev/null || true
 done
 
+# Run until we get a result since switching simulator versions often causes CoreSimulatorService to throw an exception.
+devices=""
+until [ "$devices" != "" ]; do
+    devices="$(xcrun simctl list devices -j || true)"
+done
+
 # Shut down booted simulators
-xcrun simctl list devices -j | ruby -rjson -e 'puts JSON.parse($stdin.read)["devices"].flat_map { |d| d[1] }.select { |d| d["state"] == "Booted" && d["availability"] == "(available)" }.map { |d| d["udid"] }' | while read udid; do
+echo "$devices" | ruby -rjson -e 'puts JSON.parse($stdin.read)["devices"].flat_map { |d| d[1] }.select { |d| d["state"] == "Booted" && d["availability"] == "(available)" }.map { |d| d["udid"] }' | while read udid; do
     echo "shutting down simulator with ID: $udid"
     xcrun simctl shutdown $udid
 done
 
 # Erase all available simulators
 echo "erasing simulators"
-xcrun simctl list devices -j | ruby -rjson -e 'puts JSON.parse($stdin.read)["devices"].flat_map { |d| d[1] }.select { |d| d["availability"] == "(available)" }.map { |d| d["udid"] }' | while read udid; do
+echo "$devices" | ruby -rjson -e 'puts JSON.parse($stdin.read)["devices"].flat_map { |d| d[1] }.select { |d| d["availability"] == "(available)" }.map { |d| d["udid"] }' | while read udid; do
     xcrun simctl erase $udid &
 done
 wait
@@ -32,10 +38,10 @@ fi
 
 # Wait until the boot completes
 echo "waiting for simulator to boot..."
-until xcrun simctl list devices -j | ruby -rjson -e 'exit JSON.parse($stdin.read)["devices"].flat_map { |d| d[1] }.any? { |d| d["state"] == "Booted" }'; do
+until xcrun simctl list devices -j | ruby -rjson -e 'exit JSON.parse($stdin.read)["devices"].flat_map { |d| d[1] }.any? { |d| d["availability"] == "(available)" && d["state"] == "Booted" }'; do
     sleep 1
 done
 
 # Wait until the simulator is fully booted by waiting for it to launch SpringBoard
-xcrun simctl launch booted com.apple.springboard || true
+xcrun simctl launch booted com.apple.springboard >/dev/null 2>&1 || true
 echo "simulator booted"
