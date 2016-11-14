kevincador/realm-cocoa

Refactor binding to move sync functionality into object store
Changes:
- All non-trivial `RLMSync*` types are now thin wrappers around their new object store counterparts
- Removed the file manager
- Removed the session handles
- Added a 'refresh handle', a helper object managed by a `RLMSyncUser` that does nothing but periodically refresh a session as long as that session remains live
- Updated existing unit tests to work with the new code

Realm/ObjectServerTests/RLMObjectServerTests.m
@@ -34,8 +34,8 @@
                                                    server:[RLMSyncTestCase authServerURL]];
     RLMSyncUser *secondUser = [self logInUserForCredential:[RLMSyncTestCase basicCredential:NO]
                                                     server:[RLMSyncTestCase authServerURL]];
-    // Logging in with equivalent credentials should return the same user object instance.
-    XCTAssertEqual(firstUser, secondUser);
+    // Two users created with the same credential should resolve to the same actual user.
+    XCTAssertTrue([firstUser.identity isEqualToString:secondUser.identity]);
 
     // Trying to "create" a username/password account that already exists should cause an error.
     XCTestExpectation *expectation = [self expectationWithDescription:@""];
@@ -72,7 +72,7 @@
                                               server:[RLMObjectServerTests authServerURL]];
     XCTAssertNotNil(user);
     XCTAssertEqual([[RLMSyncUser all] count], 1U);
-    XCTAssertTrue([[RLMSyncUser all] containsObject:user]);
+    XCTAssertTrue([[[RLMSyncUser all] firstObject].identity isEqualToString:user.identity]);
 }
 
 #pragma mark - Sync
@@ -90,7 +90,9 @@
                                               server:[RLMObjectServerTests authServerURL]];
     NSError *error = nil;
     NSURL *url = [NSURL URLWithString:@"realm://localhost:9080/testSyncWithAdminToken"];
-    RLMRealm *realm = [self openRealmForURL:url user:user error:&error];
+    RLMRealmConfiguration *c = [[RLMRealmConfiguration defaultConfiguration] copy];
+    c.syncConfiguration = [[RLMSyncConfiguration alloc] initWithUser:user realmURL:url];
+    RLMRealm *realm = [RLMRealm realmWithConfiguration:c error:&error];
     XCTAssertNil(error);
     XCTAssertTrue(realm.isEmpty);
 }

realm/realm-swift

Realm/ObjectServerTests/RLMObjectServerTests.m
@@ -41,8 +41,8 @@
     RLMSyncUser *secondUser = [self logInUserForCredential:[RLMSyncTestCase basicCredentialWithName:ACCOUNT_NAME()
                                                                                            register:NO]
                                                     server:[RLMSyncTestCase authServerURL]];
-    // Logging in with equivalent credentials should return the same user object instance.
-    XCTAssertEqual(firstUser, secondUser);
+    // Two users created with the same credential should resolve to the same actual user.
+    XCTAssertTrue([firstUser.identity isEqualToString:secondUser.identity]);
     // Authentication server property should be properly set.
     XCTAssertEqualObjects(firstUser.authenticationServer, [RLMSyncTestCase authServerURL]);
 
@@ -83,7 +83,7 @@
                                               server:[RLMObjectServerTests authServerURL]];
     XCTAssertNotNil(user);
     XCTAssertEqual([[RLMSyncUser allUsers] count], 1U);
-    XCTAssertTrue([[RLMSyncUser allUsers] containsObject:user]);
+    XCTAssertTrue([[[RLMSyncUser allUsers] firstObject].identity isEqualToString:user.identity]);
     XCTAssertEqualObjects([RLMSyncUser currentUser], user);
 
     RLMSyncUser *user2 = [self logInUserForCredential:[RLMObjectServerTests basicCredentialWithName:[ACCOUNT_NAME() stringByAppendingString:@"2"]
@@ -108,7 +108,11 @@
     RLMSyncUser *user = [self logInUserForCredential:credential
                                               server:[RLMObjectServerTests authServerURL]];
     NSURL *url = [NSURL URLWithString:@"realm://localhost:9080/testSyncWithAdminToken"];
-    RLMRealm *realm = [self openRealmForURL:url user:user];
+    RLMRealmConfiguration *c = [[RLMRealmConfiguration defaultConfiguration] copy];
+    c.syncConfiguration = [[RLMSyncConfiguration alloc] initWithUser:user realmURL:url];
+    NSError *error = nil;
+    RLMRealm *realm = [RLMRealm realmWithConfiguration:c error:&error];
+    XCTAssertNil(error);
     XCTAssertTrue(realm.isEmpty);
 }
 