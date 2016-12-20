c4c51c042d0078a054d7dfadf3fa343acb491fd4
JP Simard <jp@jpsim.com>
2016-12-20 10:51:42 -0800

    reset file if a file format upgrade is required in ResetFile mode

--- Realm/Tests/RealmTests.mm
+++ Realm/Tests/RealmTests.mm
@@ -204,6 +204,26 @@
         XCTAssertEqualObjects(oldData, newData); \
 } while (0)
 
+- (void)testFileFormatUpgradeRequiredDeleteRealmIfNeeded {
+    RLMRealmConfiguration *config = [RLMRealmConfiguration defaultConfiguration];
+    config.deleteRealmIfMigrationNeeded = YES;
+
+    NSURL *bundledRealmURL = [[NSBundle bundleForClass:[RealmTests class]] URLForResource:@"fileformat-pre-null" withExtension:@"realm"];
+    [NSFileManager.defaultManager copyItemAtURL:bundledRealmURL toURL:config.fileURL error:nil];
+
+    @autoreleasepool {
+        XCTAssertTrue([[RLMRealm realmWithConfiguration:config error:nil] isEmpty]);
+    }
+
+    bundledRealmURL = [[NSBundle bundleForClass:[RealmTests class]] URLForResource:@"fileformat-old-date" withExtension:@"realm"];
+    [NSFileManager.defaultManager removeItemAtURL:config.fileURL error:nil];
+    [NSFileManager.defaultManager copyItemAtURL:bundledRealmURL toURL:config.fileURL error:nil];
+
+    @autoreleasepool {
+        XCTAssertTrue([[RLMRealm realmWithConfiguration:config error:nil] isEmpty]);
+    }
+}
+
 - (void)testFileFormatUpgradeRequiredButDisabled {
     RLMRealmConfiguration *config = [RLMRealmConfiguration defaultConfiguration];
     config.disableFormatUpgrade = true;
