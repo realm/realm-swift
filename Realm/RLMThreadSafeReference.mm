295aebc5151e494aea54c9d3d4db802548ed8915
JP Simard <jp@jpsim.com>
2017-01-02 16:38:27 -0800

    translate C++ errors in RLMThreadSafeReference

--- Realm/RLMThreadSafeReference.mm
+++ Realm/RLMThreadSafeReference.mm
@@ -21,6 +21,16 @@
 #import "RLMUtil.hpp"
 #import "shared_realm.hpp"
 
+template<typename Function>
+static auto translateErrors(Function&& f) {
+    try {
+        return f();
+    }
+    catch (std::exception const& e) {
+        @throw RLMException(e);
+    }
+}
+
 @implementation RLMThreadSafeReference {
 @private
     std::unique_ptr<realm::ThreadSafeReferenceBase> _reference;
@@ -36,19 +46,18 @@
     REALM_ASSERT_DEBUG([threadConfined conformsToProtocol:@protocol(RLMThreadConfined)]);
     if (![threadConfined conformsToProtocol:@protocol(RLMThreadConfined_Private)]) {
         @throw RLMException(@"Illegal custom conformance to `RLMThreadConfined` by `%@`", threadConfined.class);
-    }
-    if (threadConfined.invalidated) {
+    } else if (threadConfined.invalidated) {
         @throw RLMException(@"Cannot construct reference to invalidated object");
-    }
-    if (!threadConfined.realm) {
+    } else if (!threadConfined.realm) {
         @throw RLMException(@"Cannot construct reference to unmanaged object, "
                             "which can be passed across threads directly");
     }
 
-    // TODO: C++ exception is thrown if this is the wrong thread. Should this be translated to a RLMException?
+    translateErrors([&] {
     //       Should we check the thread ourselves here?
-    _reference = [(id<RLMThreadConfined_Private>)threadConfined rlm_newThreadSafeReference];
-    _metadata = ((id<RLMThreadConfined_Private>)threadConfined).rlm_objectiveCMetadata;
+        _reference = [(id<RLMThreadConfined_Private>)threadConfined rlm_newThreadSafeReference];
+        _metadata = ((id<RLMThreadConfined_Private>)threadConfined).rlm_objectiveCMetadata;
+    });
     _type = threadConfined.class;
 
     return self;
@@ -62,7 +71,10 @@
     if (!_reference) {
         @throw RLMException(@"Can only resolve a thread safe reference once.");
     }
-    return [_type rlm_objectWithThreadSafeReference:std::move(_reference) metadata:_metadata realm:realm];
+    return translateErrors([&] {
+        return [_type rlm_objectWithThreadSafeReference:std::move(_reference)
+                                               metadata:_metadata realm:realm];
+    });
 }
 
 - (BOOL)isInvalidated {
