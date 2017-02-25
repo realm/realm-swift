3bbbd8a2013a71bf65dfaf27cd4fdde9096ae5a2
Austin Zheng <az@realm.io>
2017-02-24 18:22:40 -0800

    Replace `RLMRealm` extension with helper function

--- Realm/RLMSyncUtil.mm
+++ Realm/RLMSyncUtil.mm
@@ -18,6 +18,7 @@
 
 #import <Foundation/Foundation.h>
 
+#import "RLMSyncConfiguration_Private.hpp"
 #import "RLMSyncUtil_Private.hpp"
 #import "RLMSyncUser_Private.hpp"
 #import "RLMRealmConfiguration+Sync.h"
@@ -26,6 +27,8 @@
 #import "RLMSyncPermissionChange.h"
 #import "RLMSyncPermissionOffer.h"
 #import "RLMSyncPermissionOfferResponse.h"
+
+#import "sync/sync_user.hpp"
 
 static RLMRealmConfiguration *RLMRealmSpecialPurposeConfiguration(RLMSyncUser *user, NSString *realmName) {
     NSURLComponents *components = [NSURLComponents componentsWithURL:user.authenticationServer resolvingAgainstBaseURL:NO];
@@ -93,6 +96,21 @@ RLMSyncStopPolicy translateStopPolicy(SyncSessionStopPolicy stop_policy)
         case SyncSessionStopPolicy::AfterChangesUploaded:   return RLMSyncStopPolicyAfterChangesUploaded;
     }
     REALM_UNREACHABLE();
+}
+
+std::shared_ptr<SyncSession> sync_session_for_realm(RLMRealm *realm)
+{
+    RLMRealmConfiguration *realmConfig = realm.configuration;
+    if (RLMSyncConfiguration *syncConfig = realmConfig.syncConfiguration) {
+        SyncConfig config = [syncConfig rawConfiguration];
+        std::shared_ptr<SyncUser> user = config.user;
+        if (user && user->state() != SyncUser::State::Error) {
+            NSString *path = [realmConfig.fileURL absoluteString];
+            REALM_ASSERT(path);
+            return user->session_for_on_disk_path([path UTF8String]);
+        }
+    }
+    return nullptr;
 }
 
 }
