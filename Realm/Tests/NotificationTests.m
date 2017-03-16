5f45a6559b4a7cd6fccccd9c6ada62fddf5b4b2d
Thomas Goyne <tgoyne@gmail.com>
2017-03-15 17:23:21 -0700

    Ensure that background Realms are torn down before clearing the cache in tests

--- Realm/Tests/NotificationTests.m
+++ Realm/Tests/NotificationTests.m
@@ -898,6 +898,13 @@ static void ExpectChange(id self, NSArray *deletions, NSArray *insertions, NSArr
     _propertyNames = [_obj.objectSchema.properties valueForKey:@"name"];
 }
 
+- (void)tearDown {
+    _values = nil;
+    _initialValues = nil;
+    _obj = nil;
+    [super tearDown];
+}
+
 - (void)testDeleteObservedObject {
     XCTestExpectation *expectation = [self expectationWithDescription:@""];
     RLMNotificationToken *token = [_obj addNotificationBlock:^(BOOL deleted, NSArray *changes, NSError *error) {
