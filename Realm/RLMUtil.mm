2656fb964259c4ceecab474a3261123966f60726
Cédric Luthi <cedric.luthi@gmail.com>
2017-03-29 23:31:14 +0200

    Safer way to check who is the caller
    +[NSThread callStackSymbols] is not usable on real devices because names are redacted. But we know we want the caller to be -[NSSubqueryExpression expressionValueWithObject:context:] and we can get this information with `dladdr`.

--- Realm/RLMUtil.mm
+++ Realm/RLMUtil.mm
@@ -32,6 +32,7 @@
 #import <realm/mixed.hpp>
 #import <realm/table_view.hpp>
 
+#include <dlfcn.h>
 #include <sys/sysctl.h>
 #include <sys/types.h>
 
@@ -361,10 +362,20 @@ BOOL RLMIsRunningInPlayground() {
 }
 
 void RLMWorkaroundRadar31252694() {
+    static IMP expressionValueWithObjectIMP;
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        Class NSSubqueryExpression = NSClassFromString([@[@"N", @"S", @"Subquery", @"Expression"] componentsJoinedByString:@""]);
+        SEL expressionValueWithObjectSEL = NSSelectorFromString([@[@"expression", @"Value", @"With", @"Object", @":", @"context", @":"] componentsJoinedByString:@""]);
+        Method expressionValueWithObject = class_getInstanceMethod(NSSubqueryExpression, expressionValueWithObjectSEL);
+        expressionValueWithObjectIMP = method_getImplementation(expressionValueWithObject);
+    });
+
     SEL isNSArraySEL = NSSelectorFromString([@[@"is", @"NSArray", @"_", @"_"] componentsJoinedByString:@""]);
     IMP isNSArrayIMP = imp_implementationWithBlock(^(__unused id array) {
-        NSArray<NSString *> *callStackSymbols = [NSThread callStackSymbols];
-        return callStackSymbols.count > 0 && [callStackSymbols[1] containsString:@"-[NSSubqueryExpression expressionValueWithObject:context:]"];
+        Dl_info caller;
+        dladdr(__builtin_return_address(0), &caller);
+        return caller.dli_saddr == expressionValueWithObjectIMP;
     });
     Method isInvalidated = class_getInstanceMethod(RLMArray.class, @selector(isInvalidated));
     const char *typeEncoding = method_getTypeEncoding(isInvalidated);
