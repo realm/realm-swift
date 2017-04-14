sekiguchik/realm-cocoa

Add aggregate functions to RLMArray and RLMCollection
List and RealmCollection had these, but in obj-c only RLMResults did, which was
oddly inconsistent.

Also add support for them on unmanaged arrays, which previously did not work
for List.

Realm/RLMArray.mm
@@ -18,10 +18,12 @@
 
 #import "RLMArray_Private.hpp"
 
-#import "RLMObject_Private.h"
-#import "RLMObjectStore.h"
 #import "RLMObjectSchema.h"
+#import "RLMObjectStore.h"
+#import "RLMObject_Private.h"
+#import "RLMProperty.h"
 #import "RLMQueryUtil.hpp"
+#import "RLMSchema_Private.h"
 #import "RLMSwiftSupport.h"
 #import "RLMThreadSafeReference_Private.hpp"
 #import "RLMUtil.hpp"
@@ -342,6 +344,55 @@ static void RLMValidateArrayBounds(__unsafe_unretained RLMArray *const ar,
 
 - (void)setValue:(id)value forKey:(NSString *)key {
     [_backingArray setValue:value forKey:key];
+}
+
+- (void)validateAggregateProperty:(NSString *)propertyName
+                           method:(SEL)aggregateMethod
+                        allowDate:(bool)allowDate {
+    RLMObjectSchema *objectSchema;
+    if (_backingArray.count) {
+        objectSchema = [_backingArray[0] objectSchema];
+    }
+    else {
+        objectSchema = [RLMSchema.partialSharedSchema schemaForClassName:_objectClassName];
+    }
+
+    RLMProperty *prop = RLMValidatedProperty(objectSchema, propertyName);
+    switch (prop.type) {
+        case RLMPropertyTypeInt:
+        case RLMPropertyTypeFloat:
+        case RLMPropertyTypeDouble:
+            break;
+        case RLMPropertyTypeDate:
+            if (allowDate) {
+                break;
+            }
+            [[clang::fallthrough]];
+        default:
+            @throw RLMException(@"%@ is not supported for %@ property '%@.%@'",
+                                NSStringFromSelector(aggregateMethod),
+                                RLMTypeToString(prop.type), _objectClassName, propertyName);
+    }
+}
+
+- (id)minOfProperty:(NSString *)property {
+    [self validateAggregateProperty:property method:_cmd allowDate:true];
+    return [_backingArray valueForKeyPath:[@"@min." stringByAppendingString:property]];
+}
+
+- (id)maxOfProperty:(NSString *)property {
+    [self validateAggregateProperty:property method:_cmd allowDate:true];
+    return [_backingArray valueForKeyPath:[@"@max." stringByAppendingString:property]];
+}
+
+- (id)sumOfProperty:(NSString *)property {
+    [self validateAggregateProperty:property method:_cmd allowDate:false];
+    return [_backingArray valueForKeyPath:[@"@sum." stringByAppendingString:property]];
+}
+
+- (id)averageOfProperty:(NSString *)property {
+    [self validateAggregateProperty:property method:_cmd allowDate:false];
+    return [_backingArray valueForKeyPath:[@"@avg." stringByAppendingString:property]];
 }
 
 - (NSUInteger)indexOfObjectWithPredicate:(NSPredicate *)predicate {

realm/realm-swift

Realm/RLMArray.mm
@@ -18,10 +18,12 @@
 
 #import "RLMArray_Private.hpp"
 
-#import "RLMObject_Private.h"
-#import "RLMObjectStore.h"
 #import "RLMObjectSchema.h"
+#import "RLMObjectStore.h"
+#import "RLMObject_Private.h"
+#import "RLMProperty_Private.h"
 #import "RLMQueryUtil.hpp"
+#import "RLMSchema_Private.h"
 #import "RLMSwiftSupport.h"
 #import "RLMThreadSafeReference_Private.hpp"
 #import "RLMUtil.hpp"
@@ -342,6 +344,55 @@ static void RLMValidateArrayBounds(__unsafe_unretained RLMArray *const ar,
 
 - (void)setValue:(id)value forKey:(NSString *)key {
     [_backingArray setValue:value forKey:key];
+}
+
+- (void)validateAggregateProperty:(NSString *)propertyName
+                           method:(SEL)aggregateMethod
+                        allowDate:(bool)allowDate {
+    RLMObjectSchema *objectSchema;
+    if (_backingArray.count) {
+        objectSchema = [_backingArray[0] objectSchema];
+    }
+    else {
+        objectSchema = [RLMSchema.partialSharedSchema schemaForClassName:_objectClassName];
+    }
+
+    RLMProperty *prop = RLMValidatedProperty(objectSchema, propertyName);
+    switch (prop.type) {
+        case RLMPropertyTypeInt:
+        case RLMPropertyTypeFloat:
+        case RLMPropertyTypeDouble:
+            break;
+        case RLMPropertyTypeDate:
+            if (allowDate) {
+                break;
+            }
+            [[clang::fallthrough]];
+        default:
+            @throw RLMException(@"%@ is not supported for %@ property '%@.%@'",
+                                NSStringFromSelector(aggregateMethod),
+                                RLMTypeToString(prop.type), _objectClassName, propertyName);
+    }
+}
+
+- (id)minOfProperty:(NSString *)property {
+    [self validateAggregateProperty:property method:_cmd allowDate:true];
+    return [_backingArray valueForKeyPath:[@"@min." stringByAppendingString:property]];
+}
+
+- (id)maxOfProperty:(NSString *)property {
+    [self validateAggregateProperty:property method:_cmd allowDate:true];
+    return [_backingArray valueForKeyPath:[@"@max." stringByAppendingString:property]];
+}
+
+- (id)sumOfProperty:(NSString *)property {
+    [self validateAggregateProperty:property method:_cmd allowDate:false];
+    return [_backingArray valueForKeyPath:[@"@sum." stringByAppendingString:property]];
+}
+
+- (id)averageOfProperty:(NSString *)property {
+    [self validateAggregateProperty:property method:_cmd allowDate:false];
+    return [_backingArray valueForKeyPath:[@"@avg." stringByAppendingString:property]];
 }
 
 - (NSUInteger)indexOfObjectWithPredicate:(NSPredicate *)predicate {