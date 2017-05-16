d23c133f0599e335bce50d19d5309b9ea2c5ba96
Karl Kuehn <kuehn.karl@gmail.com>
2017-05-16 15:59:27 -0700

    enable CI testing on an iOS 10 device

--- build.sh
+++ build.sh
@@ -1056,6 +1056,11 @@ EOM
         # FIXME: Re-enable once CI can properly unlock the keychain
         export REALM_DISABLE_METADATA_ENCRYPTION=1
 
+        # strip off the ios|tvos version specifier, e.g. the last part of: `ios-device-objc-ios8`
+        if [[ "$target" =~ ^((ios|tvos)-device(-(objc|swift))?)(-(ios|tvos)[[:digit:]]+)?$ ]]; then
+            export target=${BASH_REMATCH[1]}
+        fi
+
         if [ "$target" = "docs" ]; then
             sh build.sh set-swift-version
             sh build.sh verify-docs
