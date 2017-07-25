17cf8e88e679d25e4a8b1a234791d2db88567f5a
JP Simard <jp@jpsim.com>
2017-07-25 15:55:46 -0700

    only apply swift_objc_members attribute if available

--- Realm/Tests/RLMTestCase.h
+++ Realm/Tests/RLMTestCase.h
@@ -33,7 +33,13 @@ NSData *RLMGenerateKey(void);
 }
 #endif
 
-__attribute__((swift_objc_members)) // workaround for rdar://33514802
+#if __has_attribute(swift_objc_members)
+#define REALM_SWIFT_OBJC_MEMBERS __attribute__((swift_objc_members))
+#else
+#define REALM_SWIFT_OBJC_MEMBERS
+#endif
+
+REALM_SWIFT_OBJC_MEMBERS // workaround for rdar://33514802
 @interface RLMTestCase : XCTestCase
 
 - (RLMRealm *)realmWithTestPath;
