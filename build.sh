3cbb72372f8bae8975b3e41b94b2aafea9a98f82
Karl Kuehn <karl.kuehn@realm.io>
2017-08-15 13:09:38 -0700

    fix error message when device is missing

--- build.sh
+++ build.sh
@@ -231,6 +231,7 @@ test_devices() {
     local serial_numbers=()
     local serial_numbers_text=$(/usr/sbin/system_profiler SPUSBDataType | /usr/bin/awk '/^ +Serial Number: / { match($0, /^ +Serial Number: /); print substr($0, RLENGTH + 1) }')
     while read -r number; do
+        if [ -z "$number" ]; then continue; fi
         serial_numbers+=("$number")
     done <<< "$serial_numbers_text"
     if [[ ${#serial_numbers[@]} == 0 ]]; then
