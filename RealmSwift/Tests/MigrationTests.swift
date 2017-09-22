9992ac3b35831f4629168f476f620a8a475755f5
Mark Rowe <mar@realm.io>
2017-09-22 15:40:10 -0700

    Update a test case now that a more precise error is being raised.

--- RealmSwift/Tests/MigrationTests.swift
+++ RealmSwift/Tests/MigrationTests.swift
@@ -116,7 +116,7 @@ class MigrationTests: TestCase {
         _ = try! Realm()
         XCTAssertEqual(0, try! schemaVersionAtURL(defaultRealmURL()),
                        "Initial version should be 0")
-        assertFails(.fail) {
+        assertFails(.filePermissionDenied) {
             try schemaVersionAtURL(URL(fileURLWithPath: "/dev/null"))
         }
     }
