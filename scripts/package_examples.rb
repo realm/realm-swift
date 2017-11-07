862da40d6b5237f81f3b68c3c1c0069c0a5f50ee
Mark Rowe <mar@realm.io>
2017-11-07 12:41:02 -0800

    Package examples for Swift 3.2.2 and 4.0.2 as well.

--- scripts/package_examples.rb
+++ scripts/package_examples.rb
@@ -41,7 +41,7 @@ base_examples = [
   "examples/tvos/swift",
 ]
 
-swift_versions = %w(3.0 3.0.1 3.0.2 3.1 3.2 4.0)
+swift_versions = %w(3.0 3.0.1 3.0.2 3.1 3.2 3.2.2 4.0 4.0.2)
 
 # Remove reference to Realm.xcodeproj from all example workspaces.
 base_examples.each do |example|
