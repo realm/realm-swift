71e1358b37fa9a64b27bad2e496e82e19bd9826d
Thomas Goyne <tg@realm.io>
2018-04-23 13:41:57 -0700

    Make the timeout for HTTP requests configurable for tests

--- Realm/RLMSyncUser.mm
+++ Realm/RLMSyncUser.mm
@@ -299,65 +299,53 @@ PermissionChangeCallback RLMWrapPermissionStatusCallback(RLMPermissionStatusBloc
                                    userInfo:nil]);
         return;
     }
-    [RLMNetworkClient sendRequestToEndpoint:[RLMSyncChangePasswordEndpoint endpoint]
-                                     server:self.authenticationServer
+    [RLMSyncChangePasswordEndpoint sendRequestToServer:self.authenticationServer
                                        JSON:@{kRLMSyncTokenKey: self._refreshToken,
                                               kRLMSyncUserIDKey: userID,
-                                              kRLMSyncDataKey: @{ kRLMSyncNewPasswordKey: newPassword }
-                                              }
-                                    timeout:60
+                                              kRLMSyncDataKey: @{kRLMSyncNewPasswordKey: newPassword}}
                                     options:[[RLMSyncManager sharedManager] networkRequestOptions]
-                                 completion:^(NSError *error, __unused NSDictionary *json) {
-        completion(error);
-    }];
+                                 completion:completion];
 }
 
 + (void)requestPasswordResetForAuthServer:(NSURL *)serverURL
                                 userEmail:(NSString *)email
                                completion:(RLMPasswordChangeStatusBlock)completion {
-    [RLMNetworkClient sendRequestToEndpoint:[RLMSyncUpdateAccountEndpoint endpoint]
-                                     server:serverURL
-                                       JSON:@{@"provider_id": email, @"data": @{@"action": @"reset_password"}}
-                                    timeout:60
-                                    options:[[RLMSyncManager sharedManager] networkRequestOptions]
-                                 completion:^(NSError *error, NSDictionary *) { completion(error); }];
+    [RLMSyncUpdateAccountEndpoint sendRequestToServer:serverURL
+                                                 JSON:@{@"provider_id": email, @"data": @{@"action": @"reset_password"}}
+                                              options:[[RLMSyncManager sharedManager] networkRequestOptions]
+                                           completion:completion];
 }
 
 + (void)completePasswordResetForAuthServer:(NSURL *)serverURL
                                      token:(NSString *)token
                                   password:(NSString *)newPassword
                                 completion:(RLMPasswordChangeStatusBlock)completion {
-    [RLMNetworkClient sendRequestToEndpoint:[RLMSyncUpdateAccountEndpoint endpoint]
-                                     server:serverURL
-                                       JSON:@{@"data": @{@"action": @"complete_reset",
-                                                         @"token": token,
-                                                         @"new_password": newPassword}}
-                                    timeout:60
-                                    options:[[RLMSyncManager sharedManager] networkRequestOptions]
-                                 completion:^(NSError *error, NSDictionary *) { completion(error); }];
+    [RLMSyncUpdateAccountEndpoint sendRequestToServer:serverURL
+                                                 JSON:@{@"data": @{@"action": @"complete_reset",
+                                                                   @"token": token,
+                                                                   @"new_password": newPassword}}
+                                              options:[[RLMSyncManager sharedManager] networkRequestOptions]
+                                           completion:completion];
 }
 
 + (void)requestEmailConfirmationForAuthServer:(NSURL *)serverURL
                                     userEmail:(NSString *)email
                                    completion:(RLMPasswordChangeStatusBlock)completion {
-    [RLMNetworkClient sendRequestToEndpoint:[RLMSyncUpdateAccountEndpoint endpoint]
-                                     server:serverURL
-                                       JSON:@{@"data": @{@"provider_id": email, @"action": @"request_email_confirmation"}}
-                                    timeout:60
-                                    options:[[RLMSyncManager sharedManager] networkRequestOptions]
-                                 completion:^(NSError *error, NSDictionary *) { completion(error); }];
+    [RLMSyncUpdateAccountEndpoint sendRequestToServer:serverURL
+                                                 JSON:@{@"data": @{@"provider_id": email,
+                                                                   @"action": @"request_email_confirmation"}}
+                                              options:[[RLMSyncManager sharedManager] networkRequestOptions]
+                                           completion:completion];
 }
 
 + (void)confirmEmailForAuthServer:(NSURL *)serverURL
                             token:(NSString *)token
                        completion:(RLMPasswordChangeStatusBlock)completion {
-    [RLMNetworkClient sendRequestToEndpoint:[RLMSyncUpdateAccountEndpoint endpoint]
-                                     server:serverURL
-                                       JSON:@{@"data": @{@"action": @"confirm_email",
-                                                         @"token": token}}
-                                    timeout:60
-                                    options:[[RLMSyncManager sharedManager] networkRequestOptions]
-                                 completion:^(NSError *error, NSDictionary *) { completion(error); }];
+    [RLMSyncUpdateAccountEndpoint sendRequestToServer:serverURL
+                                                 JSON:@{@"data": @{@"action": @"confirm_email",
+                                                                   @"token": token}}
+                                              options:[[RLMSyncManager sharedManager] networkRequestOptions]
+                                           completion:completion];
 }
 
 #pragma mark - Administrator API
