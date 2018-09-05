572c394c9daede73f774ade7b37069fe8da431c4
Marin Todorov <marin@underplot.com>
2018-09-05 20:25:12 +0200

    adds a list of the supported types List can store (#5493)

--- RealmSwift/List.swift
+++ RealmSwift/List.swift
@@ -42,7 +42,9 @@ public class ListBase: RLMListBase {
 /**
  `List` is the container type in Realm used to define to-many relationships.
 
- Like Swift's `Array`, `List` is a generic type that is parameterized on the type of `Object` it stores.
+ Like Swift's `Array`, `List` is a generic type that is parameterized on the type it stores. This can be either an `Object`
+ subclass or one of the following types: `Bool`, `Int`, `Int8`, `Int16`, `Int32`, `Int64`, `Float`, `Double`, `String`, `Data`,
+ and `Date` (and their optional versions)
 
  Unlike Swift's native collections, `List`s are reference types, and are only immutable if the Realm that manages them
  is opened as read-only.
