3fa64d36a451135854a2fbd62fb3a84d1b77c2fe
Thomas Goyne <tg@realm.io>
2019-08-30 16:46:48 -0700

    Fix style check failures

--- examples/installation/osx/swift/XCFrameworkExample/XCFrameworkExample/AppDelegate.swift
+++ examples/installation/osx/swift/XCFrameworkExample/XCFrameworkExample/AppDelegate.swift
@@ -23,17 +23,4 @@ open class MyModel: Object {}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
-
--- examples/installation/osx/swift/XCFrameworkExample/XCFrameworkExample/ViewController.swift
+++ examples/installation/osx/swift/XCFrameworkExample/XCFrameworkExample/ViewController.swift
@@ -19,19 +19,4 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
-
