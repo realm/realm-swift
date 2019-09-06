695a2150e612de4715b68c5547312f40fd130f2c
Liam Nichols <liam.nichols.ln@gmail.com>
2019-09-06 12:45:55 +0100

    [ObjectSchema] Expose objectClass from RLMObjectSchema private interface
    RLMObjectSchema and ObjectSchema both expose the 'className' property however since Swift Object subclasses have their class names demangled via RLMSwiftSupport, there is no way to associate an ObjectSchema instance back to its original Swift class. This change exposes a mechanism for such capabilities and adds unit test coverage to validate it

--- RealmSwift/ObjectSchema.swift
+++ RealmSwift/ObjectSchema.swift
@@ -18,6 +18,7 @@
 
 import Foundation
 import Realm
+import Realm.Private
 
 /**
  This class represents Realm model object schemas.
@@ -43,6 +44,9 @@ public struct ObjectSchema: CustomStringConvertible {
 
     /// The name of the class the schema describes.
     public var className: String { return rlmObjectSchema.className }
+
+    /// The object class the schema describes.
+    public var objectClass: AnyClass { return rlmObjectSchema.objectClass }
 
     /// The property which serves as the primary key for the class the schema describes, if any.
     public var primaryKeyProperty: Property? {
