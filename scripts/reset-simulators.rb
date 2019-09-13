30672feba6bf7a1b6adca7ca387c8077c2265dba
Thomas Goyne <tg@realm.io>
2019-09-12 22:06:55 -0700

    Try to work around Xcode 10 simctl not producing valid JSON when running on a machine with Xcode 11 installed

--- scripts/reset-simulators.rb
+++ scripts/reset-simulators.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'json'
+require 'open3'
 
 def platform_for_runtime(runtime)
   runtime['identifier'].gsub(/com.apple.CoreSimulator.SimRuntime.([^-]+)-.*/, '\1')
@@ -17,9 +18,23 @@ def platform_for_device_type(device_type)
   end
 end
 
+def simctl(args)
+  # When running on a machine with Xcode 11 installed, Xcode 10 sometimes
+  # incorrectly thinks that it has not completed its first-run installation.
+  # This results in it printing errors related to that to stdout in front of
+  # the actual JSON output that we want.
+  Open3.popen3('xcrun simctl ' + args) do |stdin, stdout, strerr, wait_thr|
+    while line = stdout.gets
+      if not line.start_with? 'Install'
+        return line + stdout.read, wait_thr.value.exitstatus
+      end
+    end
+  end
+end
+
 def wait_for_core_simulator_service
   # Run until we get a result since switching simulator versions often causes CoreSimulatorService to throw an exception.
-  while `xcrun simctl list devices`.empty?
+  while simctl('list devices')[0].empty?
   end
 end
 
@@ -53,7 +68,7 @@ begin
   all_available_devices = []
   (0..5).each do |shutdown_attempt|
     begin
-      devices_json = `xcrun simctl list devices -j`
+      devices_json = simctl('list devices -j')[0]
       all_devices = JSON.parse(devices_json)['devices'].flat_map { |_, devices| devices }
     rescue JSON::ParserError
       sleep shutdown_attempt if shutdown_attempt > 0
@@ -78,8 +93,8 @@ begin
   puts ' done!'
 
   # Recreate all simulators.
-  runtimes = JSON.parse(`xcrun simctl list runtimes -j`)['runtimes']
-  device_types = JSON.parse(`xcrun simctl list devicetypes -j`)['devicetypes']
+  runtimes = JSON.parse(simctl('list runtimes -j')[0])['runtimes']
+  device_types = JSON.parse(simctl('list devicetypes -j')[0])['devicetypes']
 
   runtimes_by_platform = Hash.new { |hash, key| hash[key] = [] }
   runtimes.each do |runtime|
@@ -91,8 +106,8 @@ begin
   device_types.each do |device_type|
     platform = platform_for_device_type(device_type)
     runtimes_by_platform[platform].each do |runtime|
-      output = `xcrun simctl create '#{device_type['name']}' '#{device_type['identifier']}' '#{runtime['identifier']}' 2>&1`
-      next if $? == 0
+      output, ec = simctl("create '#{device_type['name']}' '#{device_type['identifier']}' '#{runtime['identifier']}' 2>&1")
+      next if ec == 0
 
       # Error code 161-163 indicate that the given device is not supported by the runtime, such as the iPad 2 and
       # iPhone 4s not being supported by the iOS 10 simulator runtime.
