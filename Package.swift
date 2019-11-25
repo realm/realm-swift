62bd8d4f0511cccaa0e79d79056665bf8f8fbf03
Thomas Goyne <tg@realm.io>
2019-11-25 10:47:10 -0800

    Bump version to alpha.2

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 import Foundation
 
 let coreVersionStr = "6.0.0-alpha.27"
-let cocoaVersionStr = "5.0.0-alpha.1"
+let cocoaVersionStr = "5.0.0-alpha.2"
 
 let coreVersionPieces = coreVersionStr.split(separator: ".")
 let coreVersionExtra = coreVersionPieces[2].split(separator: "-")
