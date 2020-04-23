49537e553e7c20494e6a19ca684a4ca038816dee
Jason Flax <jsflax@gmail.com>
2020-04-23 21:47:31 +0100

    10.0.0-alpha.1

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 import Foundation
 
 let coreVersionStr = "10.0.0-alpha.6"
-let cocoaVersionStr = "10.0.0-alpha.0"
+let cocoaVersionStr = "10.0.0-alpha.1"
 
 let coreVersionPieces = coreVersionStr.split(separator: ".")
 let coreVersionExtra = coreVersionPieces[2].split(separator: "-")
