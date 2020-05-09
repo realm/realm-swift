4cfdf9da492d51e2565fa27d2c3a8172ab8f4755
Thomas Goyne <tg@realm.io>
2020-05-08 22:13:21 -0700

    Release 5.0.0-beta.5

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 import Foundation
 
 let coreVersionStr = "6.0.4"
-let cocoaVersionStr = "5.0.0-beta.4"
+let cocoaVersionStr = "5.0.0-beta.5"
 
 let coreVersionPieces = coreVersionStr.split(separator: ".")
 let coreVersionExtra = coreVersionPieces[2].split(separator: "-")
