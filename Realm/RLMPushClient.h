6e74af513c636265f62ab57a0ced2ecc67d7beac
ericjmossman@gmail.com <ericjmossman@gmail.com>
2020-06-03 17:33:41 -0400

    Add RLMPushClient first header draft

---
+++ Realm/RLMPushClient.h
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2020 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class RLMApp, RLMSyncUser;
+
+typedef void(^RLMOptionalErrorBlock)(NSError * _Nullable);
+
+@interface RLMPushClient : NSObject
+
+@property (nonatomic, readonly, nonnull) NSString *serviceName;
+
+@property (nonatomic, assign) NSUInteger timeoutMs;
+
+- (void)registerDeviceWithToken:(NSString *)token
+                       syncUser:(RLMSyncUser *)syncUser
+                     completion:(RLMOptionalErrorBlock)completion;
+
+- (void)deregisterDeviceWithToken:(NSString *)token
+                         syncUser:(RLMSyncUser *)syncUser
+                       completion:(RLMOptionalErrorBlock)completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
