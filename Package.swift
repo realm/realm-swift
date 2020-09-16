4029098c9735856441f6bea7237a0165d3c4a15a
Thomas Goyne <tg@realm.io>
2020-09-16 07:53:26 -0700

    Update core and object store

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let coreVersionStr = "6.0.25"
+let coreVersionStr = "6.0.26"
 let cocoaVersionStr = "5.4.0"
 
 let coreVersionPieces = coreVersionStr.split(separator: ".")
