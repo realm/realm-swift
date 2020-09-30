6aed122f87cbfc4d46a9e556b12de4b5b83ebaf4
ericjmossman@gmail.com <ericjmossman@gmail.com>
2020-09-30 16:54:58 -0400

    Add Credential -> RLMCredential converter

--- RealmSwift/ObjectiveCSupport+Sync.swift
+++ RealmSwift/ObjectiveCSupport+Sync.swift
@@ -31,4 +31,31 @@ public extension ObjectiveCSupport {
     static func convert(object: RLMSyncConfiguration) -> SyncConfiguration {
         return SyncConfiguration(config: object)
     }
+    
+    // !!!: shorten case params
+    // !!!: Change objc api
+    /// Convert a `Credentials` to a `RLMCredentials`
+    static func convert(object: Credentials) -> RLMCredentials {
+        switch object {
+        case .facebook(let accessToken):
+            return RLMCredentials(facebookToken: accessToken)
+        case .google(let serverAuthCode):
+            return RLMCredentials(googleToken: serverAuthCode)
+        case .apple(let idToken):
+            return RLMCredentials(appleToken: idToken)
+        case .emailPassword(let email,let password):
+            return RLMCredentials(email: email, password: password)
+            // !!!: rename param
+        case .JWT(let token):
+            return RLMCredentials(jwt: token)
+        case .function(payload: let payload, let error):
+            return RLMCredentials(functionPayload: payload, error: error)
+        case .userAPIKey(APIKey: let APIKey):
+            return RLMCredentials(userAPIKey: APIKey)
+        case .serverAPIKey(serverAPIKey: let serverAPIKey):
+            return RLMCredentials(serverAPIKey: serverAPIKey)
+        case .anonymous:
+            return RLMCredentials.anonymous()
+        }
+    }
 }
