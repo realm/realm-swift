1b020c819cf5b6e7ddf855d8dc02922e28b26245
Thomas Goyne <tg@realm.io>
2020-09-30 19:00:20 -0700

    Upgrade to core 6.1.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let coreVersionStr = "6.1.2"
+let coreVersionStr = "6.1.3"
 let cocoaVersionStr = "5.4.6"
 
 let coreVersionPieces = coreVersionStr.split(separator: ".")
