a2da9d0ac847c012deba4529448848d3d5b7b046
Lee Maguire <lee.maguire@mongodb.com>
2020-11-13 09:33:32 +0000

    Add testing for Mac Catalyst using Cocoapods (#6926)

--- scripts/pr-ci-matrix.rb
+++ scripts/pr-ci-matrix.rb
@@ -41,6 +41,7 @@ targets = {
   'cocoapods-ios' => oldest_and_latest,
   'cocoapods-ios-dynamic' => oldest_and_latest,
   'cocoapods-watchos' => oldest_and_latest,
+  'cocoapods-catalyst' => oldest_and_latest,
 
   'swiftpm' => oldest_and_latest,
   'swiftpm-address' => latest_only,
@@ -61,9 +62,9 @@ output_file = """
 # https://wiki.jenkins-ci.org/display/JENKINS/Yaml+Axis+Plugin
 # This is a generated file produced by scripts/pr-ci-matrix.rb.
 
-xcode_version: #{XCODE_VERSIONS.map { |v| "\n - #{v}" }.join()}
-target: #{targets.map { |k, v| "\n - #{k}" }.join()}
-configuration: #{CONFIGURATIONS.map { |v| "\n - #{v}" }.join()}
+xcode_version:#{XCODE_VERSIONS.map { |v| "\n - #{v}" }.join()}
+target:#{targets.map { |k, v| "\n - #{k}" }.join()}
+configuration:#{CONFIGURATIONS.map { |v| "\n - #{v}" }.join()}
 
 exclude:
 """
