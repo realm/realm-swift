anatoliykant/realm-cocoa

Switch the swift package to use the monorepo

Package.swift
@@ -13,6 +13,7 @@ let cxxSettings: [CXXSetting] = [
     .headerSearchPath("include"),
     .headerSearchPath("Realm/ObjectStore/src"),
     .define("REALM_SPM", to: "1"),
+    .define("REALM_ENABLE_SYNC", to: "1"),
     .define("REALM_COCOA_VERSION", to: "@\"\(cocoaVersionStr)\""),
     .define("REALM_VERSION", to: "\"\(coreVersionStr)\""),
 
@@ -45,40 +46,14 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-core", .exact(Version(coreVersionStr)!)),
+        .package(url: "https://github.com/realm/realm-core", .branch("monorepo"))
     ],
     targets: [
       .target(
             name: "Realm",
-            dependencies: ["RealmCore"],
+            dependencies: ["RealmObjectStore"],
             path: ".",
             sources: [
-                "Realm/ObjectStore/src/binding_callback_thread_observer.cpp",
-                "Realm/ObjectStore/src/collection_notifications.cpp",
-                "Realm/ObjectStore/src/impl/apple/external_commit_helper.cpp",
-                "Realm/ObjectStore/src/impl/apple/keychain_helper.cpp",
-                "Realm/ObjectStore/src/impl/collection_change_builder.cpp",
-                "Realm/ObjectStore/src/impl/collection_notifier.cpp",
-                "Realm/ObjectStore/src/impl/list_notifier.cpp",
-                "Realm/ObjectStore/src/impl/object_notifier.cpp",
-                "Realm/ObjectStore/src/impl/primitive_list_notifier.cpp",
-                "Realm/ObjectStore/src/impl/realm_coordinator.cpp",
-                "Realm/ObjectStore/src/impl/results_notifier.cpp",
-                "Realm/ObjectStore/src/impl/transact_log_handler.cpp",
-                "Realm/ObjectStore/src/impl/weak_realm_notifier.cpp",
-                "Realm/ObjectStore/src/index_set.cpp",
-                "Realm/ObjectStore/src/list.cpp",
-                "Realm/ObjectStore/src/object.cpp",
-                "Realm/ObjectStore/src/object_changeset.cpp",
-                "Realm/ObjectStore/src/object_schema.cpp",
-                "Realm/ObjectStore/src/object_store.cpp",
-                "Realm/ObjectStore/src/results.cpp",
-                "Realm/ObjectStore/src/schema.cpp",
-                "Realm/ObjectStore/src/shared_realm.cpp",
-                "Realm/ObjectStore/src/thread_safe_reference.cpp",
-                "Realm/ObjectStore/src/util/scheduler.cpp",
-                "Realm/ObjectStore/src/util/uuid.cpp",
-                "Realm/ObjectStore/src/util/scheduler.cpp",
                 "Realm/RLMAccessor.mm",
                 "Realm/RLMAnalytics.mm",
                 "Realm/RLMArray.mm",
@@ -108,7 +83,30 @@ let package = Package(
                 "Realm/RLMSwiftSupport.m",
                 "Realm/RLMThreadSafeReference.mm",
                 "Realm/RLMUpdateChecker.mm",
-                "Realm/RLMUtil.mm"
+                "Realm/RLMUtil.mm",
+
+                // Sync source files
+                "Realm/RLMApp.mm",
+                "Realm/RLMAPIKeyAuth.mm",
+                "Realm/RLMBSON.mm",
+                "Realm/RLMCredentials.mm",
+                "Realm/RLMEmailPasswordAuth.mm",
+                "Realm/RLMFindOneAndModifyOptions.mm",
+                "Realm/RLMFindOptions.mm",
+                "Realm/RLMMongoClient.mm",
+                "Realm/RLMMongoCollection.mm",
+                "Realm/RLMNetworkTransport.mm",
+                "Realm/RLMProviderClient.mm",
+                "Realm/RLMPushClient.mm",
+                "Realm/RLMRealm+Sync.mm",
+                "Realm/RLMRealmConfiguration+Sync.mm",
+                "Realm/RLMSyncConfiguration.mm",
+                "Realm/RLMSyncManager.mm",
+                "Realm/RLMSyncSession.mm",
+                "Realm/RLMSyncUtil.mm",
+                "Realm/RLMUpdateResult.mm",
+                "Realm/RLMUser.mm",
+                "Realm/RLMUserAPIKey.mm"
             ],
             publicHeadersPath: "include",
             cxxSettings: cxxSettings
@@ -118,13 +116,8 @@ let package = Package(
             dependencies: ["Realm"],
             path: "RealmSwift",
             exclude: [
-                "Sync.swift",
-                "BSON.swift",
-                "App.swift",
-                "MongoClient.swift",
-                "ObjectiveCSupport+Sync.swift",
-                "ObjectiveCSupport+BSON.swift",
                 "Tests",
+                "Nonsync.swift",
             ]
         ),
         .target(

realm/realm-swift

Package.swift
@@ -11,8 +11,8 @@ let coreVersionExtra = coreVersionPieces[2].split(separator: "-")
 let cxxSettings: [CXXSetting] = [
     .headerSearchPath("."),
     .headerSearchPath("include"),
-    .headerSearchPath("Realm/ObjectStore/src"),
     .define("REALM_SPM", to: "1"),
+    .define("REALM_ENABLE_SYNC", to: "1"),
     .define("REALM_COCOA_VERSION", to: "@\"\(cocoaVersionStr)\""),
     .define("REALM_VERSION", to: "\"\(coreVersionStr)\""),
 
@@ -45,40 +45,14 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-core", .exact(Version(coreVersionStr)!)),
+        .package(url: "https://github.com/realm/realm-core", .branch("master"))
     ],
     targets: [
       .target(
             name: "Realm",
-            dependencies: ["RealmCore"],
+            dependencies: ["RealmObjectStore"],
             path: ".",
             sources: [
-                "Realm/ObjectStore/src/binding_callback_thread_observer.cpp",
-                "Realm/ObjectStore/src/collection_notifications.cpp",
-                "Realm/ObjectStore/src/impl/apple/external_commit_helper.cpp",
-                "Realm/ObjectStore/src/impl/apple/keychain_helper.cpp",
-                "Realm/ObjectStore/src/impl/collection_change_builder.cpp",
-                "Realm/ObjectStore/src/impl/collection_notifier.cpp",
-                "Realm/ObjectStore/src/impl/list_notifier.cpp",
-                "Realm/ObjectStore/src/impl/object_notifier.cpp",
-                "Realm/ObjectStore/src/impl/primitive_list_notifier.cpp",
-                "Realm/ObjectStore/src/impl/realm_coordinator.cpp",
-                "Realm/ObjectStore/src/impl/results_notifier.cpp",
-                "Realm/ObjectStore/src/impl/transact_log_handler.cpp",
-                "Realm/ObjectStore/src/impl/weak_realm_notifier.cpp",
-                "Realm/ObjectStore/src/index_set.cpp",
-                "Realm/ObjectStore/src/list.cpp",
-                "Realm/ObjectStore/src/object.cpp",
-                "Realm/ObjectStore/src/object_changeset.cpp",
-                "Realm/ObjectStore/src/object_schema.cpp",
-                "Realm/ObjectStore/src/object_store.cpp",
-                "Realm/ObjectStore/src/results.cpp",
-                "Realm/ObjectStore/src/schema.cpp",
-                "Realm/ObjectStore/src/shared_realm.cpp",
-                "Realm/ObjectStore/src/thread_safe_reference.cpp",
-                "Realm/ObjectStore/src/util/scheduler.cpp",
-                "Realm/ObjectStore/src/util/uuid.cpp",
-                "Realm/ObjectStore/src/util/scheduler.cpp",
                 "Realm/RLMAccessor.mm",
                 "Realm/RLMAnalytics.mm",
                 "Realm/RLMArray.mm",
@@ -108,7 +82,31 @@ let package = Package(
                 "Realm/RLMSwiftSupport.m",
                 "Realm/RLMThreadSafeReference.mm",
                 "Realm/RLMUpdateChecker.mm",
-                "Realm/RLMUtil.mm"
+                "Realm/RLMUtil.mm",
+
+                // Sync source files
+                "Realm/NSError+RLMSync.m",
+                "Realm/RLMApp.mm",
+                "Realm/RLMAPIKeyAuth.mm",
+                "Realm/RLMBSON.mm",
+                "Realm/RLMCredentials.mm",
+                "Realm/RLMEmailPasswordAuth.mm",
+                "Realm/RLMFindOneAndModifyOptions.mm",
+                "Realm/RLMFindOptions.mm",
+                "Realm/RLMMongoClient.mm",
+                "Realm/RLMMongoCollection.mm",
+                "Realm/RLMNetworkTransport.mm",
+                "Realm/RLMProviderClient.mm",
+                "Realm/RLMPushClient.mm",
+                "Realm/RLMRealm+Sync.mm",
+                "Realm/RLMRealmConfiguration+Sync.mm",
+                "Realm/RLMSyncConfiguration.mm",
+                "Realm/RLMSyncManager.mm",
+                "Realm/RLMSyncSession.mm",
+                "Realm/RLMSyncUtil.mm",
+                "Realm/RLMUpdateResult.mm",
+                "Realm/RLMUser.mm",
+                "Realm/RLMUserAPIKey.mm"
             ],
             publicHeadersPath: "include",
             cxxSettings: cxxSettings
@@ -118,13 +116,8 @@ let package = Package(
             dependencies: ["Realm"],
             path: "RealmSwift",
             exclude: [
-                "Sync.swift",
-                "BSON.swift",
-                "App.swift",
-                "MongoClient.swift",
-                "ObjectiveCSupport+Sync.swift",
-                "ObjectiveCSupport+BSON.swift",
                 "Tests",
+                "Nonsync.swift",
             ]
         ),
         .target(
@@ -150,8 +143,7 @@ let package = Package(
             ],
             cxxSettings: cxxSettings + [
                 .headerSearchPath("Realm"),
-                .headerSearchPath(".."),
-                .headerSearchPath("../ObjectStore/src"),
+                .headerSearchPath("..")
             ]
         ),
         .testTarget(