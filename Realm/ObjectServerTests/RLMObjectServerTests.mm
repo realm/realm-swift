2229fef4701a4143b08659dc40a507c19e84acfb
Thomas Goyne <tg@realm.io>
2021-02-12 16:02:47 -0800

    Fix the testCompactOnLaunch test on arm64
    The file size after compaction is rounded up to the next page boundary, which
    on arm64 is 16k rather than 4k.

--- Realm/ObjectServerTests/RLMObjectServerTests.mm
+++ Realm/ObjectServerTests/RLMObjectServerTests.mm
@@ -37,6 +37,7 @@
 
 #import <realm/object-store/shared_realm.hpp>
 #import <realm/object-store/sync/sync_manager.hpp>
+#import <realm/util/file.hpp>
 
 #if TARGET_OS_OSX
 
@@ -1655,7 +1656,7 @@ static const NSInteger NUMBER_OF_BIG_OBJECTS = 2;
 
     auto finalSize = [[fileManager attributesOfItemAtPath:path error:nil][NSFileSize] unsignedLongLongValue];
     XCTAssertLessThan(finalSize, initialSize);
-    XCTAssertLessThanOrEqual(finalSize, usedSize + 4096U);
+    XCTAssertLessThanOrEqual(finalSize, usedSize + realm::util::page_size());
 }
 
 #pragma mark - Read Only
