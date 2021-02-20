eef57ae09a89a821dd9f141187810bbc79523ca9
Thomas Goyne <tg@realm.io>
2021-02-19 16:37:42 -0800

    Upgrade to core 10.5.3

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let coreVersionStr = "10.5.1"
+let coreVersionStr = "10.5.3"
 let cocoaVersionStr = "10.6.0"
 
 let coreVersionPieces = coreVersionStr.split(separator: ".")
