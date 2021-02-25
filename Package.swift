eeaf17aa27c1d1713038c6ba27f06e257c22d384
Lee Maguire <lee.maguire@mongodb.com>
2021-02-25 09:04:22 +0000

    Don't use v11 branch

--- Package.swift
+++ Package.swift
@@ -62,7 +62,7 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-core", .branch("v11"))
+        .package(url: "https://github.com/realm/realm-core", .exact(Version(coreVersionStr)!))
     ],
     targets: [
       .target(
