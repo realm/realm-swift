a97c0a3b0c4150f57a5e047503802937ed28d6b4
Jason Flax <jsflax@gmail.com>
2021-03-03 15:50:01 -0500

    try test with no destination

--- build.sh
+++ build.sh
@@ -668,7 +668,7 @@ case "$COMMAND" in
         ;;
 
     "test-swiftui")
-        xctest 'SwiftUITestHost' -sdk iphonesimulator -destination 'name=iPhone 8'
+        xctest 'SwiftUITestHost' -sdk iphonesimulator
         exit 0
         ;;
 
