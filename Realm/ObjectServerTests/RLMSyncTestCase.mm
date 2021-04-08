3885dfe46e961822b10037845581bce7d374a765
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-04-08 09:23:04 +0200

    Remove unnecessary return

--- Realm/ObjectServerTests/RLMSyncTestCase.mm
+++ Realm/ObjectServerTests/RLMSyncTestCase.mm
@@ -603,7 +603,6 @@ static NSURL *syncDirectoryForChildProcess() {
     else {
         NSError *error;
         _appId = NSProcessInfo.processInfo.environment[@"RLMParentAppId"] ?: [RealmServer.shared createAppAndReturnError:&error];
-
         if (error) {
             NSLog(@"Failed to create app: %@", error);
             abort();
@@ -619,8 +618,6 @@ static NSURL *syncDirectoryForChildProcess() {
     RLMSyncManager *syncManager = self.app.syncManager;
     syncManager.logLevel = RLMSyncLogLevelTrace;
     syncManager.userAgent = self.name;
-
-    return _app;
 }
 
 - (NSString *)appId {
