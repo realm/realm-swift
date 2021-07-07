cb726e7e50ee39ada7fd32468bb624558e2ffd86
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-07-07 17:30:45 +0200

    Restate log level to its default value

--- Realm/ObjectServerTests/RealmServer.swift
+++ Realm/ObjectServerTests/RealmServer.swift
@@ -405,7 +405,7 @@ public class RealmServer: NSObject {
     @objc public static var shared = RealmServer()
 
     /// Log level for the server and mongo processes.
-    public var logLevel = LogLevel.info
+    public var logLevel = LogLevel.none
 
     /// Process that runs the local mongo server. Should be terminated on exit.
     private let mongoProcess = Process()
