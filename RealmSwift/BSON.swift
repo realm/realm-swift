3ecd65fe4e148ebcd6701c85946f330fca6a5803
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-07-28 15:54:19 +0200

    Resolve PR comments

--- RealmSwift/BSON.swift
+++ RealmSwift/BSON.swift
@@ -18,7 +18,7 @@
 
 import Realm
 
-/// Protocol representing a PartitionValue..
+/// Protocol representing a PartitionValue for synced Realms.
 public protocol PartitionValue {
 }
 
--- RealmSwift/SwiftUI.swift
+++ RealmSwift/SwiftUI.swift
@@ -660,7 +660,7 @@ extension EnvironmentValues {
 
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 private class ObservableAsyncOpenStorage: ObservableObject {
-    var app: App
+    private var app: App
     var configuration: Realm.Configuration
     var partitionValue: AnyBSON
 
