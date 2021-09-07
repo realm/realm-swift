fd93754a305dfe5f7db2227ee7d1a50ce28c59e9
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-09-08 01:07:35 +0200

    Remove ambiguous function

--- RealmSwift/App.swift
+++ RealmSwift/App.swift
@@ -486,20 +486,6 @@ public extension App {
     }
 }
 
-#if swift(>=5.5)
-@available(macOS 12.0, tvOS 15.0, iOS 15.0, watchOS 8.0, *)
-extension App {
-    /// Login to a user for the Realm app.
-    /// @param credentials The credentials identifying the user.
-    /// @returns A publisher that eventually return `User` or `Error`.
-    public func login(credentials: Credentials) async throws -> User {
-        return try await withCheckedThrowingContinuation { continuation in
-            self.login(credentials: credentials, continuation.resume)
-        }
-    }
-}
-#endif // swift(>=5.5)
-
 @available(OSX 10.15, watchOS 6.0, iOS 13.0, iOSApplicationExtension 13.0, OSXApplicationExtension 10.15, tvOS 13.0, macCatalyst 13.0, macCatalystApplicationExtension 13.0, *)
 public extension PushClient {
     /// Request to register device token to the server
