099e55a5d8d13a42e8b4ae385f8f2e23e95d86bd
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-09-17 19:52:39 +0200

    Add  `async` APIs for `EmailPasswordAuth.callResetPasswordFunction`, `User.linkUser`, `MongoCollection` and user functions

--- RealmSwift/Sync.swift
+++ RealmSwift/Sync.swift
@@ -297,7 +297,7 @@ public typealias Provider = RLMIdentityProvider
     /// A closure type for the dynamic remote function type.
     public typealias ResultFunction = ([AnyBSON], @escaping ResultFunctionCompletionHandler) -> Void
 
-    /// The implementation of @dynamicMemberLookup that allows for dynamic remote function calls.
+    /// The implementation of @dynamicMemberLookup that allows for dynamic remote function calls with a `ResultFunctionCompletionHandler` completion.
     public subscript(dynamicMember string: String) -> ResultFunction {
         return { (arguments: [AnyBSON], completionHandler: @escaping ResultFunctionCompletionHandler) in
             let objcArgs = arguments.map(ObjectiveCSupport.convertBson)
@@ -311,13 +311,51 @@ public typealias Provider = RLMIdentityProvider
         }
     }
 
+    /// The implementation of @dynamicMemberLookup that allows for dynamic remote function calls with a `callable` return.
+    public subscript(dynamicMember string: String) -> FunctionCallable {
+        FunctionCallable(name: string, user: user)
+    }
+}
+
+/// Structure enabling the following syntactic sugar for user functions:
+///
+///     guard case let .int32(sum) = try await user.functions.sum([1, 2, 3, 4, 5]) else {
+///        return
+///     }
+///
+/// The dynamic member name (`sum` in the above example) is provided by `@dynamicMemberLookup`
+/// which is directly associated with the function name.
+@dynamicCallable
+public struct FunctionCallable {
+    fileprivate let name: String
+    fileprivate let user: User
+
     #if !(os(iOS) && (arch(i386) || arch(arm)))
-    /// The implementation of @dynamicMemberLookup that allows for dynamic remote function calls.
+    /// The implementation of @dynamicCallable that allows  for `Future<AnyBSON, Error>` callable return.
+    ///
+    ///     let cancellable = user.functions.sum([1, 2, 3, 4, 5])
+    ///        .sink(receiveCompletion: { result in
+    ///     }, receiveValue: { value in
+    ///        // Returned value from function
+    ///     })
+    ///
     @available(OSX 10.15, watchOS 6.0, iOS 13.0, iOSApplicationExtension 13.0, OSXApplicationExtension 10.15, tvOS 13.0, macCatalyst 13.0, macCatalystApplicationExtension 13.0, *)
-    public subscript(dynamicMember string: String) -> ([AnyBSON]) -> Future<AnyBSON, Error> {
-        return { (arguments: [AnyBSON]) in
-            return Future<AnyBSON, Error> { self[dynamicMember: string](arguments, $0) }
+    public func dynamicallyCall(withArguments args: [[AnyBSON]]) -> Future<AnyBSON, Error> {
+        return Future<AnyBSON, Error> { promise in
+            let objcArgs = args.first!.map(ObjectiveCSupport.convertBson)
+            self.user.__callFunctionNamed(name, arguments: objcArgs) { (bson: RLMBSON?, error: Error?) in
+                if let b = bson.map(ObjectiveCSupport.convertBson), let bson = b {
+                    promise(.success(bson))
+                } else {
+                    promise(.failure(error ?? Realm.Error.callFailed))
+                }
+            }
         }
+    }
+    #else
+    /// :nodoc:
+    public func dynamicallyCall(withArguments args: [Never]) {
+        //   noop
     }
     #endif
 }
@@ -710,3 +748,42 @@ public extension User {
         }
     }
 }
+
+#if swift(>=5.5) && canImport(_Concurrency)
+@available(macOS 12.0, tvOS 15.0, iOS 15.0, watchOS 8.0, *)
+public extension User {
+    /// Links the currently authenticated user with a new identity, where the identity is defined by the credential
+    /// specified as a parameter. This will only be successful if this `User` is the currently authenticated
+    /// with the client from which it was created. On success a new user will be returned with the new linked credentials.
+    /// - Parameters:
+    ///   - credentials: The `Credentials` used to link the user to a new identity.
+    /// - Returns:A `User` after successfully update its identity.
+    func linkUser(credentials: Credentials) async throws -> User {
+        return try await withCheckedThrowingContinuation { continuation in
+            linkUser(credentials: credentials, continuation.resume)
+        }
+    }
+}
+
+@available(macOS 12.0, tvOS 15.0, iOS 15.0, watchOS 8.0, *)
+extension FunctionCallable {
+    /// The implementation of @dynamicMemberLookup that allows  for `async await` callable return.
+    ///
+    ///     guard case let .int32(sum) = try await user.functions.sum([1, 2, 3, 4, 5]) else {
+    ///        return
+    ///     }
+    ///
+    public func dynamicallyCall(withArguments args: [[AnyBSON]]) async throws -> AnyBSON {
+        try await withCheckedThrowingContinuation { continuation in
+            let objcArgs = args.first!.map(ObjectiveCSupport.convertBson)
+            self.user.__callFunctionNamed(name, arguments: objcArgs) { (bson: RLMBSON?, error: Error?) in
+                if let b = bson.map(ObjectiveCSupport.convertBson), let bson = b {
+                    continuation.resume(returning: bson)
+                } else {
+                    continuation.resume(throwing: error ?? Realm.Error.callFailed)
+                }
+            }
+        }
+    }
+}
+#endif // swift(>=5.5)
