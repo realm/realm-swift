cf4e05e7a99cbab72c926f9be830741fb73bd98f
nirinchev <nirinchev@users.noreply.github.com>
2021-10-04 16:02:11 +0000

    Update Core to 670559b385025d12c7a31d3444ee1fa556600048

--- Package.swift
+++ Package.swift
@@ -122,7 +122,7 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .exact(Version(coreVersionStr)!))
+        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .revision("670559b385025d12c7a31d3444ee1fa556600048"))
     ],
     targets: [
       .target(
