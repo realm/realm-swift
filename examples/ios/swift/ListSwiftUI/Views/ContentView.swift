264147fbfd0a66208a769eda0d1212c610c1f65c
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-10-15 19:57:36 +0200

    Fix example for new approach

--- examples/ios/swift/ListSwiftUI/Views/ContentView.swift
+++ examples/ios/swift/ListSwiftUI/Views/ContentView.swift
@@ -190,13 +190,9 @@ struct ReminderListResultsView: View {
                     }.accessibilityIdentifier(list.name)
                 }.onDelete(perform: $reminders.remove)
             }
-            .searchable(text: $reminders.searchWhere { reminder, searchString in
-                reminder.title.contains(searchString)
-            }) {
-                ForEach(reminders) { remindersFiltered in
-                    Text(remindersFiltered.name).searchCompletion(remindersFiltered.name)
-                }
-            }
+            .searchable(text: $searchFilter,
+                        collection: $reminders,
+                        keyPath: \.name)
         } else {
             List {
                 ForEach(reminders) { list in
