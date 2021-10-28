16429d7e89c5968e4a28a5b5091effa4be46e9cd
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-10-28 14:13:52 +0200

    Flexible Sync version 1

--- Package.swift
+++ Package.swift
@@ -122,7 +122,7 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .branch("master"))
+        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", from: "dp/flexible_sync")
     ],
     targets: [
       .target(
