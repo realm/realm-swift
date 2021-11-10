640e096d1cf6b9d2c32f3b928d3f132074ddb8d9
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-11-10 21:31:32 +0100

    Revert dependency changes on Package.swift

--- Package.swift
+++ Package.swift
@@ -122,7 +122,7 @@ let package = Package(
             targets: ["Realm", "RealmSwift"]),
     ],
     dependencies: [
-        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", exact(Version(coreVersionStr)!))
+        .package(name: "RealmDatabase", url: "https://github.com/realm/realm-core", .exact(Version(coreVersionStr)!))
     ],
     targets: [
       .target(
