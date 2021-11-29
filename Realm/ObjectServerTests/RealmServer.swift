826400fca5b833158fec26464cdc13b8d1a9f71e
Diana Perez Afanador <dianaafanador3@gmail.com>
2021-11-29 18:57:30 +0100

    Update to use Environment values for AWS Id/Secret Keys

--- Realm/ObjectServerTests/RealmServer.swift
+++ Realm/ObjectServerTests/RealmServer.swift
@@ -439,10 +439,6 @@ public class RealmServer: NSObject {
     /// The current admin session
     private var session: Admin.AdminSession?
 
-    /// AWS access key and Id, for local testing
-    private static let awsAccessKeyId = "<AWS_ACCESS_KEY_ID>"
-    private static let awsSecretAccessKey = "<AWS_SECRET_ACCESS_KEY>"
-
     /// Check if the BaaS files are present and we can run the server
     @objc public class func haveServer() -> Bool {
         let goDir = RealmServer.buildDir.appendingPathComponent("stitch")
@@ -530,12 +526,14 @@ public class RealmServer: NSObject {
     private func launchServerProcess() throws {
         let binDir = Self.buildDir.appendingPathComponent("bin").path
         let libDir = Self.buildDir.appendingPathComponent("lib").path
+        let awsAccessKeyId = ProcessInfo.processInfo.environment["AWS_ACCESS_KEY_ID"]!
+        let awsSecretAccessKey = ProcessInfo.processInfo.environment["AWS_SECRET_ACCESS_KEY"]!
         let binPath = "$PATH:\(binDir)"
         let env = [
             "PATH": binPath,
             "DYLD_LIBRARY_PATH": libDir,
-            "AWS_ACCESS_KEY_ID": Self.awsAccessKeyId,
-            "AWS_SECRET_ACCESS_KEY": Self.awsSecretAccessKey
+            "AWS_ACCESS_KEY_ID": awsAccessKeyId,
+            "AWS_SECRET_ACCESS_KEY": awsSecretAccessKey
         ]
 
         let stitchRoot = RealmServer.buildDir.path + "/go/src/github.com/10gen/stitch"
