c427bbacc3a442d6b7f62b744ee24ad374487036
Pavel Yakimenko <pavel.yakimenko@mongodb.com>
2021-12-13 13:54:14 +0000

    Fix docs for async writes objc

--- Realm/RLMRealm.h
+++ Realm/RLMRealm.h
@@ -518,7 +518,7 @@ typedef unsigned AsyncHandle;
 /**
  Begins asynchronous write transaction.
  @param block The block containing actions to perform.
- `block` should end by calling `commitAsyncWriteTransaction`(), `cancelAsyncTransaction:`,
+ `block` should end by calling `commitAsyncWriteTransaction`, `cancelAsyncTransaction:`,
  `commitWriteTransaction` or `cancelWriteTransaction`.
  Returning without one of these calls will be equivalent to calling `cancelAsyncTransaction`.
  @return Asynchronous transaction's handle.
@@ -534,14 +534,13 @@ typedef unsigned AsyncHandle;
 /** Commit asynchronous transaction.
  @param doneBlock  is queued for execution on the scheduler associated with
    the current realm. It will run after the commit has reached stable storage.
- @param isGroupingAllowed
+ @param isGroupingAllowed If `true`, the next `commitAsyncWriteTransaction` *may* run without an
+ intervening synchronization of stable storage.  Such a sequence of commits form a group.
+ In case of a platform crash, either none or all of the commits in a group will reach stable storage.
+
  * The call returns immediately allowing the caller to proceed while
    the I/O is performed on a dedicated background thread.
  * Callbacks to `doneBlock` will occur in the order of `commitAsyncWriteTransaction`
- * If `isGroupingAllowed` is set, the next `commitAsyncWriteTransaction` *may* run without an
-   intervening synchronization of stable storage.
- * Such a sequence of commits form a group. In case of a platform crash,
-   either none or all of the commits in a group will reach stable storage.
 */
 - (AsyncHandle)commitAsyncWriteTransaction:(nullable void(^)())doneBlock isGroupingAllowed:(BOOL)isGroupingAllowed;
 /// :nodoc:
