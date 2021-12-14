8edce808e011666762d8accde1b50a7444922fd6
Thomas Goyne <thomas.goyne@mongodb.com>
2021-12-14 07:58:53 -0800

    Require Xcode 13.2 for async/await (#7568)

--- Realm/ObjectServerTests/SwiftMongoClientTests.swift
+++ Realm/ObjectServerTests/SwiftMongoClientTests.swift
@@ -870,7 +870,7 @@ class SwiftMongoClientTests: SwiftSyncTestCase {
 }
 
 // MARK: - AsyncAwaitMongoClientTests
-#if swift(>=5.5) && canImport(_Concurrency)
+#if swift(>=5.5.2) && canImport(_Concurrency)
 @available(macOS 12.0, *)
 class AsyncAwaitMongoClientTests: SwiftSyncTestCase {
     func setupMongoCollection() async throws -> MongoCollection {
