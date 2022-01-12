488f22f8070e1a3f0355dbbc8f21d0c2704bb23c
Thomas Goyne <thomas.goyne@mongodb.com>
2022-01-12 08:21:29 -0800

    Upgrade to core 10.8.0 (#7602)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let coreVersionStr = "11.6.1"
+let coreVersionStr = "11.8.0"
 let cocoaVersionStr = "10.21.0"
 
 let coreVersionPieces = coreVersionStr.split(separator: ".")
