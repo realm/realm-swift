9dd41bdd01067ad1d137ad57f1246893c4e321c1
Diana Perez Afanador <dianaafanador3@gmail.com>
2022-02-03 13:02:47 +0100

    Revert "Decouple _RealmSchemaDiscoverable from RealmCollectionValue"
    This reverts commit be150d1dc1ccdf8b028f8179141a644a03933947.

--- RealmSwift/Impl/Persistable.swift
+++ RealmSwift/Impl/Persistable.swift
@@ -42,8 +42,13 @@ public protocol _Persistable: _RealmSchemaDiscoverable, _ObjcBridgeable {
     // Do the values of this type need to be cached on the Persisted?
     static var _rlmRequiresCaching: Bool { get }
     // Get the zero/empty/nil value for this type. Used to supply a default
-    // when the user does not declare one in their model.
-    static func _rlmDefaultValue() -> Self
+    // when the user does not declare one in their model. When `forceDefaultInitialization`
+    // is true we *must* return a non-nil, default instance of `Self`. The latter is
+    // used in conjunction with key path string tracing.
+    static func _rlmDefaultValue(_ forceDefaultInitialization: Bool) -> Self
+    // If we are in key path tracing mode, instantiate an empty object and forward
+    // the lastAccessedNames array.
+    static func _rlmKeyPathRecorder(with lastAccessedNames: NSMutableArray) -> Self
     // The type which is actually stored in the Realm. This is Self for types
     // we support directly, but may be a different type for enums and mapped types.
     associatedtype PersistedType: _Persistable
@@ -52,6 +57,24 @@ public protocol _Persistable: _RealmSchemaDiscoverable, _ObjcBridgeable {
 extension _Persistable {
     public static var _rlmRequiresCaching: Bool {
         false
+    }
+}
+
+extension _RealmSchemaDiscoverable where Self: _Persistable {
+    public static func _rlmKeyPathRecorder(with lastAccessedNames: NSMutableArray) -> Self {
+        let value = Self._rlmDefaultValue(true)
+
+        if let value = value as? ObjectBase {
+            value.lastAccessedNames = lastAccessedNames
+            value.prepareForRecording()
+            return value as! Self
+        }
+
+        if var value = value as? PropertyNameConvertible {
+            value.lastAccessedNames = lastAccessedNames
+            return value as! Self
+        }
+        return value
     }
 }
 
@@ -74,7 +97,7 @@ public protocol _DefaultConstructible {
     init()
 }
 extension _Persistable where Self: _DefaultConstructible {
-    public static func _rlmDefaultValue() -> Self {
+    public static func _rlmDefaultValue(_ forceDefaultInitialization: Bool) -> Self {
         .init()
     }
 }
--- RealmSwift/Projection.swift
+++ RealmSwift/Projection.swift
@@ -239,12 +239,6 @@ open class Projection<Root: ObjectBase>: RealmCollectionValue, ProjectionObserva
 }
 """
     }
-
-    /// :nodoc:
-    public static func _rlmDefaultValue() -> Self {
-        fatalError()
-    }
-
 }
 
 extension ProjectionObservable {
@@ -421,20 +415,21 @@ extension ProjectionObservable {
     public func observe(keyPaths: [PartialKeyPath<Self>] = [],
                         on queue: DispatchQueue? = nil,
                         _ block: @escaping (ObjectChange<Self>) -> Void) -> NotificationToken {
-        var kps: [String]
+        let kps: [String]
         if keyPaths.isEmpty {
             kps = _schema.map(\.originPropertyKeyPathString)
         } else {
-            kps = []
-            let root = Root.keyPathRecorder(with: [])
-            let projection = Self(projecting: root) // tracer time
-            for keyPath in keyPaths {
-                root.lastAccessedNames = NSMutableArray()
-                _ = projection[keyPath: keyPath]
-                kps.append(root.lastAccessedNames!.componentsJoined(by: "."))
+            let emptyRoot = Root()
+            emptyRoot.lastAccessedNames = NSMutableArray()
+            emptyRoot.prepareForRecording()
+            let emptyProjection = Self(projecting: emptyRoot) // tracer time
+            keyPaths.forEach {
+                _ = emptyProjection[keyPath: $0]
             }
+            kps = emptyRoot.lastAccessedNames! as! [String]
         }
-        return rootObject._observe(keyPaths: kps, on: queue, { change in
+        return rootObject._observe(keyPaths: kps,
+                                   on: queue, { change in
             block(ObjectChange<Self>.processChange(change, self))
         })
     }
