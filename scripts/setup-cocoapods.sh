bf5c0461952c427caaf5ef738a2050dcaaaa9830
Thomas Goyne <thomas.goyne@mongodb.com>
2022-02-14 10:49:16 -0800

    Simplify cocoapods setup (#7661)
    We no longer need the shared setup stuff like detecting the Xcode and Swift
    version for cocoapods setup, so extract cocoapods setup (and core downloading)
    from build.sh into its own script.

---
+++ scripts/setup-cocoapods.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+source_root="$(dirname "$0")/.."
+readonly source_root
+
+if [ ! -f "$source_root/core/version.txt" ]; then
+  sh "$source_root/scripts/download-core.sh"
+fi
+
+rm -rf "$source_root/include"
+mkdir -p "$source_root/include"
+cp -R "$source_root/core/realm-monorepo.xcframework/ios-armv7_arm64/Headers" "$source_root/include/core"
+
+mkdir -p "$source_root/include"
+echo '' > "$source_root/Realm/RLMPlatform.h"
+cp "$source_root/Realm/"*.h "$source_root/Realm/"*.hpp "$source_root/include"
