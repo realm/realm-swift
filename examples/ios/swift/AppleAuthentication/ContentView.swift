17a73cdbd49ae2f130293b92b0cb2b86f7af9ea1
Pavel Yakimenko <pavel.yakimenko@mongodb.com>
2022-06-02 14:42:23 +0100

    Use Atlas App Services app

--- examples/ios/swift/AppleAuthentication/ContentView.swift
+++ examples/ios/swift/AppleAuthentication/ContentView.swift
@@ -20,8 +20,7 @@ import SwiftUI
 import AuthenticationServices
 import RealmSwift
 
-/// Your Atlas Device Sync app ID
-/// You can find it in Atlas App Services
+/// Your Atlas App Services app ID
 let appId = "your-app-id"
 
 struct ContentView: View {
