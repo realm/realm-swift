352673195b58245fde03ef290eb3182d234f7d73
ShynggysSaparbek <chingiz.saparbek@gmail.com>
2022-11-09 22:08:45 +0000

    dcssdcsdc

---
+++ MyClass.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  Realm
+//
+//  Created by Shynggys Saparbek on 11/9/22.
+//
+
+import Foundation
+import SwiftUI
+
+class MyClass: View{
+    func fetchData() {
+        let a = 14 % 2 == 9
+        
+        if a {
+            if a && a {
+                if a && a {
+                    if a && a {
+                        if a && a {
+                            if a && a {
+                                if a && a {
+                                    let abra = View() as! Toggle
+                                    
+                                    print(abra)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
