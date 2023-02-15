85c94f45876c06e0a0e6b04044dda77e4d0795b2
Thomas Goyne <tg@realm.io>
2023-02-15 12:26:16 -0800

    Release 10.36.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 import Foundation
 
 let coreVersionStr = "13.4.1"
-let cocoaVersionStr = "10.35.1"
+let cocoaVersionStr = "10.36.0"
 
 let coreVersionPieces = coreVersionStr.split(separator: ".")
 let coreVersionExtra = coreVersionPieces[2].split(separator: "-")
