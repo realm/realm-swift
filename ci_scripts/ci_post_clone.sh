271b157661e2921907b9dcdf1f3109b6b172a6bd
Diana Perez Afanador <dianaafanador3@gmail.com>
2023-09-15 12:47:48 +0200

    Solve PR comments

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -12,25 +12,35 @@ env
 # Create CoreSimulator.log
 touch ~/Library/Logs/CoreSimulator/CoreSimulator.log || true
 
+# Setup environment
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+
 ######################################
 # CI Helpers
 ######################################
 
 install_dependencies() {
     echo ">>> Installing dependencies for ${CI_WORKFLOW}"
-    ruby -v
 
     brew install moreutils
 
     if [[ "$CI_WORKFLOW" == "docs"* ]]; then
+        install_ruby
         gem install jazzy -v 0.14.3
     elif [[ "$CI_WORKFLOW" == "swiftlint"* ]]; then
         brew install swiftlint
     elif [[ "$CI_WORKFLOW" == "cocoapods"* ]]; then
+        install_ruby
         brew install cocoapods
+    elif [[ "$$CI_WORKFLOW" = *"xcode"* ]] || [[ "$target" = "xcframework"* ]]; then
+        install_ruby
     fi
 
     gem install xcpretty -v 0.3.0
+    echo ">>> Using ruby version $(ruby -v)"
 }
 
 install_ruby() {
@@ -39,16 +49,9 @@ install_ruby() {
     brew install ruby
     echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> ~/.bash_profile
     source ~/.bash_profile
-
-    echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
-    echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
-    export GEM_HOME=$HOME/gems
-    export PATH="$GEM_HOME/bin:$PATH"
-    ruby -v
 }
 
 # Dependencies
-install_ruby
 install_dependencies
 
 # CI Workflows
@@ -56,3 +59,6 @@ cd ..
 
 export target=$(echo "$CI_WORKFLOW" | cut -f1 -d_)
 sh -x build.sh ci-pr | ts
+
+# Print environment at the end of ci_post_clone.sh
+env
