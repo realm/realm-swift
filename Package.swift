efa4cb58d0f408d14cf70e818d79aa0d7527090e
Diana Maria Perez Afanador <dianaafanador3@gmail.com>
2024-04-17 11:43:53 +0200

    Upgrade to core 14.5.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 import Foundation
 
-let coreVersion = Version("14.5.0")
+let coreVersion = Version("14.5.2")
 let cocoaVersion = Version("10.49.1")
 
 let cxxSettings: [CXXSetting] = [
