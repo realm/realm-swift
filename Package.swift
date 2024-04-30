ab144081d22617f57b6018bbc56d8210cf0eea29
Thomas Goyne <tg@realm.io>
2024-04-30 16:02:25 -0700

    Allow Realm to be built as a dynamic library when installing via SPM
    This is required for compliance with the new privacy manifest rules. While App
    Store Connect supports static libraries, Xcode doesn't actually build static
    libraries for static targets; it just links the object files directly into the
    app. It also doesn't implement the xcprivacy manifest merging that the
    documentation claims exists, so our privacy manfiest is never actually used.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 import Foundation
 
 let coreVersion = Version("13.26.0")
-let cocoaVersion = Version("10.48.1")
+let cocoaVersion = Version("10.48.2")
 
 let cxxSettings: [CXXSetting] = [
     .headerSearchPath("."),
@@ -147,10 +147,12 @@ let package = Package(
     products: [
         .library(
             name: "Realm",
+            type: .dynamic,
             targets: ["Realm"]),
         .library(
             name: "RealmSwift",
-            targets: ["Realm", "RealmSwift"]),
+            type: .dynamic,
+            targets: ["RealmSwift"]),
     ],
     dependencies: [
         .package(url: "https://github.com/realm/realm-core.git", exact: coreVersion)
