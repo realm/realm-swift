4c4413abd0cd2221f59318f800960fe5bddc1494
Diana Perez Afanador <dianaafanador3@gmail.com>
2024-06-06 19:03:53 +0200

    Release 10.51.0 (#8614)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 import Foundation
 
 let coreVersion = Version("14.9.0")
-let cocoaVersion = Version("10.50.1")
+let cocoaVersion = Version("10.51.0")
 
 let cxxSettings: [CXXSetting] = [
     .headerSearchPath("."),
