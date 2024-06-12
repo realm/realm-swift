e4726b37486cb19a7b5f6d07ad3113ba980084bb
Thomas Goyne <tg@realm.io>
2024-06-12 12:27:51 -0700

    Disable a Swift 5.10-specific workaround when building with Swift 6

--- RealmSwift/Impl/RealmCollectionImpl.swift
+++ RealmSwift/Impl/RealmCollectionImpl.swift
@@ -180,7 +180,7 @@ internal func with<A: Actor, Value: ThreadConfined, Return: Sendable>(
         let unchecked = Unchecked(wrappedValue: value)
         return try await actor.invoke { actor in
             if !Task.isCancelled {
-#if swift(>=5.10)
+#if swift(>=5.10) && compiler(<6)
                 // As of Swift 5.10 the compiler incorrectly thinks that this
                 // is an async hop even though the isolation context is
                 // unchanged. This is fixed in 5.11.
@@ -205,7 +205,7 @@ internal func with<A: Actor, Value: ThreadConfined, Return: Sendable>(
         guard let value = tsr.resolve(in: realm) else {
             return nil
         }
-#if swift(>=5.10)
+#if swift(>=5.10) && compiler(<6)
         // As above; this is safe but 5.10's sendability checking can't prove it
         // nonisolated(unsafe) can't be applied to a let in guard so we need
         // a second variable
