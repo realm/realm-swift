7f2a236e18da056a4e16e565c3ba59587da6898c
Diana Maria Perez Afanador <dianaafanador3@gmail.com>
2024-06-21 11:54:14 +0200

    Test

--- Package.swift
+++ Package.swift
@@ -147,11 +147,9 @@ let package = Package(
     products: [
         .library(
             name: "Realm",
-            type: .dynamic,
             targets: ["Realm"]),
         .library(
             name: "RealmSwift",
-            type: .dynamic,
             targets: ["RealmSwift"]),
     ],
     dependencies: [
